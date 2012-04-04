01033f2a6573dddf22ff70d39d7259f4b450e347
CreativeJuiz <CreativeJuiz@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-04-04 17:27:41 +0000

    New backup version
    git-svn-id: https://plugins.svn.wordpress.org/juiz-last-tweet-widget/tags/1.0.4@527389 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ js/juiz_ltw_auto_slide.js
@@ -0,0 +1,47 @@
+jQuery(document).ready(function(){
+
+	if( jQuery('.juiz_ltw_autoslide').length > 0 ) {
+	
+		jQuery('.juiz_ltw_autoslide').each(function(){
+		
+			var $parent = jQuery(this);
+			var $tweets = $parent.find('li');
+			var ltw_the_delay = $parent.data('delay');
+			var ltw_interval;
+			
+			jQuery($tweets).filter(':not(":first")').hide();
+			jQuery($tweets).filter(':first').addClass('current');
+			
+			jQuery($parent).addClass('hasjs');
+			$parent.prepend($parent.find('.user_avatar'));
+			$parent.find('ul .user_avatar').remove();
+			
+			
+			function juiz_ltw_next_one() {
+				
+				if( jQuery($parent).find('.current').next('li').length > 0 ) {
+					jQuery($parent).find('.current').hide().removeClass('current').next('li').fadeIn(400).addClass('current');
+				}
+				else {
+					jQuery($parent).find('.current').hide().removeClass('current');
+					jQuery($tweets).filter(':first').addClass('current').fadeIn(400);
+				}
+					
+			}
+			
+			ltw_interval = setInterval(juiz_ltw_next_one, ltw_the_delay*1000);
+			
+			$tweets.mouseenter(function(){
+				clearInterval(ltw_interval);
+				ltw_interval = false;
+			})
+			.mouseleave(function(){
+				if(!ltw_interval)
+					ltw_interval = setInterval(juiz_ltw_next_one, ltw_the_delay*1000);
+			});
+			
+		});
+		
+	}
+
+});
---
+++ uninstall.php
@@ -0,0 +1,11 @@
+<?php
+/*
+	The uninstall file
+	(to make your blog always clean when you need to test plugins :p)
+*/
+
+if( !defined( 'ABSPATH') &&  !defined('WP_UNINSTALL_PLUGIN') )
+	    exit();
+	
+	delete_option( 'widget_juiz_last_tweet_widget');
+
