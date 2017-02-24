0cd0701c3563ed51580f94cc439766657c4da809
Tobias Kunze <rixx@cutebit.de>
2017-02-24 17:07:31 +0100

    Begin a new Django project

---
+++ src/pretalx/settings.py
@@ -0,0 +1,228 @@
+import os
+
+from django.contrib.messages import constants as messages  # NOQA
+from django.utils.crypto import get_random_string
+from django.utils.translation import ugettext_lazy as _  # NOQA
+
+
+# File system and directory settings
+
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+DATA_DIR = 'data'
+LOG_DIR = os.path.join(DATA_DIR, 'logs')
+MEDIA_ROOT = os.path.join(DATA_DIR, 'media')
+STATIC_ROOT = os.path.join(os.path.dirname(__file__), 'static.dist')
+
+if not os.path.exists(DATA_DIR):
+    os.mkdir(DATA_DIR)
+if not os.path.exists(LOG_DIR):
+    os.mkdir(LOG_DIR)
+if not os.path.exists(MEDIA_ROOT):
+    os.mkdir(MEDIA_ROOT)
+
+SECRET_FILE = os.path.join(DATA_DIR, '.secret')
+if os.path.exists(SECRET_FILE):
+    with open(SECRET_FILE, 'r') as f:
+        SECRET_KEY = f.read().strip()
+else:
+    chars = 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)'
+    SECRET_KEY = get_random_string(50, chars)
+    with open(SECRET_FILE, 'w') as f:
+        os.chmod(SECRET_FILE, 0o600)
+        os.chown(SECRET_FILE, os.getuid(), os.getgid())
+        f.write(SECRET_KEY)
+
+
+# General setup settings
+
+DEBUG = True
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(DATA_DIR, 'db.sqlite3'),
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
+        'CONN_MAX_AGE': 0,
+    }
+}
+
+STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+SITE_URL = 'http://localhost'
+
+ALLOWED_HOSTS = ['*']
+
+
+# Internal settings
+
+SESSION_COOKIE_NAME = 'pretalx_session'
+CSRF_COOKIE_NAME = 'pretalx_csrftoken'
+SESSION_COOKIE_HTTPONLY = True
+
+INSTALLED_APPS = [
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+]
+
+try:
+    import django_extensions  # noqa
+    INSTALLED_APPS.append('django_extensions')
+except ImportError:
+    pass
+
+MIDDLEWARE = [
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+try:
+    import debug_toolbar  # noqa
+    if DEBUG:
+        INSTALLED_APPS.append('debug_toolbar.apps.DebugToolbarConfig')
+        MIDDLEWARE.append('debug_toolbar.middleware.DebugToolbarMiddleware')
+except ImportError:
+    pass
+
+
+# Security settings
+X_FRAME_OPTIONS = 'DENY'
+
+# URL settings
+ROOT_URLCONF = 'pretalx.urls'
+
+WSGI_APPLICATION = 'pretalx.wsgi.application'
+
+USE_I18N = True
+USE_L10N = True
+USE_TZ = True
+
+# AUTH_USER_MODEL = 'core.User'
+LOGIN_URL = '/login'  # global login does not yet exist
+LOGIN_URL_CONTROL = 'auth.login'
+
+template_loaders = (
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
+)
+if not DEBUG:
+    template_loaders = (
+        ('django.template.loaders.cached.Loader', template_loaders),
+    )
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [
+            os.path.join(DATA_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'templates'),
+        ],
+        'OPTIONS': {
+            'context_processors': [
+                'django.contrib.auth.context_processors.auth',
+                'django.template.context_processors.debug',
+                'django.template.context_processors.i18n',
+                'django.template.context_processors.media',
+                "django.template.context_processors.request",
+                'django.template.context_processors.static',
+                'django.template.context_processors.tz',
+                'django.contrib.messages.context_processors.messages',
+            ],
+            'loaders': template_loaders
+        },
+    },
+]
+
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+)
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'pretalx/static')
+] if os.path.exists(os.path.join(BASE_DIR, 'pretalx/static')) else []
+
+
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
+
+
+DEBUG_TOOLBAR_PATCH_SETTINGS = False
+
+
+DEBUG_TOOLBAR_CONFIG = {
+    'JQUERY_URL': '',
+}
+
+INTERNAL_IPS = ('127.0.0.1', '::1')
+
+MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
+
+loglevel = 'DEBUG' if DEBUG else 'INFO'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'default': {
+            'format': '%(levelname)s %(asctime)s %(name)s %(module)s %(message)s'
+        },
+    },
+    'handlers': {
+        'console': {
+            'level': loglevel,
+            'class': 'logging.StreamHandler',
+            'formatter': 'default'
+        },
+        'file': {
+            'level': loglevel,
+            'class': 'logging.FileHandler',
+            'filename': os.path.join(LOG_DIR, 'pretalx.log'),
+            'formatter': 'default'
+        },
+    },
+    'loggers': {
+        '': {
+            'handlers': ['file', 'console'],
+            'level': loglevel,
+            'propagate': True,
+        },
+        'django.request': {
+            'handlers': ['file', 'console',],
+            'level': loglevel,
+            'propagate': True,
+        },
+        'django.security': {
+            'handlers': ['file', 'console',],
+            'level': loglevel,
+            'propagate': True,
+        },
+        'django.db.backends': {
+            'handlers': ['file', 'console'],
+            'level': 'INFO',  # Do not output all the queries
+            'propagate': True,
+        }
+    },
+}
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
