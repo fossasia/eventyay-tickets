245143998b5218d9159ce8d729a46f8452fe6189
Raphael Michel <mail@raphaelmichel.de>
2017-02-25 21:02:39 +0100

    [orga] Dashboard view

--- src/pretalx/orga/urls.py
+++ src/pretalx/orga/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
 
-from .views import auth
+from .views import auth, dashboard
 
 orga_urls = [
     url('^login/$', auth.LoginView.as_view(), name='login'),
     url('^logout/$', auth.logout_view, name='logout'),
+    url('^$', dashboard.DashboardView.as_view(), name='dashboard'),
 ]
