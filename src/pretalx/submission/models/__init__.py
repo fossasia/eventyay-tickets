dd3e45edeb24d14d1bbada576a207fd8dca2982d
Tobias Kunze <rixx@cutebit.de>
2017-02-25 14:32:37 +0100

    [submission] CfP model

--- src/pretalx/submission/models/__init__.py
+++ src/pretalx/submission/models/__init__.py
@@ -1,7 +1,9 @@
+from .cfp import CfP
 from .submission import Submission, SubmissionStates
 from .type import SubmissionType
 
 __all__ = [
+    'CfP',
     'Submission',
     'SubmissionStates'
     'SubmissionType',
---
+++ src/pretalx/submission/models/cfp.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+
+class CfP(models.Model):
+    event = models.OneToOneField(
+        to='event.Event',
+        on_delete=models.PROTECT,
+    )
+    headline = models.CharField(
+        max_length=300,
+    )
+    text = models.TextField()
+    default_type = models.ForeignKey(
+        to='submission.SubmissionType',
+        on_delete=models.PROTECT,
+    )
+    # languages
