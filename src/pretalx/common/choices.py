8478758c3321db904be72d2a2a52c52dbdd61781
Tobias Kunze <rixx@cutebit.de>
2017-02-25 13:34:28 +0100

    [common] Add choices helper class

---
+++ src/pretalx/common/choices.py
@@ -0,0 +1,29 @@
+class Choices:
+    """
+    Helper class to make choices available as class variables, expose a list
+    with valid choices and at the same time generate the choices tuples for
+    the model class.
+
+    Usage:
+        class MyChoices(Choices):
+            ONE = 'one'
+            TWO = 'dwa'
+            valid_choices = [ONE, TWO]
+
+        class MyModel(models.Model):
+            variant = models.CharField(
+                max_length=MyChoices.get_max_length(),
+                choices=MyChoices.get_choices(),
+            )
+
+    Stolen from bumf (https://github.com/bumfiness/bumf).
+    """
+    @classmethod
+    def get_choices(cls):
+        return (
+            (val, val) for val in cls.valid_choices
+        )
+
+    @classmethod
+    def get_max_length(cls):
+        return max([len(val) for val in cls.valid_choices])
