9f3620be55b1651dec88e6c34a24cdb44137ef31
Tobias Kunze <rixx@cutebit.de>
2017-02-25 22:08:56 +0100

    [orga] Add view mixin

---
+++ src/pretalx/orga/authorization.py
@@ -0,0 +1,28 @@
+from django.core.exceptions import PermissionDenied
+
+from pretalx.event.models import Event
+from pretalx.person.models import EventPermission
+
+
+class OrgaPermissionRequired:
+    login_url = 'orga:login'
+
+    def dispatch(self, request, *args, **kwargs):
+        event_slug = kwargs.get('event')
+
+        if event_slug:
+            try:
+                request.event = Event.objects.get(slug=event_slug)
+            except Event.DoesNotExist:
+                raise PermissionDenied()
+
+            is_permitted = EventPermission.objects.filter(
+                user=request.user,
+                event=request.event,
+                is_orga=True,
+            ).exists()
+
+            if not is_permitted:
+                raise PermissionDenied()
+
+        return super().dispatch(request, args, kwargs)
