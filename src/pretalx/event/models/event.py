cdc992c300a3eaceffdae93524aff1794b8c5989
Tobias Kunze <rixx@cutebit.de>
2017-02-25 13:01:46 +0100

    [event] Add more fields to event model

--- src/pretalx/event/models/event.py
+++ src/pretalx/event/models/event.py
@@ -1,3 +1,5 @@
+import pytz
+
 from django.core.validators import RegexValidator
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
@@ -21,6 +23,11 @@ class Event(models.Model):
         ],
         verbose_name=_("Short form"),
     )
+    subtitle = models.CharField(
+        max_length=200,
+        verbose_name=_('Subitle'),
+        null=True, blank=True,
+    )
     is_public = models.BooleanField(
         default=False,
         verbose_name=_('Event is public')
@@ -33,3 +40,22 @@ class Event(models.Model):
     date_from = models.DateTimeField(verbose_name=_("Event start time"))
     date_to = models.DateTimeField(null=True, blank=True,
                                    verbose_name=_("Event end time"))
+    timezone = models.CharField(
+        choices=[(tz, tz) for tz in pytz.common_timezones],
+        max_length=30,
+        default='Europe/Berlin',
+    )
+    email = models.EmailField(
+        verbose_name=_('Orga email address'),
+        help_text=_('Will be used as sender/reply-to in emails'),
+        null=True, blank=True,
+    )
+    color = models.CharField(
+        max_length=7,
+        verbose_name=_('Main event color'),
+        help_text=_('Please provide a hex value like #00ff00 if you do not like pretalx colors.'),
+        null=True, blank=True,
+        validators=[],
+    )
+    # enable_feedback = models.BooleanField(default=False)
+    # send_notifications = models.BooleanField(default=True)
