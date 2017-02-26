498e73a66c9c18a541dc4c21198fc0fe2e37b9a7
Raphael Michel <mail@raphaelmichel.de>
2017-02-26 13:15:51 +0100

    Use django-bootstrap4

--- src/pretalx/settings.py
+++ src/pretalx/settings.py
@@ -69,6 +69,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'compressor',
+    'bootstrap4',
     'pretalx.common',
     'pretalx.event',
     'pretalx.person',
