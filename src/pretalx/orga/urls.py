4f440d84c67ad745cdefe0f3c30cb049729387ea
Tobias Kunze <rixx@cutebit.de>
2017-02-26 12:30:37 +0100

    [orga] Provide UpdateView

--- src/pretalx/orga/urls.py
+++ src/pretalx/orga/urls.py
@@ -6,6 +6,7 @@ orga_urls = [
     url('^login/$', auth.LoginView.as_view(), name='login'),
     url('^logout/$', auth.logout_view, name='logout'),
     url('^event/new/', event.EventCreate.as_view(), name='event.create'),
+    url('^event/(?P<event>\w+)/update/', event.EventUpdate.as_view(), name='event.update'),
     url('^event/(?P<event>\w+)/', event.EventDetail.as_view(), name='event.view'),
     url('^$', dashboard.DashboardView.as_view(), name='dashboard'),
 ]
--- src/pretalx/orga/views/event.py
+++ src/pretalx/orga/views/event.py
@@ -2,12 +2,12 @@ from django.contrib import messages
 from django.contrib.auth.decorators import user_passes_test
 from django.core.urlresolvers import reverse
 from django.utils.decorators import method_decorator
-from django.views.generic import DetailView
-from django.views.generic.edit import CreateView
+from django.views.generic import CreateView, DetailView, UpdateView
 
 from pretalx.event.models import Event
 from pretalx.orga.authorization import OrgaPermissionRequired
 from pretalx.orga.forms import EventForm
+from pretalx.person.models import User
 
 
 @method_decorator(user_passes_test(lambda u: u.is_superuser), name='dispatch')
@@ -30,5 +30,31 @@ class EventDetail(OrgaPermissionRequired, DetailView):
     model = Event
     slug_url_kwarg = 'event'
     slug_field = 'slug'
-    form_class = EventForm
     template_name = 'orga/event/detail.html'
+
+
+class EventUpdate(OrgaPermissionRequired, UpdateView):
+    model = Event
+    slug_url_kwarg = 'event'
+    slug_field = 'slug'
+    form_class = EventForm
+    template_name = 'orga/event/create.html'
+
+    def get_queryset(self):
+        return Event.objects.filter(permissions__user=self.request.user)
+
+    def get_initial(self):
+        initial = super().get_initial()
+        initial['permissions'] = User.objects.filter(
+            permissions__is_orga=True,
+            permissions__event=self.object
+        )
+        return initial
+
+    def get_success_url(self) -> str:
+        return reverse('orga:event.view', kwargs={'event': self.object.slug})
+
+    def form_valid(self, form):
+        messages.success(self.request, 'Yay!')
+        ret = super().form_valid(form)
+        return ret
