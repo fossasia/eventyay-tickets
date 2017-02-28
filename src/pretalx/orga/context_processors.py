090564f07adcc18d5a4660c43fd5b8d7030481f4
Tobias Kunze <rixx@cutebit.de>
2017-02-28 16:43:20 +0100

    [orga] Add url_name to context_processor (pretix-style)

--- src/pretalx/orga/context_processors.py
+++ src/pretalx/orga/context_processors.py
@@ -1,9 +1,16 @@
+from django.core.urlresolvers import resolve
+
 from pretalx.event.models import Event
 
 
 def add_events(request):
     if request.resolver_match.namespace == 'orga' and not request.user.is_anonymous:
+        try:
+            url_name = resolve(request.path_info).url_name
+        except:
+            url_name = ''
         return {
-            'events': [e for e in Event.objects.filter(permissions__is_orga=True, permissions__user=request.user)]
+            'events': [e for e in Event.objects.filter(permissions__is_orga=True, permissions__user=request.user)],
+            'url_name': url_name,
         }
     return dict()
