ddeabef5ec72351328687bfb7cb17e9564640b47
Tobias Kunze <rixx@cutebit.de>
2017-03-04 20:24:53 +0100

    [orga] Start on speaker view

---
+++ src/pretalx/orga/views/speaker.py
@@ -0,0 +1,16 @@
+from django.contrib import messages
+from django.core.urlresolvers import reverse
+from django.shortcuts import redirect
+from django.utils.translation import ugettext as _
+from django.views.generic import CreateView, DetailView, TemplateView, UpdateView, View, ListView
+
+from pretalx.orga.authorization import OrgaPermissionRequired
+from pretalx.person.models import User
+
+
+class SpeakerList(OrgaPermissionRequired, ListView):
+    template_name = 'orga/speaker/list.html'
+    context_object_name = 'speakers'
+
+    def get_queryset(self):
+        return User.objects.filter(submissions__in=self.request.event.submissions.all())
