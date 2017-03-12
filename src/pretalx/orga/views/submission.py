ec8570d4319a98413e88cb7674a76e7efb8d3b3a
Tobias Kunze <rixx@cutebit.de>
2017-03-12 23:52:17 +0100

    [orga] Add loads of submission views

--- src/pretalx/orga/views/submission.py
+++ src/pretalx/orga/views/submission.py
@@ -2,11 +2,12 @@ from django.contrib import messages
 from django.core.urlresolvers import reverse
 from django.shortcuts import redirect
 from django.utils.translation import ugettext as _
-from django.views.generic import ListView, View
+from django.views.generic import ListView, TemplateView, View
 
 from pretalx.common.views import ActionFromUrl, CreateOrUpdateView
 from pretalx.orga.authorization import OrgaPermissionRequired
 from pretalx.orga.forms import SubmissionForm
+from pretalx.person.models import User
 from pretalx.submission.models import Submission, SubmissionStates
 
 
@@ -37,13 +38,66 @@ class SubmissionReject(OrgaPermissionRequired, View):
         return redirect(reverse('orga:submissions.content.view', kwargs=self.kwargs))
 
 
+class SubmissionSpeakersAdd(OrgaPermissionRequired, View):
+    def post(self, request, *args, **kwargs):
+        super().dispatch(request, *args, **kwargs)
+        submission = self.request.event.submissions.get(pk=self.kwargs.get('pk'))
+        speaker = User.objects.get(pk=request.POST.get('pk'))
+        if submission not in speaker.submissions.all():
+            speaker.submissions.add(submission)
+            speaker.save(update_fields=['submissions'])
+        messages.success(request, _('The speaker has been added to the submission.'))
+        return redirect(reverse('orga:submissions.speakers.view', kwargs=self.kwargs))
+
+
+class SubmissionSpeakersDelete(OrgaPermissionRequired, View):
+    def post(self, request, *args, **kwargs):
+        super().dispatch(request, *args, **kwargs)
+        submission = self.request.event.submissions.get(pk=self.kwargs.get('pk'))
+        speaker = User.objects.get(pk=request.POST.get('pk'))
+
+        if submission in speaker.submissions.all():
+            speaker.submissions.remove(submission)
+            speaker.save(update_fields=['submissions'])
+        messages.success(request, _('The speaker has been removed from the submission.'))
+        return redirect(reverse('orga:submissions.speakers.view', kwargs=self.kwargs))
+
+
+class SubmissionSpeakers(OrgaPermissionRequired, TemplateView):
+    template_name = 'orga/submission/speakers.html'
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        context['submission'] = self.request.event.submissions.get(pk=self.kwargs.get('pk'))
+        return context
+
+
+class SubmissionQuestions(OrgaPermissionRequired, ListView):  # TODO: move from list to formset
+    template_name = 'orga/submission/questions.html'
+    context_object_name = 'questions'
+
+    def get_queryset(self):
+        submission = self.request.event.submissions.get(pk=self.kwargs.get('pk'))
+        return submission.anwers.all()
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        context['submission'] = self.request.event.submissions.get(pk=self.kwargs.get('pk'))
+        return context
+
+
 class SubmissionContent(OrgaPermissionRequired, ActionFromUrl, CreateOrUpdateView):
     model = Submission
     form_class = SubmissionForm
-    template_name = 'orga/submission/form.html'
+    template_name = 'orga/submission/content.html'
 
     def get_object(self):
         return self.request.event.submissions.get(pk=self.kwargs.get('pk'))
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        context['submission'] = self.request.event.submissions.get(pk=self.kwargs.get('pk'))
+        return context
 
     def get_success_url(self) -> str:
         return reverse('orga:submissions.content.view', kwargs=self.kwargs)
