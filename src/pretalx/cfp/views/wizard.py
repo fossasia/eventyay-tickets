c5bf391c34f0f34e11d78c643208c94261572b4a
Tobias Kunze <rixx@cutebit.de>
2017-04-07 17:18:36 +0200

    [cfp,person] Extract login+registration form

--- src/pretalx/cfp/views/wizard.py
+++ src/pretalx/cfp/views/wizard.py
@@ -1,97 +1,23 @@
-from django import forms
-from django.contrib.auth import authenticate, login
-from django.core.exceptions import ValidationError
+from django.contrib.auth import login
 from django.shortcuts import redirect
 from django.urls import reverse
 from django.utils.crypto import get_random_string
-from django.utils.translation import ugettext_lazy as _
 from django.views import View
 from formtools.wizard.views import NamedUrlSessionWizardView
 
 from pretalx.cfp.forms.submissions import InfoForm, QuestionsForm
 from pretalx.cfp.views.event import EventPageMixin
+from pretalx.person.forms import UserForm
 from pretalx.person.models import User
 from pretalx.submission.models import (
     Answer, Question, QuestionVariant,
 )
 
 
-class UserStepForm(forms.Form):
-    login_username = forms.CharField(max_length=60,
-                                     label=_('Username or email address'),
-                                     required=False)
-    login_password = forms.CharField(widget=forms.PasswordInput,
-                                     label=_('Password'),
-                                     required=False)
-    register_username = forms.CharField(max_length=60,
-                                        label=_('Username'),
-                                        required=False)
-    register_email = forms.EmailField(label=_('Email address'),
-                                      required=False)
-    register_password = forms.CharField(widget=forms.PasswordInput,
-                                        label=_('Password'),
-                                        required=False)
-    register_password_repeat = forms.CharField(widget=forms.PasswordInput,
-                                               label=_('Password (again)'),
-                                               required=False)
-
-    def clean(self):
-        data = super().clean()
-
-        if data.get('login_username') and data.get('login_password'):
-            if '@' in data.get('login_username'):
-                try:
-                    uname = User.objects.get(email=data.get('login_username')).nick
-                except User.DoesNotExist:
-                    uname = 'user@invalid'
-            else:
-                uname = data.get('login_username')
-
-            user = authenticate(username=uname, password=data.get('login_password'))
-
-            if user is None:
-                raise ValidationError(_('No user account matches the entered credentials. '
-                                        'Are you sure that you typed your password correctly?'))
-
-            if not user.is_active:
-                raise ValidationError(_('Sorry, your account is currently disabled.'))
-
-            data['user_id'] = user.pk
-
-        elif data.get('register_username') and data.get('register_email') and data.get('register_password'):
-            if data.get('register_password') != data.get('register_password_repeat'):
-                raise ValidationError(_('You entered two different passwords. Please input the same one twice!'))
-
-            if User.objects.filter(nick=data.get('register_username')).exists():
-                raise ValidationError(_('We already have a user with that username. Did you already register before '
-                                        'and just need to log in?'))
-
-            if User.objects.filter(email=data.get('register_email')).exists():
-                raise ValidationError(_('We already have a user with that email address. Did you already register '
-                                        'before and just need to log in?'))
-
-        else:
-            raise ValidationError(
-                _('You need to fill all fields of either the login or the registration form.')
-            )
-
-        return data
-
-    def save(self):
-        data = self.cleaned_data
-        if data.get('register_username') and data.get('register_email') and data.get('register_password'):
-            user = User.objects.create_user(nick=data.get('register_username'),
-                                            email=data.get('register_email'),
-                                            password=data.get('register_password'))
-            data['user_id'] = user.pk
-
-        return data['user_id']
-
-
 FORMS = [
     ("info", InfoForm),
     ("questions", QuestionsForm),
-    ("user", UserStepForm),
+    ("user", UserForm),
 ]
 
 TEMPLATES = {
