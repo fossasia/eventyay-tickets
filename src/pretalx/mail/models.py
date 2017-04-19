dc8f3daecfa526bcd0f1f844bee1d746eb1991b6
Tobias Kunze <rixx@cutebit.de>
2017-04-19 20:28:17 +0200

    [mail] Add QueuedMail model

--- src/pretalx/mail/models.py
+++ src/pretalx/mail/models.py
@@ -29,3 +29,17 @@ class MailTemplate(models.Model):
         verbose_name=_('BCC'),
         help_text=_('Enter comma separated addresses. Will receive a blind copy of every mail sent from this template. This may be a LOT!')
     )
+
+
+class QueuedMail(models.Model):
+    event = models.ForeignKey(
+        to='event.Event',
+        on_delete=models.PROTECT,
+        related_name='queued_mails',
+    )
+    to = models.CharField(max_length=1000)  # allow multiple recipients: several perople per talk
+    reply_to = models.CharField(max_length=1000)
+    cc = models.CharField(max_length=1000)
+    bcc = models.CharField(max_length=1000)
+    subject = models.CharField(max_length=200)  # Use non-i18n fields; this is the final actual to-be-sent version
+    text = models.TextField()
