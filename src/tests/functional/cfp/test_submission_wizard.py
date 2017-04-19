0d3f4bb1b3bd21047a1943b40552a06a3fd7273d
Raphael Michel <mail@raphaelmichel.de>
2017-04-19 23:36:56 +0200

    [cfp] Add a very first functional test

---
+++ src/tests/functional/cfp/test_submission_wizard.py
@@ -0,0 +1,96 @@
+import bs4
+import pytest
+
+from pretalx.submission.models import Submission, SubmissionType
+
+
+@pytest.mark.django_db
+def test_wizard_new_user(event, question, client):
+    # Start wizard
+    resp = client.get('/test/submit/', follow=True)
+    current_url = resp.redirect_chain[-1][0]
+    assert current_url.endswith('/info/')
+    doc = bs4.BeautifulSoup(resp.rendered_content, "lxml")
+
+    # Submit first form
+    data = {
+        'info-title': 'Submission title',
+        'info-submission_type': SubmissionType.objects.filter(event=event).first().pk,
+        'info-content_locale': 'en',
+        'info-description': 'Description',
+        'info-abstract': 'Abstract',
+        'info-notes': 'Notes',
+        'info-duration': '45'
+    }
+    inp_hidden = doc.select("input[name^=submit_wizard]")[0]
+    data[inp_hidden['name']] = inp_hidden['value']
+
+    resp = client.post(current_url, data, follow=True)
+    current_url = resp.redirect_chain[-1][0]
+    assert current_url.endswith('/questions/')
+    doc = bs4.BeautifulSoup(resp.rendered_content, "lxml")
+
+    # Submit question form
+    data = {
+        'questions-question_' + str(question.pk): '42',
+    }
+    inp_hidden = doc.select("input[name^=submit_wizard]")[0]
+    data[inp_hidden['name']] = inp_hidden['value']
+
+    resp = client.post(current_url, data, follow=True)
+    current_url = resp.redirect_chain[-1][0]
+    assert current_url.endswith('/user/')
+    doc = bs4.BeautifulSoup(resp.rendered_content, "lxml")
+
+    # Submit user form
+    data = {
+        'user-register_username': 'testuser',
+        'user-register_email': 'testuser@example.org',
+        'user-register_password': 'testpassw0rd',
+        'user-register_password_repeat': 'testpassw0rd',
+    }
+    inp_hidden = doc.select("input[name^=submit_wizard]")[0]
+    data[inp_hidden['name']] = inp_hidden['value']
+
+    resp = client.post(current_url, data, follow=True)
+    current_url = resp.redirect_chain[-1][0]
+    assert current_url.endswith('/profile/')
+    doc = bs4.BeautifulSoup(resp.rendered_content, "lxml")
+
+    # Submit profile form
+    data = {
+        'profile-name': 'John Doe',
+        'profile-biography': 'I\'m awesome',
+    }
+    inp_hidden = doc.select("input[name^=submit_wizard]")[0]
+    data[inp_hidden['name']] = inp_hidden['value']
+
+    resp = client.post(current_url, data, follow=True)
+    current_url = resp.redirect_chain[-1][0]
+    assert current_url.endswith('/me/submissions')
+    doc = bs4.BeautifulSoup(resp.rendered_content, "lxml")
+    assert doc.select('.alert-success')
+    assert doc.select('.user-row')
+
+    sub = Submission.objects.last()
+    assert sub.title == 'Submission title'
+    assert sub.submission_type is not None
+    assert sub.content_locale == 'en'
+    assert sub.description == 'Description'
+    assert sub.abstract == 'Abstract'
+    assert sub.notes == 'Notes'
+    assert sub.duration == 45
+    answ = sub.answers.first()
+    assert answ.question == question
+    assert answ.answer == '42'
+    user = sub.speakers.first()
+    assert user.nick == 'testuser'
+    assert user.email == 'testuser@example.org'
+    assert user.name == 'John Doe'
+    assert user.profiles.get(event=event).biography == 'I\'m awesome'
+
+
+
+# TODO: test_wizard_no_questions
+# TODO: test_wizard_existing_user
+# TODO: test_wizard_loggedin_user
---
+++ src/tests/functional/conftest.py
@@ -0,0 +1,17 @@
+import pytest
+
+from pretalx.event.models import Event
+from pretalx.submission.models import Question, QuestionVariant
+
+
+@pytest.fixture
+def event():
+    e = Event.objects.create(name='Fancy testevent', is_public=True, slug='test')
+    e.get_cfp()  # created on access
+    return e
+
+@pytest.fixture
+def question(event):
+    return Question.objects.create(event=event, question='How old are you?',
+                                   variant=QuestionVariant.NUMBER,
+                                   required=False)
