6047032a487f9268315015d74ae51251ad64d3f6
Tobias Kunze <rixx@cutebit.de>
2017-05-12 17:03:12 +0200

    [tests] Make tests even DRYer

--- src/tests/functional/cfp/test_submission_wizard.py
+++ src/tests/functional/cfp/test_submission_wizard.py
@@ -7,17 +7,6 @@ from pretalx.submission.models import Submission, SubmissionType
 
 
 class TestWizard:
-    submission_data = {
-        'info-title': 'Submission title',
-        'info-content_locale': 'en',
-        'info-description': 'Description',
-        'info-abstract': 'Abstract',
-        'info-notes': 'Notes',
-    }
-    profile_data = {
-        'profile-name': 'John Doe',
-        'profile-biography': 'I\'m awesome',
-    }
 
     def get_response_and_url(self, client, url, follow=True, method='POST', data=None):
         if method == 'GET':
@@ -32,47 +21,77 @@ class TestWizard:
         input_hidden = doc.select("input[name^=submit_wizard]")[0]
         return input_hidden['name'], input_hidden['value']
 
+    def perform_init_wizard(self, client):
+        # Start wizard
+        response, current_url = self.get_response_and_url(client, '/test/submit/', method='GET')
+        assert current_url.endswith('/info/')
+        return response, current_url
+
+    def perform_info_wizard(
+        self, client, response, url, next='questions',
+        title='Submission title', content_locale='en', description='Description',
+        abstract='Abstract', notes='Notes', submission_type=None,
+    ):
+        submission_data = {
+            'info-title': title, 'info-content_locale': content_locale,
+            'info-description': description, 'info-abstract': abstract,
+            'info-notes': notes, 'info-submission_type': submission_type,
+        }
+        key, value = self.get_form_name(response)
+        submission_data[key] = value
+        response, current_url = self.get_response_and_url(client, url, data=submission_data)
+        assert current_url.endswith(f'/{next}/')
+        return response, current_url
+
+    def perform_question_wizard(self, client, response, url, data, next='profile'):
+        key, value = self.get_form_name(response)
+        data[key] = value
+        response, current_url = self.get_response_and_url(client, url, data=data)
+        assert current_url.endswith(f'/{next}/')
+        return response, current_url
+
+    def perform_user_wizard(self, client, response, url, username, password, next='profile', email=None, register=False):
+        if register:
+            data = {
+                'user-register_username': username,
+                'user-register_email': email,
+                'user-register_password': password,
+                'user-register_password_repeat': password,
+            }
+        else:
+            data = {
+                'user-login_username': username,
+                'user-login_password': password,
+            }
+        key, value = self.get_form_name(response)
+        data[key] = value
+        response, current_url = self.get_response_and_url(client, url, data=data)
+        assert current_url.endswith(f'/{next}/')
+        return response, current_url
+
+    def perform_profile_form(self, client, response, url, name='Jane Doe', bio='l337 hax0r', next='/me/submissions'):
+        data = {
+            'profile-name': name,
+            'profile-biography': bio,
+        }
+        key, value = self.get_form_name(response)
+        data[key] = value
+        response, current_url = self.get_response_and_url(client, url, data=data)
+        assert current_url.endswith(next)
+        return response, current_url
+
     @pytest.mark.django_db
     def test_wizard_new_user(self, event, question, client):
-        # Start wizard
-        resp, current_url = self.get_response_and_url(client, '/test/submit/', method='GET')
-        assert current_url.endswith('/info/')
+        submission_type = SubmissionType.objects.filter(event=event).first().pk
+        answer_data = {f'questions-question_{question.pk}': '42',}
 
-        # Submit info form
-        data = copy.copy(self.submission_data)
-        data['info-submission_type'] = SubmissionType.objects.filter(event=event).first().pk
-        key, value = self.get_form_name(resp)
-        data[key] = value
-        resp, current_url = self.get_response_and_url(client, current_url, data=data)
-        assert current_url.endswith('/questions/')
+        response, current_url = self.perform_init_wizard(client)
+        response, current_url = self.perform_info_wizard(client, response, current_url, submission_type=submission_type)
+        response, current_url = self.perform_question_wizard(client, response, current_url, answer_data, next='user')
+        response, current_url = self.perform_user_wizard(client, response, current_url, username='testuser', password='testpassw0rd', email='testuser@example.org', register=True)
+        response, current_url = self.perform_profile_form(client, response, current_url)
 
-        # Submit question form
-        data = {f'questions-question_{question.pk}': '42',}
-        key, value = self.get_form_name(resp)
-        data[key] = value
-        resp, current_url = self.get_response_and_url(client, current_url, data=data)
-        assert current_url.endswith('/user/')
-
-        # Submit user form
-        data = {
-            'user-register_username': 'testuser',
-            'user-register_email': 'testuser@example.org',
-            'user-register_password': 'testpassw0rd',
-            'user-register_password_repeat': 'testpassw0rd',
-        }
-        key, value = self.get_form_name(resp)
-        data[key] = value
-        resp, current_url = self.get_response_and_url(client, current_url, data=data)
-        assert current_url.endswith('/profile/')
-
-        # Submit profile form
-        data = self.profile_data
-        key, value = self.get_form_name(resp)
-        data[key] = value
-        resp, current_url = self.get_response_and_url(client, current_url, data=data)
-        assert current_url.endswith('/me/submissions')
-
-        doc = bs4.BeautifulSoup(resp.rendered_content, "lxml")
+        doc = bs4.BeautifulSoup(response.rendered_content, "lxml")
         assert doc.select('.alert-success')
         assert doc.select('.user-row')
 
@@ -89,49 +108,22 @@ class TestWizard:
         user = sub.speakers.first()
         assert user.nick == 'testuser'
         assert user.email == 'testuser@example.org'
-        assert user.name == 'John Doe'
-        assert user.profiles.get(event=event).biography == 'I\'m awesome'
+        assert user.name == 'Jane Doe'
+        assert user.profiles.get(event=event).biography == 'l337 hax0r'
 
 
     @pytest.mark.django_db
     def test_wizard_existing_user(self, event, client, question, user):
-        # Start wizard
-        resp, current_url = self.get_response_and_url(client, '/test/submit/', method='GET')
-        assert current_url.endswith('/info/')
+        submission_type = SubmissionType.objects.filter(event=event).first().pk
+        answer_data = {f'questions-question_{question.pk}': '42',}
 
-        # Submit info form
-        data = copy.copy(self.submission_data)
-        data['info-submission_type'] = SubmissionType.objects.filter(event=event).first().pk
-        key, value = self.get_form_name(resp)
-        data[key] = value
-        resp, current_url = self.get_response_and_url(client, current_url, data=data)
-        assert current_url.endswith('/questions/')
+        response, current_url = self.perform_init_wizard(client)
+        response, current_url = self.perform_info_wizard(client, response, current_url, submission_type=submission_type)
+        response, current_url = self.perform_question_wizard(client, response, current_url, answer_data, next='user')
+        response, current_url = self.perform_user_wizard(client, response, current_url, username='testuser', password='testpassw0rd')
+        response, current_url = self.perform_profile_form(client, response, current_url)
 
-        # Submit question form
-        data = {f'questions-question_{question.pk}': '42',}
-        key, value = self.get_form_name(resp)
-        data[key] = value
-        resp, current_url = self.get_response_and_url(client, current_url, data=data)
-        assert current_url.endswith('/user/')
-
-        # Submit user form
-        data = {
-            'user-login_username': 'testuser',
-            'user-login_password': 'testpassw0rd',
-        }
-        key, value = self.get_form_name(resp)
-        data[key] = value
-        resp, current_url = self.get_response_and_url(client, current_url, data=data)
-        assert current_url.endswith('/profile/')
-
-        # Submit profile form
-        data = self.profile_data
-        key, value = self.get_form_name(resp)
-        data[key] = value
-        resp, current_url = self.get_response_and_url(client, current_url, data=data)
-        assert current_url.endswith('/me/submissions')
-
-        doc = bs4.BeautifulSoup(resp.rendered_content, "lxml")
+        doc = bs4.BeautifulSoup(response.rendered_content, "lxml")
         assert doc.select('.alert-success')
         assert doc.select('.user-row')
 
@@ -143,41 +135,22 @@ class TestWizard:
         s_user = sub.speakers.first()
         assert s_user.pk == user.pk
         assert s_user.nick == 'testuser'
-        assert s_user.name == 'John Doe'
-        assert s_user.profiles.get(event=event).biography == 'I\'m awesome'
+        assert s_user.name == 'Jane Doe'
+        assert s_user.profiles.get(event=event).biography == 'l337 hax0r'
 
 
     @pytest.mark.django_db
     def test_wizard_logged_in_user(self, event, client, question, user):
+        submission_type = SubmissionType.objects.filter(event=event).first().pk
+        answer_data = {f'questions-question_{question.pk}': '42',}
+
         client.force_login(user)
+        response, current_url = self.perform_init_wizard(client)
+        response, current_url = self.perform_info_wizard(client, response, current_url, submission_type=submission_type)
+        response, current_url = self.perform_question_wizard(client, response, current_url, answer_data, next='profile')
+        response, current_url = self.perform_profile_form(client, response, current_url)
 
-        # Start wizard
-        resp, current_url = self.get_response_and_url(client, '/test/submit/', method='GET')
-        assert current_url.endswith('/info/')
-
-        # Submit info form
-        data = copy.copy(self.submission_data)
-        data['info-submission_type'] = SubmissionType.objects.filter(event=event).first().pk
-        key, value = self.get_form_name(resp)
-        data[key] = value
-        resp, current_url = self.get_response_and_url(client, current_url, data=data)
-        assert current_url.endswith('/questions/')
-
-        # Submit question form
-        data = {f'questions-question_{question.pk}': '42',}
-        key, value = self.get_form_name(resp)
-        data[key] = value
-        resp, current_url = self.get_response_and_url(client, current_url, data=data)
-        assert current_url.endswith('/profile/')
-
-        # Submit profile form
-        data = self.profile_data
-        key, value = self.get_form_name(resp)
-        data[key] = value
-        resp, current_url = self.get_response_and_url(client, current_url, data=data)
-        assert current_url.endswith('/me/submissions')
-
-        doc = bs4.BeautifulSoup(resp.rendered_content, "lxml")
+        doc = bs4.BeautifulSoup(response.rendered_content, "lxml")
         assert doc.select('.alert-success')
         assert doc.select('.user-row')
         sub = Submission.objects.last()
@@ -188,34 +161,20 @@ class TestWizard:
         s_user = sub.speakers.first()
         assert s_user.pk == user.pk
         assert s_user.nick == 'testuser'
-        assert s_user.name == 'John Doe'
-        assert s_user.profiles.get(event=event).biography == 'I\'m awesome'
+        assert s_user.name == 'Jane Doe'
+        assert s_user.profiles.get(event=event).biography == 'l337 hax0r'
 
 
     @pytest.mark.django_db
     def test_wizard_logged_in_user_no_questions(self, event, client, user):
+        submission_type = SubmissionType.objects.filter(event=event).first().pk
+
         client.force_login(user)
+        response, current_url = self.perform_init_wizard(client)
+        response, current_url = self.perform_info_wizard(client, response, current_url, submission_type=submission_type, next='profile')
+        response, current_url = self.perform_profile_form(client, response, current_url)
 
-        # Start wizard
-        resp, current_url = self.get_response_and_url(client, '/test/submit/', method='GET')
-        assert current_url.endswith('/info/')
-
-        # Submit info form
-        data = copy.copy(self.submission_data)
-        data['info-submission_type'] = SubmissionType.objects.filter(event=event).first().pk
-        key, value = self.get_form_name(resp)
-        data[key] = value
-        resp, current_url = self.get_response_and_url(client, current_url, data=data)
-        assert current_url.endswith('/profile/')
-
-        # Submit profile form
-        data = self.profile_data
-        key, value = self.get_form_name(resp)
-        data[key] = value
-        resp, current_url = self.get_response_and_url(client, current_url, data=data)
-        assert current_url.endswith('/me/submissions')
-
-        doc = bs4.BeautifulSoup(resp.rendered_content, "lxml")
+        doc = bs4.BeautifulSoup(response.rendered_content, "lxml")
         assert doc.select('.alert-success')
         assert doc.select('.user-row')
         sub = Submission.objects.last()
@@ -224,8 +183,8 @@ class TestWizard:
         s_user = sub.speakers.first()
         assert s_user.pk == user.pk
         assert s_user.nick == 'testuser'
-        assert s_user.name == 'John Doe'
-        assert s_user.profiles.get(event=event).biography == 'I\'m awesome'
+        assert s_user.name == 'Jane Doe'
+        assert s_user.profiles.get(event=event).biography == 'l337 hax0r'
 
 
 # TODO: test failed registration
