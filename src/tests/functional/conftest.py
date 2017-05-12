9ee1394764dc37e661e56825ed30016249cce73b
Tobias Kunze <rixx@cutebit.de>
2017-05-12 17:39:52 +0200

    [tests] Add orga fixtures

--- src/tests/functional/conftest.py
+++ src/tests/functional/conftest.py
@@ -1,7 +1,7 @@
 import pytest
 
 from pretalx.event.models import Event
-from pretalx.person.models import User
+from pretalx.person.models import EventPermission, User
 from pretalx.submission.models import Question, QuestionVariant
 
 
@@ -22,3 +22,16 @@ def question(event):
 @pytest.fixture
 def user(event):
     return User.objects.create_user('testuser', 'testpassw0rd')
+
+
+@pytest.fixture
+def orga_user(event):
+    user = User.objects.create_user('orgauser', 'orgapassw0rd')
+    EventPermission.objects.create(user=user, event=event, is_orga=True)
+    return user
+
+
+@pytest.fixture
+def orga_client(orga_user, client):
+    client.force_login(orga_user)
+    return client
