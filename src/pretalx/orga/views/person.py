7a52cec5da69454bf2fb88199fd940047aa06a7b
Tobias Kunze <rixx@cutebit.de>
2017-05-19 16:41:36 +0200

    [orga] Return dict instead of list

--- src/pretalx/orga/views/person.py
+++ src/pretalx/orga/views/person.py
@@ -10,15 +10,18 @@ class UserList(View):
     def dispatch(self, request, *args, **kwargs):
         search = request.GET.get('search')
         if not search or len(search) < 3:
-            return JsonResponse([], safe=False)
+            return JsonResponse({'count': 0})
 
         queryset = User.objects.filter(
             Q(nick__icontains=search) | Q(name__icontains=search)
         )
-        return JsonResponse([
-            {
-                'nick': user.nick,
-                'name': user.name,
-            }
-            for user in queryset
-        ], safe=False)
+        return JsonResponse({
+            'count': len(queryset),
+            'results': [
+                {
+                    'nick': user.nick,
+                    'name': user.name,
+                }
+                for user in queryset
+            ],
+        })
