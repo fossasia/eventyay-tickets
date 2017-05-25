5d2a594a05f537cb22c81ea35a8fc6129776d7b3
Raphael Michel <mail@raphaelmichel.de>
2017-05-25 19:48:12 +0200

    Change settings during tests to avoid static file issues
    Also, disable caching and mails during testing

--- src/tests/test_settings.py
+++ src/tests/test_settings.py
@@ -1,4 +1,37 @@
+import atexit
+import os
+import tempfile
+
+tmpdir = tempfile.TemporaryDirectory()
+os.environ.setdefault('DATA_DIR', tmpdir.name)
+
 from pretalx.settings import *  # NOQA
 
+DATA_DIR = tmpdir.name
+LOG_DIR = os.path.join(DATA_DIR, 'logs')
+MEDIA_ROOT = os.path.join(DATA_DIR, 'media')
+
+atexit.register(tmpdir.cleanup)
+
+EMAIL_BACKEND = 'django.core.mail.outbox'
+
+COMPRESS_ENABLED = COMPRESS_OFFLINE = False
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
+
 DEBUG = True
-COMPRESS_OFFLINE = False
+DEBUG_PROPAGATE_EXCEPTIONS = True
+
+PASSWORD_HASHERS = ['django.contrib.auth.hashers.MD5PasswordHasher']
+
+# Disable celery
+CELERY_ALWAYS_EAGER = True
+HAS_CELERY = False
+
+# Don't use redis
+SESSION_ENGINE = "django.contrib.sessions.backends.db"
+HAS_REDIS = False
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.dummy.DummyCache',
+    }
+}
