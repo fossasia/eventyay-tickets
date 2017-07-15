c228ce65a90efb18c536b64953b4b1f88cdc7b49
Tobias Kunze <rixx@cutebit.de>
2017-07-15 22:47:49 +0200

    [schedule] Use scrollY, too

--- src/pretalx/static/orga/js/schedule.js
+++ src/pretalx/static/orga/js/schedule.js
@@ -225,7 +225,7 @@ var app = new Vue({
           dragController.draggedTalk.room = newRoomColumn.dataset.id
           dragController.roomColumn.classList.add('hover-active')
           if (dragController.roomColumn) {
-            var position = event.clientY - dragController.roomColumn.offsetTop
+            var position = event.clientY - dragController.roomColumn.offsetTop + window.scrollY
             position -= position % 5
             dragController.draggedTalk.start = moment(this.start).add(position, 'minutes')
           }
