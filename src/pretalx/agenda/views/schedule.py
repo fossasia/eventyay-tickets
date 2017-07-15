e28cc87a4fdb9ff8abdba11ddec62a531c021c26
Tobias Kunze <rixx@cutebit.de>
2017-07-16 00:00:52 +0200

    [agenda] Start ScheduleView implementation

--- src/pretalx/agenda/views/schedule.py
+++ src/pretalx/agenda/views/schedule.py
@@ -1,8 +1,28 @@
-from django.views.generic import View
+from django.views.generic import TemplateView, View
 
 
-class ScheduleView(View):
-    pass
+class ScheduleView(TemplateView):
+    template_name = 'agenda/schedule.html'
+
+    def get_object(self):
+        if self.request.GET.get('version'):
+            return self.request.event.schedules.filter(version=self.request.GET.get('version'))
+        return self.request.event.current_schedule
+
+
+    def get_context_data(self):
+        ctx = super().get_context_data()
+        schedule = self.get_object()
+
+        if not schedule and self.request.GET.get('version'):
+            ctx['version'] = self.request.GET.get('version')
+            ctx['error'] = 'wrong-version'
+            return ctx
+        elif not schedule:
+            ctx['error'] = 'no-schedule'
+            return ctx
+        ctx['schedule'] = schedule
+        return ctx
 
 
 class TalkView(View):
