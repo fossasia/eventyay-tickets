ae430bcee773fbe9767d9e722e41b5cb10903c19
Tobias Kunze <rixx@cutebit.de>
2017-07-16 22:45:46 +0200

    [agenda] Get context data for frabview

--- src/pretalx/agenda/views/schedule.py
+++ src/pretalx/agenda/views/schedule.py
@@ -1,3 +1,5 @@
+from datetime import timedelta
+
 from django.views.generic import TemplateView, View
 
 
@@ -9,7 +11,7 @@ class ScheduleView(TemplateView):
             return self.request.event.schedules.filter(version=self.request.GET.get('version'))
         return self.request.event.current_schedule
 
-    def get_context_data(self):
+    def get_context_data(self, event):
         ctx = super().get_context_data()
         schedule = self.get_object()
 
@@ -22,6 +24,32 @@ class ScheduleView(TemplateView):
             return ctx
         ctx['schedule'] = schedule
         ctx['schedules'] = self.request.event.schedules.filter(published__isnull=False).values_list('version')
+        return ctx
+
+
+class FrabView(ScheduleView):
+    template_name = 'agenda/schedule.xml'
+
+    def get_context_data(self, event):
+        ctx = super().get_context_data(event)
+        event = self.request.event
+        schedule = ctx.get('schedule')
+        if not schedule:
+            return ctx
+
+        ctx['data'] = [
+            {
+                'index': index + 1,
+                'start': current_date,
+                'end': current_date + timedelta(days=1),
+                'rooms': [{
+                    'name': room.name,
+                    'talks': [talk for talk in schedule.talks.filter(start__date=current_date.date(), room=room)],
+                } for room in event.rooms.all()],
+            } for index, current_date in enumerate([
+                event.datetime_from + timedelta(days=i) for i in range((event.date_to - event.date_from).days + 1)
+            ])
+        ]
         return ctx
 
 
