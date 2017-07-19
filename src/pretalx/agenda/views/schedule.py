d1e4a010923719226e9c1eb87249ae7e6e185314
Tobias Kunze <rixx@cutebit.de>
2017-07-19 23:55:17 +0200

    [agenda] Fix missing variable

--- src/pretalx/agenda/views/schedule.py
+++ src/pretalx/agenda/views/schedule.py
@@ -14,6 +14,7 @@ class ScheduleView(TemplateView):
     def get_context_data(self, event):
         ctx = super().get_context_data()
         schedule = self.get_object()
+        event = self.request.event
 
         if not schedule and self.request.GET.get('version'):
             ctx['version'] = self.request.GET.get('version')
