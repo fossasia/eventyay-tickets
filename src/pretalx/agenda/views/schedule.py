c6fa590502a76a58e3b13e222debf4082f9534df
Tobias Kunze <rixx@cutebit.de>
2017-07-19 23:56:39 +0200

    [schedule] Provide first_talk and last_talk per day
    … and keep talks ordered by start time

--- src/pretalx/agenda/views/schedule.py
+++ src/pretalx/agenda/views/schedule.py
@@ -43,9 +43,11 @@ class FrabView(ScheduleView):
                 'index': index + 1,
                 'start': current_date,
                 'end': current_date + timedelta(days=1),
+                'first_talk': schedule.talks.filter(start__date=current_date.date()).order_by('start').first(),
+                'last_talk': schedule.talks.filter(end__date=current_date.date()).order_by('end').last(),
                 'rooms': [{
                     'name': room.name,
-                    'talks': [talk for talk in schedule.talks.filter(start__date=current_date.date(), room=room)],
+                    'talks': [talk for talk in schedule.talks.filter(start__date=current_date.date(), room=room).order_by('start')],
                 } for room in event.rooms.all()],
             } for index, current_date in enumerate([
                 event.datetime_from + timedelta(days=i) for i in range((event.date_to - event.date_from).days + 1)
