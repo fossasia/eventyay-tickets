52bd7381e95188d1dd87b8dc90f3f003af17955e
Tobias Kunze <rixx@cutebit.de>
2017-07-22 20:30:53 +0200

    [agenda] Only show confirmed submissions in frontend

--- src/pretalx/agenda/views/schedule.py
+++ src/pretalx/agenda/views/schedule.py
@@ -4,6 +4,8 @@ from csp.decorators import csp_update
 from django.utils.decorators import method_decorator
 from django.views.generic import TemplateView, View
 
+from pretalx.submission.models import SubmissionStates
+
 
 class ScheduleDataView(TemplateView):
     template_name = 'agenda/schedule.html'
@@ -27,16 +29,18 @@ class ScheduleDataView(TemplateView):
             return ctx
         ctx['schedule'] = schedule
         ctx['schedules'] = event.schedules.filter(published__isnull=False).values_list('version')
+        talks = schedule.talks.filter(submission__state=SubmissionStates.CONFIRMED)
+
         ctx['data'] = [
             {
                 'index': index + 1,
                 'start': current_date,
                 'end': current_date + timedelta(days=1),
-                'first_talk': schedule.talks.filter(start__date=current_date.date()).order_by('start').first(),
-                'last_talk': schedule.talks.filter(end__date=current_date.date()).order_by('end').last(),
+                'first_talk': talks.filter(start__date=current_date.date()).order_by('start').first(),
+                'last_talk': talks.filter(end__date=current_date.date()).order_by('end').last(),
                 'rooms': [{
                     'name': room.name,
-                    'talks': [talk for talk in schedule.talks.filter(start__date=current_date.date(), room=room).order_by('start')],
+                    'talks': [talk for talk in talks.filter(start__date=current_date.date(), room=room).order_by('start')],
                 } for room in event.rooms.all()],
             } for index, current_date in enumerate([
                 event.datetime_from + timedelta(days=i) for i in range((event.date_to - event.date_from).days + 1)
