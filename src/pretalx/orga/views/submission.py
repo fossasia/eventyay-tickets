fossasia/eventyay-tickets

commit 657aaed3dd667f5a3086bd6ec4e01a0f15a55418

[orga] Allow to confirm submission

src/pretalx/orga/views/submission.py
@@ -24,12 +24,26 @@ class SubmissionAccept(View):
             return redirect(reverse('orga:submissions.content.view', kwargs=self.kwargs))
 
 
+class SubmissionConfirm(View):
+    def dispatch(self, request, *args, **kwargs):
+        super().dispatch(request, *args, **kwargs)
+        submission = self.request.event.submissions.get(pk=self.kwargs.get('pk'))
+
+        try:
+            submission.confirm(person=request.user, orga=True)
+            messages.success(request, _('The submission has been confirmed.'))
+            return redirect(reverse('orga:submissions.content.view', kwargs=self.kwargs))
+        except SubmissionError:
+            messages.error(request, _('A submission must be accepted to become confirmed.'))
+            return redirect(reverse('orga:submissions.content.view', kwargs=self.kwargs))
+
+
 class SubmissionReject(View):
     def dispatch(self, request, *args, **kwargs):
         super().dispatch(request, *args, **kwargs)
         submission = self.request.event.submissions.get(pk=self.kwargs.get('pk'))
-        submission.reject(person=request.user)
-        messages.success(request, _('The submission has been rejected.'))
+        submission.confirm(person=request.user)
+        messages.success(request, _('The submission has been marked as confirmed.'))
         return redirect(reverse('orga:submissions.content.view', kwargs=self.kwargs))
 
 

src/pretalx/submission/models/submission.py
@@ -121,6 +121,14 @@ class Submission(LogMixin, models.Model):
             return self.submission_type.default_duration
         return self.duration
 
+    def confirm(self, person=None, force=False, orga=False):
+        if self.state not in [SubmissionStates.ACCEPTED] and not force:
+            raise SubmissionError(f'Submission must be accepted, not {self.state} to be confirmed.')
+
+        self.state = SubmissionStates.CONFIRMED
+        self.save(update_fields=['state'])
+        self.log_action('pretalx.submission.confirmation', person=person, orga=orga)
+
     def accept(self, person=None, force=False):
         if self.state not in [SubmissionStates.SUBMITTED, SubmissionStates.REJECTED] and not force:
             raise SubmissionError(f'Submission must be submitted or rejected, not {self.state} to be accepted.')

src/pretalx/cfp/views/user.py
@@ -108,9 +108,7 @@ class SubmissionConfirmView(LoggedInEventPageMixin, SubmissionViewMixin, View):
     def dispatch(self, request, *args, **kwargs):
         submission = self.get_object()
         if submission.state == SubmissionStates.ACCEPTED:
-            submission.state = SubmissionStates.CONFIRMED
-            submission.save(update_fields=['state'])
-            submission.log_action('pretalx.submission.confirmation', person=request.user)
+            submission.confirm(person=request.user, orga=False)
             messages.success(self.request, _('Your submission has been confirmed – we\'re looking forward to seeing you!'))
         elif submission.state == SubmissionStates.CONFIRMED:
             messages.success(self.request, _('This submission has already been confirmed – we\'re looking forward to seeing you!'))

src/pretalx/orga/templates/orga/submission/content.html
@@ -13,9 +13,13 @@
                 {% elif action == "edit" %}
                     <button type="submit" class="btn btn-primary">{% trans "Save" %}</button>
                 {% elif action == "view" %}
-                    <a class="btn btn-primary" href={% url "orga:submissions.accept" event=request.event.slug pk=form.instance.pk %}>{% trans "Accept" %}</a>
-                    <a class="btn btn-secondary" href={% url "orga:submissions.content.edit" event=request.event.slug pk=form.instance.pk %}>{% trans "Edit" %}</a>
-                    <a class="btn btn-danger" href={% url "orga:submissions.reject" event=request.event.slug pk=form.instance.pk %}>{% trans "Reject" %}</a>
+                    {% if form.instance.state == "submitted" or form.instance.state == "rejected" %}
+                        <a class="btn btn-primary" href="{% url "orga:submissions.accept" event=request.event.slug pk=form.instance.pk %}">{% trans "Accept" %}</a>
+                    {% elif form.instance.state == "accepted" %}
+                        <a class="btn btn-secondary" href="{% url "orga:submissions.confirm" event=request.event.slug pk=form.instance.pk %}">{% trans "Confirm" %}</a>
+                    {% endif %}
+                    <a class="btn btn-secondary" href="{% url "orga:submissions.content.edit" event=request.event.slug pk=form.instance.pk %}">{% trans "Edit" %}</a>
+                    <a class="btn btn-danger" href="{% url "orga:submissions.reject" event=request.event.slug pk=form.instance.pk %}">{% trans "Reject" %}</a>
                 {% endif %}
             </div>
         </div>

src/pretalx/orga/urls.py
@@ -52,6 +52,7 @@ orga_urls = [
             url('^edit$', submission.SubmissionContent.as_view(), name='submissions.content.edit'),
             url('^accept$', submission.SubmissionAccept.as_view(), name='submissions.accept'),
             url('^reject$', submission.SubmissionReject.as_view(), name='submissions.reject'),
+            url('^confirm', submission.SubmissionConfirm.as_view(), name='submissions.confirm'),
             url('^questions$', submission.SubmissionQuestions.as_view(), name='submissions.questions.view'),
             url('^questions/edit$', submission.SubmissionQuestions.as_view(), name='submissions.questions.edit'),
             url('^speakers$', submission.SubmissionSpeakers.as_view(), name='submissions.speakers.view'),

commit ab85077ee541b2ea81e14ebce8012d8a48e27ecb

[orga] Fix bug introduced in 657aaed

src/pretalx/orga/views/submission.py
@@ -42,8 +42,8 @@ class SubmissionReject(View):
     def dispatch(self, request, *args, **kwargs):
         super().dispatch(request, *args, **kwargs)
         submission = self.request.event.submissions.get(pk=self.kwargs.get('pk'))
-        submission.confirm(person=request.user)
-        messages.success(request, _('The submission has been marked as confirmed.'))
+        submission.reject(person=request.user)
+        messages.success(request, _('The submission has been rejected.'))
         return redirect(reverse('orga:submissions.content.view', kwargs=self.kwargs))
 
 