0cf85260f0fe279cb6417a8a39e5bead92ff98bf
Tobias Kunze <rixx@cutebit.de>
2017-07-24 14:03:31 +0200

    [orga] Add mail composition form

--- src/pretalx/orga/forms/mails.py
+++ src/pretalx/orga/forms/mails.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.utils.translation import ugettext_lazy as _
 
 from pretalx.common.forms import ReadOnlyFlag
 from pretalx.mail.models import MailTemplate, QueuedMail
@@ -22,5 +23,23 @@ class OutboxMailForm(ReadOnlyFlag, forms.ModelForm):
         ]
 
 
-class WriteMailForm(OutboxMailForm):
-    pass
+class WriteMailForm(forms.ModelForm):
+    recipients = forms.MultipleChoiceField(choices=(
+        ('submitted', _('Everyone with submission(s) that have not been accepted/rejected yet')),
+        ('accepted', _('All accepted speakers (who have not confirmed their talk yet)')),
+        ('confirmed', _('All confirmed speakers')),
+        ('rejected', _('All rejected speakers')),
+        ('selected_submissions', _('All speakers of the selected submissions below'))
+    ), widget=forms.CheckboxSelectMultiple)
+    submissions = forms.MultipleChoiceField(required=False)
+    reply_to = forms.EmailField(required=False)
+
+    def __init__(self, event, **kwargs):
+        super().__init__(**kwargs)
+        self.fields['submissions'].choices = [(sub.code, sub.title) for sub in event.submissions.all()]
+
+    class Meta:
+        model = QueuedMail
+        fields = [
+            'cc', 'bcc', 'subject', 'text',
+        ]
