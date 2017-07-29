d4020b94d95ee08072e9c4a8827199dbab052963
Tobias Kunze <rixx@cutebit.de>
2017-07-29 18:38:55 +0200

    [orga] Show speaker email field when creating submission

--- src/pretalx/orga/forms/submission.py
+++ src/pretalx/orga/forms/submission.py
@@ -2,6 +2,7 @@ from django import forms
 
 from pretalx.common.forms import ReadOnlyFlag
 from pretalx.submission.models import Submission, SubmissionType
+from django.utils.translation import ugettext as _
 
 
 class SubmissionForm(ReadOnlyFlag, forms.ModelForm):
@@ -9,6 +10,11 @@ class SubmissionForm(ReadOnlyFlag, forms.ModelForm):
     def __init__(self, event, **kwargs):
         super().__init__(**kwargs)
         self.fields['submission_type'].queryset = SubmissionType.objects.filter(event=event)
+
+        if not self.instance.pk:
+            self.fields['speaker'] = forms.EmailField(
+                help_text=_('Add the email of the speaker holding the talk. They will be invited to create an account.')
+            )
 
     class Meta:
         model = Submission
