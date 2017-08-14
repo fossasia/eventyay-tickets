e494aaf1b661d53e18d9c5fedbcbe5a2c483d0f7
Tobias Kunze <rixx@cutebit.de>
2017-08-14 21:19:50 +0200

    [tests] Test correct schedule.xml format
    Should prevent issues such as #59 in the future.

---
+++ src/tests/functional/agenda/test_schedule_export.py
@@ -0,0 +1,15 @@
+import pytest
+from django.urls import reverse
+from lxml import etree
+
+
+@pytest.mark.django_db
+def test_schedule_export(slot, client, schedule_schema):
+    response = client.get(reverse(f'agenda:frab', kwargs={'event': slot.submission.event.slug}), follow=True)
+    assert response.status_code == 200
+
+    content = response.content.decode()
+    assert slot.submission.title in content
+
+    parser = etree.XMLParser(schema=schedule_schema)
+    root = etree.fromstring(response.content, parser)  # Will raise if the schedule does not match the schema
