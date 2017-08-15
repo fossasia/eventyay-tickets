e9e5e9e67179fe3a550874b7b2184b0ceccb773d
Tobias Kunze <rixx@cutebit.de>
2017-08-15 23:35:32 +0200

    [tests] Fix test url

--- src/tests/functional/agenda/test_schedule_export.py
+++ src/tests/functional/agenda/test_schedule_export.py
@@ -5,7 +5,7 @@ from lxml import etree
 
 @pytest.mark.django_db
 def test_schedule_export(slot, client, schedule_schema):
-    response = client.get(reverse(f'agenda:frab', kwargs={'event': slot.submission.event.slug}), follow=True)
+    response = client.get(reverse(f'agenda:frab-xml', kwargs={'event': slot.submission.event.slug}), follow=True)
     assert response.status_code == 200
 
     content = response.content.decode()
