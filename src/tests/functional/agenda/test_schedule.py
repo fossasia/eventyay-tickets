f491e937f3ea27f881fd7ead6d130b9cf6e5e127
Tobias Kunze <rixx@cutebit.de>
2017-08-26 16:11:54 +0200

    [tests] Add agenda view tests

---
+++ src/tests/functional/agenda/test_schedule.py
@@ -0,0 +1,25 @@
+import pytest
+
+
+@pytest.mark.django_db
+def test_can_see_schedule(client, event, slot):
+    response = client.get(event.urls.schedule, follow=True)
+    assert event.schedules.count() == 2
+    assert response.status_code == 200
+    assert slot.submission.title in response.content.decode()
+
+
+@pytest.mark.django_db
+def test_can_see_talk(client, event, slot):
+    response = client.get(slot.submission.urls.public, follow=True)
+    assert event.schedules.count() == 2
+    assert response.status_code == 200
+    assert slot.submission.title in response.content.decode()
+
+
+@pytest.mark.django_db
+def test_cannot_see_nonpublic_talk(client, event, slot):
+    event.is_public = False
+    event.save()
+    response = client.get(slot.submission.urls.public, follow=True)
+    assert response.status_code == 404
