888fbfa8d45f4885fd981e565b8719074a1c92f0
Tobias Kunze <rixx@cutebit.de>
2017-08-27 23:01:22 +0200

    [orga] get_context_data takes additional arguments

--- src/pretalx/orga/views/mails.py
+++ src/pretalx/orga/views/mails.py
@@ -155,8 +155,8 @@ class TemplateDetail(ActionFromUrl, CreateOrUpdateView):
     form_class = MailTemplateForm
     template_name = 'orga/mails/template_form.html'
 
-    def get_context_data(self):
-        ctx = super().get_context_data()
+    def get_context_data(self, *args, **kwargs):
+        ctx = super().get_context_data(*args, **kwargs)
         ctx['placeholders'] = get_context_explanation()
         return ctx
 
