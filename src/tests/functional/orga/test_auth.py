8e583b006d44f5fdcb31b82319158fa20a4241cb
luto <m@luto.at>
2017-08-28 16:27:23 +0200

    [orga] make the buttons width on the orga frontpage more consitent

--- src/tests/functional/orga/test_auth.py
+++ src/tests/functional/orga/test_auth.py
@@ -8,4 +8,4 @@ def test_orga_successful_login(client, user):
     user.save()
     response = client.post(reverse('orga:login'), data={'username': user.nick, 'password': 'testtest'}, follow=True)
     assert response.status_code == 200
-    assert 'Uh, a new event? Head over here, please!' in response.content.decode()
+    assert 'Uh, a new event?' in response.content.decode()
