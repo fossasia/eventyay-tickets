deed27650aae7c6316d4e1d916930798760d8ee8
Tobias Kunze <rixx@cutebit.de>
2017-08-30 00:28:16 +0200

    Feed pylama

--- src/tests/functional/orga/test_frab_import.py
+++ src/tests/functional/orga/test_frab_import.py
@@ -2,9 +2,7 @@ import pytest
 from django.core.management import call_command
 
 from pretalx.event.models import Event
-from pretalx.person.models import EventPermission, SpeakerProfile, User
 from pretalx.schedule.models import Room, TalkSlot
-from pretalx.submission.models import Submission
 
 
 @pytest.mark.django_db
@@ -16,7 +14,7 @@ def test_frab_import_minimal():
 
     assert TalkSlot.objects.count() == 2
     assert TalkSlot.objects.all()[0].schedule.version == '1.99b 🍕'
-    assert TalkSlot.objects.all()[1].schedule.version == None
+    assert TalkSlot.objects.all()[1].schedule.version is None
 
     assert Event.objects.count() == 1
     assert Event.objects.all()[0].name == 'PrivacyWeek 2016'
