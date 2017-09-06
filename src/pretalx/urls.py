e746ebc245c82c7e1502ce0a24847ab4985fb6f3
Tobias Kunze <rixx@cutebit.de>
2017-09-06 09:00:35 +0200

    Load custom css in templates

--- src/pretalx/urls.py
+++ src/pretalx/urls.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.conf.urls import include, url
+from django.conf.urls.static import static
 
 from .agenda.urls import agenda_urls
 from .cfp.urls import cfp_urls
@@ -16,3 +17,4 @@ if settings.DEBUG:
     urlpatterns += [
         url(r'^__debug__/', include(debug_toolbar.urls)),
     ]
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
