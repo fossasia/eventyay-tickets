a4de99e1edddf1219d0677272145415d7d038f5e
luto <m@luto.at>
2017-09-25 01:32:50 +0200

    [schedule] delete Availability when associated Event, Person or Room is deleted

--- src/pretalx/schedule/models/availability.py
+++ src/pretalx/schedule/models/availability.py
@@ -7,18 +7,18 @@ class Availability(LogMixin, models.Model):
     event = models.ForeignKey(
         to='event.Event',
         related_name='availabilities',
-        on_delete=models.PROTECT,
+        on_delete=models.CASCADE,
     )
     person = models.ForeignKey(
         to='person.User',
         related_name='availabilities',
-        on_delete=models.PROTECT,
+        on_delete=models.CASCADE,
         null=True, blank=True,
     )
     room = models.ForeignKey(
         to='schedule.Room',
         related_name='availabilities',
-        on_delete=models.PROTECT,
+        on_delete=models.CASCADE,
         null=True, blank=True,
     )
     start = models.DateTimeField()
