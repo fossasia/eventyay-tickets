f652b38d1effdc063f37dab65ddf8ed9069513e0
Tobias Kunze <rixx@cutebit.de>
2017-09-25 20:20:34 +0200

    [orga] Get started on a dashboard
    refs #96

--- src/pretalx/orga/views/dashboard.py
+++ src/pretalx/orga/views/dashboard.py
@@ -1,16 +1,17 @@
-from django.shortcuts import redirect
 from django.views.generic import TemplateView
+
+from pretalx.event.stages import get_stages
 
 
 class DashboardView(TemplateView):
     template_name = 'orga/dashboard.html'
 
-    def dispatch(self, request):
-        if request.user.is_anonymous:
-            return redirect('orga:login')
-
-        return super().dispatch(request)
-
 
 class EventDashboardView(TemplateView):
     template_name = 'orga/event/dashboard.html'
+
+    def get_context_data(self, event):
+        ctx = super().get_context_data()
+        event = self.request.event
+        ctx['timeline'] = get_stages(event)
+        return ctx
