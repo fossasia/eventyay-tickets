344ac7062f7dae3ec1e5b9a4b488dda1c99d52af
Tobias Kunze <rixx@cutebit.de>
2017-09-25 19:53:29 +0200

    [event] Track event stages, closing #109

---
+++ src/pretalx/event/stages.py
@@ -0,0 +1,73 @@
+import copy
+
+from django.utils.timezone import now
+from django.utils.translation import ugettext_lazy as _
+
+
+def _is_in_preparation(event):
+    return not event.is_public
+
+
+def _is_cfp_open(event):
+    return not _is_in_preparation(event) and not event.cfp.deadline > now()
+
+
+def _is_in_review(event):
+    return not _is_cfp_open(event) and event.schedules.count() <= 1
+
+
+def _is_schedule_released(event):
+    return not _is_in_review(event) and now() < event.datetime_from
+
+
+def _is_running(event):
+    return event.datetime_from <= now() <= event.datetime_to
+
+
+def _is_in_wrapup(event):
+    return event.datetime_to >= now()
+
+
+STAGES = {
+    'PREPARATION': {
+        'name': _('Preparation'),
+        'method': _is_in_preparation,
+    },
+    'CFP_OPEN': {
+        'name': _('CfP is open'),
+        'method': _is_cfp_open,
+    },
+    'REVIEW': {
+        'name': _('Review'),
+        'method': _is_in_review,
+    },
+    'SCHEDULE': {
+        'name': _('Schedule'),
+        'method': _is_schedule_released,
+    },
+    'EVENT': {
+        'name': _('Event'),
+        'method': _is_running,
+    },
+    'WRAPUP': {
+        'name': _('Wrapup'),
+        'method': _is_in_wrapup,
+    },
+}
+STAGE_ORDER = ['PREPARATION', 'CFP_OPEN', 'REVIEW', 'SCHEDULE', 'EVENT', 'WRAPUP']
+
+
+def in_stage(event, stage):
+    return STAGES[stage]['method'](event)
+
+
+def get_stages(event):
+    inactive_state = 'done'
+    stages = copy.deepcopy(STAGES)
+
+    for stage in STAGES:
+        is_stage_active = in_stage(event, stage)
+        if is_stage_active:
+            inactive_state = 'next'
+        stages[stage]['state'] = 'active' if is_stage_active else inactive_state
+    return stages
