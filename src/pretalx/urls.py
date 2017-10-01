7dc0d750d37f9dcae3309390eb3ff8b05eafd175
Tobias Kunze <rixx@cutebit.de>
2017-10-01 21:53:29 +0200

    Don't assume that DDT is installed in debug mode

--- src/pretalx/urls.py
+++ src/pretalx/urls.py
@@ -1,3 +1,5 @@
+from contextlib import suppress
+
 from django.conf import settings
 from django.conf.urls import include, url
 from django.conf.urls.static import static
@@ -13,8 +15,9 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
-    import debug_toolbar
-    urlpatterns += [
-        url(r'^__debug__/', include(debug_toolbar.urls)),
-    ]
+    with suppress(ImportError):
+        import debug_toolbar
+        urlpatterns += [
+            url(r'^__debug__/', include(debug_toolbar.urls)),
+        ]
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
