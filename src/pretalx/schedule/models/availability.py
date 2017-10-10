cef3e7ea4ef024b20337cdff165ff815f7e95689
luto <m@luto.at>
2017-10-10 11:41:20 +0200

    [schedule] add set theory operations for lists of Availabilities

--- src/pretalx/schedule/models/availability.py
+++ src/pretalx/schedule/models/availability.py
@@ -35,3 +35,106 @@ class Availability(LogMixin, models.Model):
             # make sure all-day availabilities are displayed properly in fullcalendar
             'allDay': (self.start.time() == zerotime and self.end.time() == zerotime)
         }
+
+    def overlaps(self, other, strict):
+        """ test if two Availabilities overlap or are directly adjacent to eachother, if not in strict mode """
+
+        if not isinstance(other, Availability):
+            raise Exception('Please provide an Availability object')
+
+        if strict:
+            return (self.start <= other.start < self.end) or \
+                   (self.start < other.end <= self.end) or \
+                   (other.start <= self.start < other.end) or \
+                   (other.start < self.end <= other.end)
+        else:
+            return (self.start <= other.start <= self.end) or \
+                   (self.start <= other.end <= self.end) or \
+                   (other.start <= self.start <= other.end) or \
+                   (other.start <= self.end <= other.end)
+
+    def merge_with(self, other):
+        """ return a new Availability, which spans the whole range of this one and the given one """
+
+        if not isinstance(other, Availability):
+            raise Exception('Please provide an Availability object')
+        if not other.overlaps(self, False):
+            raise Exception('Only overlapping Availabilities can be merged.')
+
+        return Availability(
+            start=min(self.start, other.start),
+            end=max(self.end, other.end),
+        )
+
+    def intersect_with(self, other):
+        """ return a new Availability, which spans the range covered by this one and the given one """
+
+        if not isinstance(other, Availability):
+            raise Exception('Please provide an Availability object')
+        if not other.overlaps(self, False):
+            raise Exception('Only overlapping Availabilities can be intersected.')
+
+        return Availability(
+            start=max(self.start, other.start),
+            end=min(self.end, other.end),
+        )
+
+    @classmethod
+    def union(cls, availabilities):
+        """ return the minimal, unique list of Availabilities, which are covered by at least one given Availability """
+        if not availabilities:
+            return []
+
+        availabilities = sorted(availabilities, key=lambda a: a.start)
+        result = [availabilities[0]]
+        availabilities = availabilities[1:]
+
+        for avail in availabilities:
+            if avail.overlaps(result[-1], False):
+                result[-1] = result[-1].merge_with(avail)
+            else:
+                result.append(avail)
+
+        return result
+
+    @classmethod
+    def _pair_intersection(cls, availabilities_a, availabilities_b):
+        """ return the list of Availabilities, which are covered by each of the given sets """
+        if not availabilities_a or not availabilities_b:
+            return []
+
+        result = []
+
+        # yay for O(b*a) time! I am sure there is some fancy trick to make this faster,
+        # but we're dealing with less than 100 items in total, sooo.. ¯\_(ツ)_/¯
+        for a in availabilities_a:
+            for b in availabilities_b:
+                if a.overlaps(b, True):
+                    result.append(a.intersect_with(b))
+
+        return result
+
+    @classmethod
+    def intersection(cls, *availabilitysets):
+        """ return the list of Availabilities, which are covered by all of the given sets """
+
+        # get rid of any overlaps and unmerged ranges in each set
+        availabilitysets = [
+            cls.union(avialset)
+            for avialset in availabilitysets
+        ]
+
+        # bail out for obvious cases (there are no sets given, one of the sets is empty)
+        if not availabilitysets:
+            return []
+        if not all(availabilitysets):
+            return []
+
+        # start with the very first set ...
+        result = availabilitysets[0]
+
+        for availset in availabilitysets[1:]:
+            # ... subtract each of the other sets
+            result = cls._pair_intersection(result, availset)
+
+        return result
---
+++ src/tests/unit/schedule/test_availability.py
@@ -0,0 +1,325 @@
+import datetime
+
+import pytest
+
+from pretalx.schedule.models import Availability
+
+
+@pytest.mark.django_db
+@pytest.mark.parametrize('one,two,expected_strict,expected', (
+    (
+        #    0000
+        #    0000
+        ((2017, 1, 1, 5), (2017, 1, 1, 9)),
+        ((2017, 1, 1, 5), (2017, 1, 1, 9)),
+        True, True,
+    ),
+    (
+        #    0000
+        #     000
+        ((2017, 1, 1, 5), (2017, 1, 1, 9)),
+        ((2017, 1, 1, 6), (2017, 1, 1, 9)),
+        True, True,
+    ),
+    (
+        #    0000
+        #      000
+        ((2017, 1, 1, 5), (2017, 1, 1, 9)),
+        ((2017, 1, 1, 7), (2017, 1, 1, 10)),
+        True, True,
+    ),
+    (
+        #    0000
+        #    000
+        ((2017, 1, 1, 5), (2017, 1, 1, 9)),
+        ((2017, 1, 1, 5), (2017, 1, 1, 8)),
+        True, True,
+    ),
+    (
+        #    0000
+        #   000
+        ((2017, 1, 1, 5), (2017, 1, 1, 9)),
+        ((2017, 1, 1, 4), (2017, 1, 1, 7)),
+        True, True,
+    ),
+    (
+        #    0000
+        # 000
+        ((2017, 1, 1, 5), (2017, 1, 1, 9)),
+        ((2017, 1, 1, 1), (2017, 1, 1, 5)),
+        False, True,
+    ),
+    (
+        #    0000
+        #        000
+        ((2017, 1, 1, 5), (2017, 1, 1, 9)),
+        ((2017, 1, 1, 9), (2017, 1, 1, 11)),
+        False, True,
+    ),
+    (
+        #    0000
+        # 00
+        ((2017, 1, 1, 5), (2017, 1, 1, 9)),
+        ((2017, 1, 1, 1), (2017, 1, 1, 4)),
+        False, False,
+    ),
+    (
+        #    0000
+        #         000
+        ((2017, 1, 1, 5), (2017, 1, 1, 9)),
+        ((2017, 1, 1, 10), (2017, 1, 1, 11)),
+        False, False,
+    ),
+    (
+        #   0000000000
+        #    00000000
+        ((2017, 10, 5, 9), (2017, 10, 5, 16)),
+        ((2017, 10, 5, 10), (2017, 10, 5, 15)),
+        True, True,
+    ),
+))
+def test_overlaps(one, two, expected_strict, expected):
+    one = Availability(start=datetime.datetime(*one[0]), end=datetime.datetime(*one[1]))
+    two = Availability(start=datetime.datetime(*two[0]), end=datetime.datetime(*two[1]))
+
+    def test(strict, expected):
+        nonlocal one, two
+        actual1 = one.overlaps(two, strict)
+        actual2 = two.overlaps(one, strict)
+        assert expected == actual1
+        assert expected == actual2
+
+    test(True, expected_strict)
+    test(False, expected)
+
+
+@pytest.mark.django_db
+@pytest.mark.parametrize('one,two,expected', (
+    (
+        # real overlap
+        Availability(start=datetime.datetime(2017, 1, 1, 4), end=datetime.datetime(2017, 1, 1, 7)),
+        Availability(start=datetime.datetime(2017, 1, 1, 5), end=datetime.datetime(2017, 1, 1, 9)),
+        Availability(start=datetime.datetime(2017, 1, 1, 4), end=datetime.datetime(2017, 1, 1, 9)),
+    ),
+    (
+        # just adjacent
+        Availability(start=datetime.datetime(2017, 1, 1, 4), end=datetime.datetime(2017, 1, 1, 7)),
+        Availability(start=datetime.datetime(2017, 1, 1, 7), end=datetime.datetime(2017, 1, 1, 8)),
+        Availability(start=datetime.datetime(2017, 1, 1, 4), end=datetime.datetime(2017, 1, 1, 8)),
+    ),
+))
+def test_merge_with(one, two, expected):
+    actual1 = one.merge_with(two)
+    actual2 = two.merge_with(one)
+    assert expected.start == actual1.start
+    assert expected.end == actual1.end
+    assert expected.start == actual2.start
+    assert expected.end == actual2.end
+
+
+@pytest.mark.parametrize('method,args,expected', (
+    (Availability.overlaps, ['i_am_no_availability', False], 'Availability object'),
+    (Availability.merge_with, ['i_am_no_availability'], 'Availability object'),
+    (Availability.merge_with, [Availability(start=datetime.datetime(2017, 1, 2), end=datetime.datetime(2017, 1, 2, 1))], 'overlap'),
+    (Availability.intersect_with, ['i_am_no_availability'], 'Availability object'),
+    (Availability.intersect_with, [Availability(start=datetime.datetime(2017, 1, 2), end=datetime.datetime(2017, 1, 2, 1))], 'overlap'),
+))
+def test_availability_fail(method, args, expected):
+    avail = Availability(start=datetime.datetime(2017, 1, 1), end=datetime.datetime(2017, 1, 1, 1))
+
+    with pytest.raises(Exception) as excinfo:
+        method(avail, *args)
+
+    assert expected in str(excinfo)
+
+
+@pytest.mark.django_db
+@pytest.mark.parametrize('avails,expected', (
+    (
+        [],
+        [],
+    ),
+    (
+        [Availability(start=datetime.datetime(2017, 1, 1, 4), end=datetime.datetime(2017, 1, 1, 7))],
+        [Availability(start=datetime.datetime(2017, 1, 1, 4), end=datetime.datetime(2017, 1, 1, 7))],
+    ),
+    (
+        [
+            # 2 is after one 1, but with a gap. Do nothing.
+            Availability(start=datetime.datetime(2017, 1, 1, 4), end=datetime.datetime(2017, 1, 1, 5)),
+            Availability(start=datetime.datetime(2017, 1, 1, 6), end=datetime.datetime(2017, 1, 1, 7)),
+        ],
+        [
+            Availability(start=datetime.datetime(2017, 1, 1, 4), end=datetime.datetime(2017, 1, 1, 5)),
+            Availability(start=datetime.datetime(2017, 1, 1, 6), end=datetime.datetime(2017, 1, 1, 7)),
+        ],
+    ),
+    (
+        [
+            # 2 is directly after one 1. Merge them.
+            Availability(start=datetime.datetime(2017, 1, 1, 4), end=datetime.datetime(2017, 1, 1, 5)),
+            Availability(start=datetime.datetime(2017, 1, 1, 5), end=datetime.datetime(2017, 1, 1, 6)),
+        ],
+        [
+            Availability(start=datetime.datetime(2017, 1, 1, 4), end=datetime.datetime(2017, 1, 1, 6)),
+        ],
+    ),
+    (
+        [
+            # 2 partly overlaps with 1. Merge them.
+            Availability(start=datetime.datetime(2017, 1, 1, 4), end=datetime.datetime(2017, 1, 1, 6)),
+            Availability(start=datetime.datetime(2017, 1, 1, 5), end=datetime.datetime(2017, 1, 1, 7)),
+        ],
+        [
+            Availability(start=datetime.datetime(2017, 1, 1, 4), end=datetime.datetime(2017, 1, 1, 7)),
+        ],
+    ),
+    (
+        [
+            # 2 partly overlaps with 1. Merge them.
+            Availability(start=datetime.datetime(2017, 1, 1, 5), end=datetime.datetime(2017, 1, 1, 7)),
+            Availability(start=datetime.datetime(2017, 1, 1, 4), end=datetime.datetime(2017, 1, 1, 6)),
+        ],
+        [
+            Availability(start=datetime.datetime(2017, 1, 1, 4), end=datetime.datetime(2017, 1, 1, 7)),
+        ],
+    ),
+    (
+        [
+            Availability(start=datetime.datetime(2017, 1, 1, 10), end=datetime.datetime(2017, 1, 1, 12)),
+            Availability(start=datetime.datetime(2017, 1, 1, 12), end=datetime.datetime(2017, 1, 1, 14)),
+            Availability(start=datetime.datetime(2017, 1, 1, 5), end=datetime.datetime(2017, 1, 1, 7)),
+            Availability(start=datetime.datetime(2017, 1, 1, 6), end=datetime.datetime(2017, 1, 1, 8)),
+            Availability(start=datetime.datetime(2017, 1, 1, 18), end=datetime.datetime(2017, 1, 1, 19)),
+            Availability(start=datetime.datetime(2017, 1, 1, 4), end=datetime.datetime(2017, 1, 1, 6)),
+        ],
+        [
+            Availability(start=datetime.datetime(2017, 1, 1, 4), end=datetime.datetime(2017, 1, 1, 8)),
+            Availability(start=datetime.datetime(2017, 1, 1, 10), end=datetime.datetime(2017, 1, 1, 14)),
+            Availability(start=datetime.datetime(2017, 1, 1, 18), end=datetime.datetime(2017, 1, 1, 19)),
+        ],
+    ),
+))
+def test_union(avails, expected):
+    actual = Availability.union(avails)
+
+    assert len(actual) == len(expected)
+
+    for act, exp in zip(actual, expected):
+        assert act.start == exp.start
+        assert act.end == exp.end
+
+
+@pytest.mark.django_db
+@pytest.mark.parametrize('availsets,expected', (
+    (
+        [],
+        [],
+    ),
+    (
+        [
+            [Availability(start=datetime.datetime(2017, 1, 1, 5), end=datetime.datetime(2017, 1, 1, 7))],
+            [],
+        ],
+        [],
+    ),
+    (
+        #    0000
+        #        0000
+        # ==============
+        #
+        [
+            [Availability(start=datetime.datetime(2017, 1, 1, 5), end=datetime.datetime(2017, 1, 1, 7))],
+            [Availability(start=datetime.datetime(2017, 1, 1, 7), end=datetime.datetime(2017, 1, 1, 9))],
+        ],
+        [],
+    ),
+    (
+        #    0000000
+        #    0000000
+        # ==============
+        #    0000000
+        [
+            [Availability(start=datetime.datetime(2017, 1, 1, 5), end=datetime.datetime(2017, 1, 1, 7))],
+            [Availability(start=datetime.datetime(2017, 1, 1, 5), end=datetime.datetime(2017, 1, 1, 7))],
+        ],
+        [Availability(start=datetime.datetime(2017, 1, 1, 5), end=datetime.datetime(2017, 1, 1, 7))],
+    ),
+    (
+        #    0000000
+        #       000000
+        # ==============
+        #       0000
+        [
+            [Availability(start=datetime.datetime(2017, 1, 1, 5), end=datetime.datetime(2017, 1, 1, 7))],
+            [Availability(start=datetime.datetime(2017, 1, 1, 6), end=datetime.datetime(2017, 1, 1, 9))],
+        ],
+        [Availability(start=datetime.datetime(2017, 1, 1, 6), end=datetime.datetime(2017, 1, 1, 7))],
+    ),
+    (
+        #    0000000
+        #  0000   00000
+        # ==============
+        #    00   00
+        [
+            [Availability(start=datetime.datetime(2017, 1, 1, 2), end=datetime.datetime(2017, 1, 1, 7))],
+            [
+                Availability(start=datetime.datetime(2017, 1, 1, 0), end=datetime.datetime(2017, 1, 1, 3)),
+                Availability(start=datetime.datetime(2017, 1, 1, 6), end=datetime.datetime(2017, 1, 1, 8)),
+            ],
+        ],
+        [
+            Availability(start=datetime.datetime(2017, 1, 1, 2), end=datetime.datetime(2017, 1, 1, 3)),
+            Availability(start=datetime.datetime(2017, 1, 1, 6), end=datetime.datetime(2017, 1, 1, 7)),
+        ],
+    ),
+    (
+        #    0000000
+        #  0000000
+        # ==============
+        #    00000
+        [
+            [Availability(start=datetime.datetime(2017, 1, 1, 2), end=datetime.datetime(2017, 1, 1, 7))],
+            [
+                Availability(start=datetime.datetime(2017, 1, 1, 0), end=datetime.datetime(2017, 1, 1, 3)),
+                Availability(start=datetime.datetime(2017, 1, 1, 3), end=datetime.datetime(2017, 1, 1, 4)),
+            ],
+        ],
+        [
+            Availability(start=datetime.datetime(2017, 1, 1, 2), end=datetime.datetime(2017, 1, 1, 4)),
+        ],
+    ),
+    (
+        #    0000000      00000     0000
+        #  0000   00000           000
+        # ================================
+        #    00   00              0
+        [
+            [
+                Availability(start=datetime.datetime(2017, 1, 1, 2), end=datetime.datetime(2017, 1, 1, 7)),
+                Availability(start=datetime.datetime(2017, 1, 1, 10), end=datetime.datetime(2017, 1, 1, 12)),
+                Availability(start=datetime.datetime(2017, 1, 1, 14), end=datetime.datetime(2017, 1, 1, 19)),
+            ],
+            [
+                Availability(start=datetime.datetime(2017, 1, 1, 0), end=datetime.datetime(2017, 1, 1, 3)),
+                Availability(start=datetime.datetime(2017, 1, 1, 6), end=datetime.datetime(2017, 1, 1, 8)),
+                Availability(start=datetime.datetime(2017, 1, 1, 13), end=datetime.datetime(2017, 1, 1, 15)),
+            ],
+        ],
+        [
+            Availability(start=datetime.datetime(2017, 1, 1, 2), end=datetime.datetime(2017, 1, 1, 3)),
+            Availability(start=datetime.datetime(2017, 1, 1, 6), end=datetime.datetime(2017, 1, 1, 7)),
+            Availability(start=datetime.datetime(2017, 1, 1, 14), end=datetime.datetime(2017, 1, 1, 15)),
+        ],
+    ),
+))
+def test_intersection(availsets, expected):
+    actual1 = Availability.intersection(*availsets)
+    actual2 = Availability.intersection(*reversed(availsets))
+
+    assert len(actual1) == len(expected)
+    assert len(actual2) == len(expected)
+
+    for act1, act2, exp in zip(actual1, actual2, expected):
+        assert act1.start == act2.start == exp.start
+        assert act1.end == act2.end == exp.end
