cdef965154e58f128af6f3934b9743447ebd8abc
Tobias Kunze <rixx@cutebit.de>
2017-10-12 19:45:57 +0200

    [tests] Test speaker view

---
+++ src/tests/functional/orga/test_speaker.py
@@ -0,0 +1,21 @@
+import pytest
+from django.urls import reverse
+
+
+@pytest.mark.django_db
+def test_orga_can_access_speaker_page(orga_client, speaker, event, submission):
+    response = orga_client.get(reverse('orga:speakers.view', kwargs={'event': event.slug, 'pk': speaker.pk}), follow=True)
+    assert response.status_code == 200
+    assert speaker.name in response.content.decode()
+
+
+@pytest.mark.django_db
+def test_orga_can_edit_speaker(orga_client, speaker, event, submission):
+    response = orga_client.post(
+        reverse('orga:speakers.edit', kwargs={'event': event.slug, 'pk': speaker.pk}),
+        data={'name': 'BESTSPEAKAR', 'biography': 'I rule!'},
+        follow=True,
+    )
+    assert response.status_code == 200
+    speaker.refresh_from_db()
+    assert speaker.name == 'BESTSPEAKAR', response.content.decode()
