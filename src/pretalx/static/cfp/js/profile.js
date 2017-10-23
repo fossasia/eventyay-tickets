46d31e3f5f721fdd144a1cf5df55736ab2fcfd5a
Tobias Kunze <rixx@cutebit.de>
2017-10-23 16:04:49 +0200

    [cfp] Preview gravatar when checking option box

--- src/pretalx/static/cfp/js/profile.js
+++ src/pretalx/static/cfp/js/profile.js
@@ -1,8 +1,10 @@
 $('#id_get_gravatar').click(() => {
     if ($('#id_get_gravatar').is(':checked')) {
         $('.user-avatar-display').slideUp();
+        $('.avatar-form img').attr('src', 'https://www.gravatar.com/avatar/' + $('.avatar-form img').attr('data-gravatar'));
     } else {
         $('.user-avatar-display').slideDown();
+        $('.avatar-form img').attr('src', $('.avatar-form img').attr('data-avatar'));
     }
 })
 if ($('#id_get_gravatar').is(':checked')) {
