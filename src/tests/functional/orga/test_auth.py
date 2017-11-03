343a1422babf96084180a3260250b57516e00c5b
Tobias Kunze <rixx@cutebit.de>
2017-11-03 21:15:28 +0100

    [tests] Test redirect for login with queryparams

--- src/tests/functional/orga/test_auth.py
+++ src/tests/functional/orga/test_auth.py
@@ -9,3 +9,17 @@ def test_orga_successful_login(client, user):
     response = client.post(reverse('orga:login'), data={'username': user.nick, 'password': 'testtest'}, follow=True)
     assert response.status_code == 200
     assert 'Uh, a new event?' in response.content.decode()
+
+
+@pytest.mark.django_db
+def test_orga_redirect_login(client, orga_user, event):
+    queryparams = 'foo=bar&something=else'
+    request_url = event.orga_urls.base + '/?' + queryparams
+    response = client.get(request_url, follow=True)
+    assert response.status_code == 200
+    assert response.redirect_chain[-1] == (f'/orga/login/?next={event.orga_urls.base}/&{queryparams}', 302)
+
+    response = client.post(response.redirect_chain[-1][0], data={'username': orga_user.nick, 'password': 'orgapassw0rd'}, follow=True)
+    assert response.status_code == 200
+    assert event.name in response.content.decode()
+    assert response.redirect_chain[-1][0] == request_url
