f024648cfec78ae2f23b71f2f2e69668b52c825d
Tobias Kunze <rixx@cutebit.de>
2017-11-05 12:50:56 +0100

    [common] Steal, erm, *borrow* pretix' rebuild command.
    Thanks, @raphaelm!

---
+++ src/pretalx/common/management/commands/rebuild.py
@@ -0,0 +1,11 @@
+from django.core.management import call_command
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    help = "Rebuild static files and language files"
+
+    def handle(self, *args, **options):
+        call_command('compilemessages', verbosity=1, interactive=False)
+        call_command('collectstatic', verbosity=1, interactive=False)
+        call_command('compress', verbosity=1, interactive=False)
