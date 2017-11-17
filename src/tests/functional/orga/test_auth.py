9856dafaf147677a5faac561bc6b60746b747ef2
Tobias Kunze <rixx@cutebit.de>
2017-11-17 23:21:53 +0100

    [tests] Add test for invite acceptance

--- src/tests/functional/orga/test_auth.py
+++ src/tests/functional/orga/test_auth.py
@@ -22,3 +22,34 @@ def test_orga_redirect_login(client, orga_user, event):
     assert response.status_code == 200
     assert event.name in response.content.decode()
     assert response.redirect_chain[-1][0] == request_url
+
+
+@pytest.mark.django_db
+def test_orga_accept_invitation_once(client, event, invitation):
+    assert invitation.user is None
+    response = client.post(
+        reverse('orga:invitation.view', kwargs={'code': invitation.invitation_token}),
+        {
+            'register_username': 'newuser',
+            'register_email': invitation.invitation_email,
+            'register_password': 'f00baar!',
+            'register_password_repeat': 'f00baar!',
+        },
+        follow=True,
+    )
+    assert response.status_code == 200
+    invitation.refresh_from_db()
+    assert invitation.user.nick == 'newuser'
+    response = client.post(
+        reverse('orga:invitation.view', kwargs={'code': invitation.invitation_token}),
+        {
+            'register_username': 'evilnewuser',
+            'register_email': invitation.invitation_email + '.evil',
+            'register_password': 'f00baar!',
+            'register_password_repeat': 'f00baar!',
+        },
+        follow=True,
+    )
+    assert response.status_code == 404
+    invitation.refresh_from_db()
+    assert invitation.user.nick == 'newuser'
