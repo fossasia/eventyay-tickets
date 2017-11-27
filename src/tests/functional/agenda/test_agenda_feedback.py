4d2ab9ca58995daee41f9ab508ca9034a6381f5d
Tobias Kunze <rixx@cutebit.de>
2017-11-27 20:30:54 +0100

    [tests.agenda] Make sure feedback creation is safe

--- src/tests/functional/agenda/test_agenda_feedback.py
+++ src/tests/functional/agenda/test_agenda_feedback.py
@@ -11,8 +11,28 @@ def test_can_create_feedback(past_slot, client):
 
 
 @pytest.mark.django_db()
+def test_cannot_create_feedback_before_talk(slot, client):
+    response = client.post(slot.submission.urls.feedback, {'review': 'cool!'}, follow=True)
+    assert slot.submission.speakers.count() == 1
+    assert response.status_code == 404
+    assert slot.submission.feedback.count() == 0
+
+
+@pytest.mark.django_db()
 def test_can_see_feedback(feedback, client):
     client.force_login(feedback.talk.speakers.first())
     response = client.get(feedback.talk.urls.feedback)
     assert response.status_code == 200
     assert feedback.review in response.content.decode()
+
+
+@pytest.mark.django_db()
+def test_can_see_feedback_form(past_slot, client):
+    response = client.get(past_slot.submission.urls.feedback, follow=True)
+    assert response.status_code == 200
+
+
+@pytest.mark.django_db()
+def test_cannot_see_feedback_form_before_talk(slot, client):
+    response = client.get(slot.submission.urls.feedback, follow=True)
+    assert response.status_code == 404
