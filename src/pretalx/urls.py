372916d7df5fa98291fc0c0e92cffc1c1daf2578
Tobias Kunze <rixx@cutebit.de>
2017-12-02 22:44:46 +0100

    Style fix

--- src/pretalx/urls.py
+++ src/pretalx/urls.py
@@ -4,7 +4,6 @@ from django.conf import settings
 from django.conf.urls import include, url
 from django.conf.urls.static import static
 
-
 urlpatterns = [
     url(r'^orga/', include('pretalx.orga.urls', namespace='orga')),
     url(r'^api/', include('pretalx.api.urls', namespace='api')),
