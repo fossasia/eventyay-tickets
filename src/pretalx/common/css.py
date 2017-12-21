e9eb4a5e5d248ed7931c7e365c8ef2146888ac8e
Tobias Kunze <rixx@cutebit.de>
2017-12-21 22:25:39 +0100

    [common] Allow min-width and max-width in custom css

--- src/pretalx/common/css.py
+++ src/pretalx/common/css.py
@@ -15,14 +15,15 @@ acceptable_css_keywords = set([
 valid_css_values = re.compile('^(#[0-9a-f]+|rgb\(\d+%?,\d*%?,?\d*%?\)?|\d{0,2}\.?\d{0,2}(cm|em|ex|in|mm|pc|pt|px|%|,|\))?)$')
 acceptable_css_properties = set([
     'azimuth', 'background-color', 'border-bottom-color', 'border-collapse',
-    'border-color', 'border-left-color', 'border-right-color', 'border-top-color',
-    'clear', 'color', 'cursor', 'direction', 'display', 'elevation', 'float', 'font',
-    'font-family', 'font-size', 'font-style', 'font-variant', 'font-weight', 'height',
-    'letter-spacing', 'line-height', 'overflow', 'pause', 'pause-after', 'pause-before',
-    'pitch', 'pitch-range', 'richness', 'speak', 'speak-header', 'speak-numeral',
-    'speak-punctuation', 'speech-rate', 'stress', 'text-align', 'text-decoration',
-    'text-indent', 'unicode-bidi', 'vertical-align', 'voice-family', 'volume',
-    'white-space', 'width',
+    'border-color', 'border-left-color', 'border-right-color',
+    'border-top-color', 'clear', 'color', 'cursor', 'direction', 'display',
+    'elevation', 'float', 'font', 'font-family', 'font-size', 'font-style',
+    'font-variant', 'font-weight', 'height', 'letter-spacing', 'line-height',
+    'max-width', 'min-width', 'overflow', 'pause', 'pause-after',
+    'pause-before', 'pitch', 'pitch-range', 'richness', 'speak',
+    'speak-header', 'speak-numeral', 'speak-punctuation', 'speech-rate',
+    'stress', 'text-align', 'text-decoration', 'text-indent', 'unicode-bidi',
+    'vertical-align', 'voice-family', 'volume', 'white-space', 'width',
 ])
 
 
