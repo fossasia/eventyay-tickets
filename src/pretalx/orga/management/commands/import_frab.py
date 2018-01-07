462bff79a785a4e1609f608e32b54056aaac1089
Tobias Kunze <rixx@cutebit.de>
2018-01-07 16:27:15 +0100

    [orga] Fix issue in frab import

--- src/pretalx/orga/management/commands/import_frab.py
+++ src/pretalx/orga/management/commands/import_frab.py
@@ -90,7 +90,7 @@ class Command(BaseCommand):
         sub.title = talk.find('title').text
         sub.description = talk.find('description').text
         if talk.find('subtitle').text.strip():
-            sub.description = talk.find('subtitle').text + '\n' + sub.description
+            sub.description = talk.find('subtitle').text + '\n' + sub.description or ''
         sub.abstract = talk.find('abstract').text
         sub.content_locale = talk.find('language').text or 'en'
         sub.do_not_record = optout
