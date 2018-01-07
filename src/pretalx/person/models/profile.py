53a2e9a6a9feae9adb7f76043082f4b49fc8bd20
luto <m@luto.at>
2018-01-07 17:04:55 +0100

    [agenda] rename speakers talks.ical to .ics

--- src/pretalx/person/models/profile.py
+++ src/pretalx/person/models/profile.py
@@ -30,7 +30,7 @@ class SpeakerProfile(LogMixin, models.Model):
 
     class urls(EventUrls):
         public = '{self.event.urls.base}/speaker/{self.user.code}'
-        talks_ical = '{self.urls.public}/talks.ical'
+        talks_ical = '{self.urls.public}/talks.ics'
 
     @cached_property
     def code(self):
