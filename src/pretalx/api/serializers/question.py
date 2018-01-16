47fc872ea28e9406492bad0ccc73d423bb95689a
Tobias Kunze <rixx@cutebit.de>
2018-01-16 21:46:50 +0100

    [api] Serialize answer options properly

--- src/pretalx/api/serializers/question.py
+++ src/pretalx/api/serializers/question.py
@@ -1,10 +1,18 @@
 from rest_framework.serializers import ModelSerializer, SlugRelatedField
 
 from pretalx.person.models import User
-from pretalx.submission.models import Answer, Question, Submission
+from pretalx.submission.models import Answer, AnswerOption, Question, Submission
+
+
+class AnswerOptionSerializer(ModelSerializer):
+
+    class Meta:
+        model = AnswerOption
+        fields = ('id', 'answer')
 
 
 class QuestionSerializer(ModelSerializer):
+    options = AnswerOptionSerializer(many=True)
 
     class Meta:
         model = Question
@@ -15,6 +23,7 @@ class AnswerSerializer(ModelSerializer):
     question = QuestionSerializer(Question.objects.all(), read_only=True)
     submission = SlugRelatedField(queryset=Submission.objects.all(), slug_field='code')
     person = SlugRelatedField(queryset=User.objects.all(), slug_field='code')
+    options = AnswerOptionSerializer(many=True)
 
     class Meta:
         model = Answer
