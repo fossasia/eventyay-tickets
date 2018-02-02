e0b7fe44a8489724e0a1711ecde757a3f3236478
Tobias Kunze <rixx@cutebit.de>
2018-02-02 19:20:24 +0100

    [submission] Only allow the current, and still open submission types

--- src/pretalx/submission/forms/submission.py
+++ src/pretalx/submission/forms/submission.py
@@ -1,5 +1,6 @@
 from django import forms
 from django.conf import settings
+from django.utils.timezone import now
 
 from pretalx.submission.models import Submission, SubmissionType
 
@@ -13,7 +14,18 @@ class InfoForm(forms.ModelForm):
         super().__init__(**kwargs)
         instance = kwargs.get('instance')
         self.fields['submission_type'].queryset = SubmissionType.objects.filter(event=self.event)
-        self.initial['submission_type'] = instance.submission_type if instance else self.event.cfp.default_type
+        self.initial['submission_type'] = getattr(instance, 'submission_type', self.event.cfp.default_type)
+        _now = now()
+        if not self.event.cfp.deadline or self.event.cfp.deadline >= _now:  # No global deadline or still open
+            types = self.event.submission_types.exclude(deadline__lt=_now)
+        else:
+            types = self.event.submission_types.filter(deadline__gte=_now)
+        pks = set(types.values_list('pk', flat=True))
+        if instance and instance.pk:
+            pks |= {instance.submission_type.pk, }
+
+        self.fields['submission_type'].queryset = self.event.submission_types.filter(pk__in=pks)
+
         locale_names = dict(settings.LANGUAGES)
         self.fields['content_locale'].choices = [(a, locale_names[a]) for a in self.event.locales]
         if readonly:
