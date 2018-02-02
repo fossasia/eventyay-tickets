be7acdbf94768345bff26dbb4c5a8e9613c21d5b
Tobias Kunze <rixx@cutebit.de>
2018-02-02 18:28:42 +0100

    [submission] Fix default submission type in form
    Closes #329

--- src/pretalx/submission/forms/submission.py
+++ src/pretalx/submission/forms/submission.py
@@ -11,8 +11,9 @@ class InfoForm(forms.ModelForm):
         readonly = kwargs.pop('readonly', False)
 
         super().__init__(**kwargs)
+        instance = kwargs.get('instance')
         self.fields['submission_type'].queryset = SubmissionType.objects.filter(event=self.event)
-        self.initial['submission_type'] = self.event.cfp.default_type
+        self.initial['submission_type'] = instance.submission_type if instance else self.event.cfp.default_type
         locale_names = dict(settings.LANGUAGES)
         self.fields['content_locale'].choices = [(a, locale_names[a]) for a in self.event.locales]
         if readonly:
