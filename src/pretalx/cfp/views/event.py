39df5e4f54e13afcfa3024a57cc7039f62887bf3
Tobias Kunze <rixx@cutebit.de>
2018-02-03 01:10:29 +0100

    Revert "[cfp] Remove most functionality of EventPageMixin"
    This reverts commit 2ebeb5a58ee2f87c468aef5d62f2ab6b4e9470f6.

--- src/pretalx/cfp/views/event.py
+++ src/pretalx/cfp/views/event.py
@@ -20,6 +20,57 @@ class EventPageMixin(PermissionRequired):
     def get_permission_object(self):
         return getattr(self.request, 'event', None)
 
+    def dispatch(self, request, *args, **kwargs):
+        self._select_locale(request)
+        return super().dispatch(request, *args, **kwargs)
+
+    def _language_from_browser(self, request, supported):
+        accept_value = request.META.get('HTTP_ACCEPT_LANGUAGE', '')
+        for accept_lang, unused in parse_accept_lang_header(accept_value):
+            if accept_lang == '*':
+                break
+
+            if not language_code_re.search(accept_lang):
+                continue
+
+            try:
+                val = get_supported_language_variant(accept_lang)
+                if val and val in supported:
+                    return val
+            except LookupError:
+                continue
+
+    def _language_from_cookie(self, request, supported):
+        cookie_value = request.COOKIES.get(settings.LANGUAGE_COOKIE_NAME)
+        with suppress(LookupError):
+            cookie_value = get_supported_language_variant(cookie_value)
+            if cookie_value and cookie_value in supported:
+                return cookie_value
+
+    def _language_from_user(self, request, supported):
+        if request.user.is_authenticated:
+            with suppress(LookupError):
+                value = get_supported_language_variant(request.user.locale)
+                if value and value in supported:
+                    return value
+
+    def _select_locale(self, request):
+        supported = request.event.locales
+        language = (
+            self._language_from_user(request, supported)
+            or self._language_from_cookie(request, supported)
+            or self._language_from_browser(request, supported)
+            or request.event.locale
+        )
+
+        translation.activate(language)
+        request.LANGUAGE_CODE = translation.get_language()
+
+        with suppress(pytz.UnknownTimeZoneError):
+            tzname = request.event.timezone
+            timezone.activate(pytz.timezone(tzname))
+            request.timezone = tzname
+
 
 class LoggedInEventPageMixin(EventPageMixin, LoginRequiredMixin):
 
