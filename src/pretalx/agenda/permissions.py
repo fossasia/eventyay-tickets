afaae0d0a31f6efcddcf63aa1362a16622d1b41f
Tobias Kunze <rixx@cutebit.de>
2018-03-02 17:47:09 +0100

    [agenda] More explicit bools in permissions

--- src/pretalx/agenda/permissions.py
+++ src/pretalx/agenda/permissions.py
@@ -5,23 +5,24 @@ from pretalx.person.permissions import is_orga
 
 @rules.predicate
 def is_agenda_visible(user, event):
-    return event and event.is_public and event.settings.show_schedule
+    return bool(event and event.is_public and event.settings.show_schedule)
 
 
 @rules.predicate
 def is_slot_visible(user, slot):
-    return is_agenda_visible(user, slot.submission.event) and slot.is_visible
+    return bool(slot and is_agenda_visible(user, slot.submission.event) and slot.is_visible)
 
 
 @rules.predicate
 def is_feedback_ready(user, submission):
-    return submission and is_slot_visible(user, submission.slot) and submission.does_accept_feedback
+    return bool(submission and is_slot_visible(user, submission.slot) and submission.does_accept_feedback)
 
 
 @rules.predicate
 def is_speaker_viewable(user, profile):
     if profile.user.submissions.filter(slots__schedule=profile.event.current_schedule).exists():
         return is_agenda_visible(user, profile.event)
+    return False
 
 
 rules.add_perm('agenda.view_schedule', is_agenda_visible | is_orga)
