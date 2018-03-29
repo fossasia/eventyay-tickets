7169da3c55136c1af3611ef0ae7a5f5000480097
Tobias Kunze <rixx@cutebit.de>
2018-03-30 00:58:45 +0200

    [common] Add the actual template tag

---
+++ src/pretalx/common/templatetags/times.py
@@ -0,0 +1,19 @@
+from django import template
+from django.utils.translation import ugettext_lazy as _
+
+register = template.Library()
+
+
+@register.filter
+def times(text: str, **kwargs):
+    """
+    This really really really could be a standard tag.
+    """
+    if text is None:
+        return ""
+    str_text = str(text)
+    if str_text == '1':
+        return _('once')
+    if str_text == '2':
+        return _('twice')
+    return _('{number} times').format(number=str_text)
