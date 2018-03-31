d8f367e60d42fce9d9107f00b4f377e94aa30d7a
Tobias Kunze <rixx@cutebit.de>
2018-03-31 17:37:59 +0200

    [submission] Fix initial submission form data

--- src/pretalx/submission/forms/submission.py
+++ src/pretalx/submission/forms/submission.py
@@ -11,11 +11,13 @@ class InfoForm(forms.ModelForm):
     def __init__(self, event, **kwargs):
         self.event = event
         readonly = kwargs.pop('readonly', False)
-
-        super().__init__(**kwargs)
         instance = kwargs.get('instance')
+        initial = kwargs.pop('initial', {})
+        initial['submission_type'] = getattr(instance, 'submission_type', self.event.cfp.default_type)
+        initial['content_locale'] = getattr(instance, 'content_locale', self.event.locale)
+
+        super().__init__(initial=initial, **kwargs)
         self.fields['submission_type'].queryset = SubmissionType.objects.filter(event=self.event)
-        self.initial['submission_type'] = getattr(instance, 'submission_type', self.event.cfp.default_type)
         _now = now()
         if not self.event.cfp.deadline or self.event.cfp.deadline >= _now:  # No global deadline or still open
             types = self.event.submission_types.exclude(deadline__lt=_now)
@@ -28,6 +30,7 @@ class InfoForm(forms.ModelForm):
         self.fields['submission_type'].queryset = self.event.submission_types.filter(pk__in=pks)
 
         locale_names = dict(settings.LANGUAGES)
+        print(self.initial)
         self.fields['content_locale'].choices = [(a, locale_names[a]) for a in self.event.locales]
         if readonly:
             for f in self.fields.values():
