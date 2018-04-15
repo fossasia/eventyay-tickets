a4444e17024abdb46b912f09bc5d10b5e9b04c97
Tobias Kunze <rixx@cutebit.de>
2018-04-15 18:03:29 +0200

    [orga] Add Organiser creation and helpful links to init command

--- src/pretalx/orga/management/commands/init.py
+++ src/pretalx/orga/management/commands/init.py
@@ -7,6 +7,9 @@ from django.db import transaction
 from django.urls import reverse
 from django.utils.translation import ugettext as _
 
+from pretalx.event.models import Organiser, Team
+from pretalx.person.models import User
+
 
 class Command(BaseCommand):
     help = 'Initializes your pretalx instance. Only to be used once.'
@@ -19,7 +22,24 @@ class Command(BaseCommand):
         self.stdout.write(_('''\nLet\'s get you a user with the right to create new events and access every event on this pretalx instance.'''))
 
         call_command('createsuperuser')
+        user = User.objects.order_by('-id').filter(is_administrator=True).first()
 
-        url = urljoin(settings.SITE_URL, reverse('orga:dashboard'))
-        self.stdout.write(_('\nNow that this is done, you can log in at {url} and get started.').format(url=url))
-        self.stdout.write(_('Use the command "import_schedule /path/to/schedule.xml" if you want to import an event."'))
+        self.stdout.write(_('''\nLet\'s also create a first organiser: This will allow you to invite further people and create events.'''))
+
+        organiser_name = input(_('\nName (e.g. "The Conference Organiser"): '))
+        organiser_slug = input(_('Slug (e.g. "conforg"): '))
+
+        organiser = Organiser.objects.create(name=organiser_name, slug=organiser_slug)
+        team = Team.objects.create(
+            organiser=organiser, name=f'Team {organiser_name}',
+            can_create_events=True, can_change_teams=True,
+            can_change_organiser_settings=True,
+        )
+        team.members.add(user)
+
+        event_url = urljoin(settings.SITE_URL, reverse('orga:event.create'))
+        team_url = urljoin(settings.SITE_URL, reverse('orga:organiser.teams.view', kwargs={'organiser': organiser_slug, 'pk': team.pk}))
+        self.stdout.write(_('\nNow that this is done, you can:'))
+        self.stdout.write(_(' - Create your first event at {event_url}').format(event_url=event_url))
+        self.stdout.write(_(' - Invite somebody to the organiser team at {team_url} and let them create the event').format(team_url=team_url))
+        self.stdout.write(_(' - Use the command "import_schedule /path/to/schedule.xml" if you want to import an event."'))
