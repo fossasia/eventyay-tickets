ToLABaki/pretalx

[agenda][orga] add setting to disable sneak peek

src/pretalx/agenda/permissions.py
@@ -9,6 +9,16 @@ def is_agenda_visible(user, event):
 
 
 @rules.predicate
+def has_agenda(user, event):
+    return bool(event.current_schedule)
+
+
+@rules.predicate
+def is_sneak_peek_visible(user, event):
+    return bool(event and event.is_public and event.settings.show_sneak_peek)
+
+
+@rules.predicate
 def is_slot_visible(user, slot):
     return bool(slot and is_agenda_visible(user, slot.submission.event) and slot.is_visible)
 
@@ -25,7 +35,8 @@ def is_speaker_viewable(user, profile):
     return False
 
 
-rules.add_perm('agenda.view_schedule', is_agenda_visible | is_orga)
+rules.add_perm('agenda.view_schedule', (has_agenda & is_agenda_visible) | is_orga)
+rules.add_perm('agenda.view_sneak_peek', (~has_agenda & is_sneak_peek_visible) | is_orga)
 rules.add_perm('agenda.view_slot', is_slot_visible | is_orga)
 rules.add_perm('agenda.view_speaker', is_speaker_viewable | is_orga)
 rules.add_perm('agenda.give_feedback', is_feedback_ready)

src/pretalx/common/models/settings.py
@@ -26,6 +26,7 @@ settings_hierarkey.add_type(LazyI18nString,
 
 
 settings_hierarkey.add_default('show_schedule', 'True', bool)
+settings_hierarkey.add_default('show_sneak_peek', 'True', bool)
 settings_hierarkey.add_default('export_html_on_schedule_release', 'True', bool)
 settings_hierarkey.add_default('custom_domain', '', str)
 

fossasia/eventyay-tickets

src/pretalx/agenda/permissions.py
@@ -9,6 +9,16 @@ def is_agenda_visible(user, event):
 
 
 @rules.predicate
+def has_agenda(user, event):
+    return bool(event.current_schedule)
+
+
+@rules.predicate
+def is_sneak_peek_visible(user, event):
+    return bool(event and event.is_public and event.settings.show_sneak_peek)
+
+
+@rules.predicate
 def is_slot_visible(user, slot):
     return bool(slot and is_agenda_visible(user, slot.submission.event) and slot.is_visible)
 
@@ -24,7 +34,8 @@ def is_speaker_viewable(user, profile):
     return is_speaker and is_agenda_visible(user, profile.event)
 
 
-rules.add_perm('agenda.view_schedule', is_agenda_visible | can_change_submissions)
+rules.add_perm('agenda.view_schedule', (has_agenda & is_agenda_visible) | can_change_submissions)
+rules.add_perm('agenda.view_sneak_peek', (~has_agenda & is_sneak_peek_visible) | can_change_submissions)
 rules.add_perm('agenda.view_slot', is_slot_visible | can_change_submissions)
 rules.add_perm('agenda.view_speaker', is_speaker_viewable | can_change_submissions)
 rules.add_perm('agenda.give_feedback', is_feedback_ready)

src/pretalx/common/models/settings.py
@@ -38,6 +38,7 @@ hierarkey.add_type(
 
 hierarkey.add_default('show_on_dashboard', 'True', bool)
 hierarkey.add_default('show_schedule', 'True', bool)
+hierarkey.add_default('show_sneak_peek', 'True', bool)
 hierarkey.add_default('export_html_on_schedule_release', 'True', bool)
 hierarkey.add_default('custom_domain', '', str)
 