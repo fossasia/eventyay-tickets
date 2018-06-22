5fdc2ce6bec7b326866182ab8b1df71c7464405f
Tobias Kunze <r@rixx.de>
2018-06-22 08:39:48 +0200

    Link changelogs/release notes in setup.py

--- src/setup.py
+++ src/setup.py
@@ -64,33 +64,33 @@ setup(
 
     keywords='conference cfp event barcamp',
     install_requires=[
-        'bleach>=2.1.2,==2.1.*',
-        'celery==4.1.*',
-        'csscompressor==0.9.*',
-        'cssutils==1.0.*',
-        'Django==2.0.*',
-        'django-bakery==0.12.*',
-        'django-bootstrap4==0.0.6',
-        'django-compressor==2.2.*',
-        'django-csp==3.4.*',
-        'django-filter==1.1.*',
-        'django-formset-js-improved==0.5.0.2',
-        'django-formtools==2.1.*',
-        'django-hierarkey==1.0.*',
-        'django-i18nfield==1.3.*',
-        'django-libsass==0.7',
-        'djangorestframework==3.7.*',
-        'inlinestyler==0.2.*',
-        'libsass<=0.13.1',  # May be removed once https://github.com/dahlia/libsass-python/pull/227 is merged
-        'Markdown==2.6.*',
+        'bleach>=2.1.2,==2.1.*', # https://bleach.readthedocs.io/en/latest/changes.html
+        'celery==4.2.*',  # search for "what's new" on http://docs.celeryproject.org/en/latest/
+        'csscompressor==0.9.*',  # 2017-11, no changelog, https://github.com/sprymix/csscompressor
+        'cssutils==1.0.*',  # https://pythonhosted.org/cssutils/CHANGELOG.html
+        'Django==2.0.*',  # https://docs.djangoproject.com/en/2.0/releases/
+        'django-bakery==0.12.*',  # http://django-bakery.readthedocs.io/en/latest/changelog.html
+        'django-bootstrap4==0.0.6',  # http://django-bootstrap4.readthedocs.io/en/latest/history.html
+        'django-compressor==2.2.*',  # https://django-compressor.readthedocs.io/en/latest/changelog/
+        'django-csp==3.4.*',  # https://github.com/mozilla/django-csp/blob/master/CHANGES
+        'django-filter==1.1.*',  # https://github.com/carltongibson/django-filter/blob/master/CHANGES.rst
+        'django-formset-js-improved==0.5.0.2',  # no changelog, https://github.com/pretix/django-formset-js
+        'django-formtools==2.1.*',  # http://django-formtools.readthedocs.io/en/latest/changelog.html
+        'django-hierarkey==1.0.*',  # no changelog, https://github.com/raphaelm/django-hierarkey
+        'django-i18nfield==1.3.*',  # 2017-11, no changelog, https://github.com/raphaelm/django-i18nfield/
+        'django-libsass==0.7',  # inactive, https://github.com/torchbox/django-libsass/blob/master/CHANGELOG.txt
+        'djangorestframework==3.8.*',  # http://www.django-rest-framework.org/topics/release-notes/
+        'inlinestyler==0.2.*',  # https://github.com/dlanger/inlinestyler/blob/master/CHANGELOG
+        'libsass==0.14.5',  # https://sass.github.io/libsass-python/changes.html
+        'Markdown==2.6.*',  # https://python-markdown.github.io/change_log/
         'pytz',
-        'reportlab==3.4.*',
-        'requests',
-        'rules==1.3.*',
-        'urlman==1.2.*',
-        'vobject==0.9.*',
-        'whitenoise==3.3.*',
-        'zxcvbn-python==4.4.*',
+        'reportlab==3.4.*',  # https://www.reportlab.com/documentation/relnotes/
+        'requests',  # http://docs.python-requests.org/en/master/community/updates/#release-history
+        'rules==1.3.*',  # https://github.com/dfunckt/django-rules/blob/master/CHANGELOG.md
+        'urlman==1.2.*',  # https://github.com/andrewgodwin/urlman/blob/master/CHANGELOG
+        'vobject==0.9.*',  # 2017-06, http://eventable.github.io/vobject/ look for "release"
+        'whitenoise==3.3.*',  # http://whitenoise.evans.io/en/stable/changelog.html
+        'zxcvbn==4.4.*',  # Nothing? https://github.com/dwolfhub/zxcvbn-python/issues/38
     ],
     extras_require={
         'dev': [
