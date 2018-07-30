0c20feab0c7a630698e36035406594d6ca59bf11
Tobias Kunze <r@rixx.de>
2018-07-30 06:46:46 +0200

    [tests] More permanent fix for failures

--- src/tests/unit/event/test_event_stages.py
+++ src/tests/unit/event/test_event_stages.py
@@ -8,7 +8,8 @@ from pretalx.event.stages import STAGE_ORDER, in_stage
 
 @pytest.mark.django_db
 @pytest.mark.parametrize(
-    'target,is_public,from_delta,to_delta,deadline_delta,has_submissions', (
+    'target,is_public,from_delta,to_delta,deadline_delta,has_submissions',
+    (
         ('PREPARATION', False, 2, 3, 1, False),
         ('PREPARATION', False, 2, 3, 1, True),
         ('CFP_OPEN', True, 2, 3, 1, False),
@@ -16,18 +17,27 @@ from pretalx.event.stages import STAGE_ORDER, in_stage
         ('REVIEW', True, 2, 3, -1, True),
         ('SCHEDULE', True, 2, 3, -1, False),
         ('EVENT', True, -2, 3, -1, False),
-        ('EVENT', True, 0, 0, -1, False),
         ('EVENT', True, -2, 3, -1, True),
+        ('EVENT', True, 0, 0, -1, False),
         ('EVENT', True, 0, 0, -1, True),
         ('WRAPUP', True, -2, -1, -1, True),
         ('WRAPUP', True, -2, -1, -1, False),
-    )
+    ),
 )
-def test_event_stages(event, submission, target, is_public, from_delta, to_delta, deadline_delta, has_submissions):
+def test_event_stages(
+    event,
+    submission,
+    target,
+    is_public,
+    from_delta,
+    to_delta,
+    deadline_delta,
+    has_submissions,
+):
     _now = now()
     event.is_public = is_public
-    event.date_from = _now + timedelta(days=from_delta)
-    event.date_to = _now + timedelta(days=to_delta)
+    event.date_from = (_now + timedelta(days=from_delta)).date()
+    event.date_to = (_now + timedelta(days=to_delta)).date()
     event.cfp.deadline = _now + timedelta(days=deadline_delta)
     event.save()
     event.cfp.save()
@@ -36,6 +46,8 @@ def test_event_stages(event, submission, target, is_public, from_delta, to_delta
         submission.state = 'DELETED'
         submission.save()
     for stage in STAGE_ORDER:
-        assert in_stage(event, stage) == (stage == target), f'Event is {stage} when it should be {target}!'
+        assert in_stage(event, stage) == (
+            stage == target
+        ), f'Event is{" not" if stage == target else ""} {stage} when it should be {target}!'
         if stage == target:
             break
