888d896cbf50289a443dbd8f8762b2280bfe0497
Tobias Kunze <r@rixx.de>
2018-08-03 11:17:37 +0200

    Adjust tox settings to travis

--- .travis/install.sh
+++ .travis/install.sh
@@ -1,9 +1,20 @@
-#!/bin/sh
+#!/bin/bash
 set -e
 set -u
 
-pip install -U pip setuptools wheel
+pip install -U pip setuptools wheel tox
 
 if [ $TOXENV = "docs" ]; then
     sudo apt-get install enchant myspell-en-gb
+    echo "Installed spellcheck dependencies"
+fi
+
+if [[ "$TOXENV" = *"mysql"* ]]; then
+    mysql -u root -e 'CREATE DATABASE pretalx DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci;'
+    echo "Created database"
+fi
+
+if [[ "$TOXENV" = *"postgres"* ]]; then
+    psql -c 'create database travis_ci_test;' -U postgres
+    echo "Created database"
 fi
