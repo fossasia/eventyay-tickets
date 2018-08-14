e828b820d087b358c081624122917800c180d054
Tobias Kunze <r@rixx.de>
2018-08-14 20:24:50 +0200

    [tests] Adjust tests to new form

--- src/tests/functional/orga/test_organiser.py
+++ src/tests/functional/orga/test_organiser.py
@@ -42,6 +42,7 @@ def test_orga_edit_team(orga_client, organiser, event):
             'form': 'team',
             'limit_events': event.pk,
             'name': 'Fancy New Name',
+            'review_override_votes': 10,
         },
     )
     assert response.status_code == 200
@@ -70,6 +71,7 @@ def test_orga_create_team(orga_client, organiser, event, is_administrator, orga_
             'limit_events': event.pk,
             'name': 'Fancy New Name',
             'organiser': organiser.pk,
+            'review_override_votes': 0,
         },
     )
     assert response.status_code == 200
