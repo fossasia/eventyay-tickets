9a6554c528aad3be19502532afa23c8f75cff2b0
Tobias Kunze <r@rixx.de>
2018-09-22 09:36:54 +0200

    [tests] Test error cases in password reset

--- src/tests/functional/cfp/test_auth.py
+++ src/tests/functional/cfp/test_auth.py
@@ -57,3 +57,75 @@ def test_can_reset_password_by_email(speaker, client, event):
         follow=True,
     )
     assert 'You are logged in as' in response.content.decode()
+
+
+@pytest.mark.django_db
+def test_cannot_use_incorrect_token(speaker, client, event):
+    response = client.post(
+        event.urls.reset + f'/abcdefg',
+        data={'password': 'mynewpassword1!', 'password_repeat': 'mynewpassword1!'},
+        follow=True,
+    )
+    assert response.status_code == 200
+
+
+@pytest.mark.django_db
+def test_cannot_reset_password_with_incorrect_input(speaker, client, event):
+    response = client.post(
+        event.urls.reset,
+        data={'login_email': speaker.email, },
+        follow=True,
+    )
+    assert response.status_code == 200
+    speaker.refresh_from_db()
+    assert speaker.pw_reset_token
+    response = client.post(
+        event.urls.reset + f'/{speaker.pw_reset_token}',
+        data={'password': 'mynewpassword1!', 'password_repeat': 'mynewpassword123!'},
+        follow=True,
+    )
+    assert response.status_code == 200
+    speaker.refresh_from_db()
+    assert speaker.pw_reset_token
+    response = client.post(
+        event.urls.login,
+        data={'login_email': speaker.email, 'login_password': 'mynewpassword1!'},
+        follow=True,
+    )
+    assert not 'You are logged in as' in response.content.decode()
+
+
+@pytest.mark.django_db
+def test_cannot_reset_password_to_insecure_password(speaker, client, event):
+    response = client.post(
+        event.urls.reset,
+        data={'login_email': speaker.email, },
+        follow=True,
+    )
+    assert response.status_code == 200
+    speaker.refresh_from_db()
+    assert speaker.pw_reset_token
+    response = client.post(
+        event.urls.reset + f'/{speaker.pw_reset_token}',
+        data={'password': 'password', 'password_repeat': 'password'},
+        follow=True,
+    )
+    assert response.status_code == 200
+    speaker.refresh_from_db()
+    assert speaker.pw_reset_token
+    response = client.post(
+        event.urls.login,
+        data={'login_email': speaker.email, 'login_password': 'mynewpassword1!'},
+        follow=True,
+    )
+    assert not 'You are logged in as' in response.content.decode()
+
+
+@pytest.mark.django_db
+def test_cannot_reset_password_without_account(speaker, client, event):
+    response = client.post(
+        event.urls.reset,
+        data={'login_email': 'incorrect' + speaker.email, },
+        follow=True,
+    )
+    assert response.status_code == 200
