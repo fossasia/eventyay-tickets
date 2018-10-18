b30da3aaaf42d38fe454acbc0027afe5bb5ded4d
Tobias Kunze <r@rixx.de>
2018-10-18 09:30:14 +0200

    Add missing tests file

---
+++ src/tests/unit/schedule/test_schedule_utils.py
@@ -0,0 +1,21 @@
+import pytest
+
+from pretalx.schedule.utils import guess_schedule_version
+
+
+@pytest.mark.django_db
+@pytest.mark.parametrize('previous,suggestion', (
+    (None, '0.1'),
+    ('0.1', '0.2'),
+    ('0,2', '0,3'),
+    ('0-3', '0-4'),
+    ('0_4', '0_5'),
+    ('1.0.1', '1.0.2'),
+    ('Nichtnumerisch', None),
+    ('1.someting', None),
+    ('something.1', 'something.2'),
+))
+def test_schedule_version_guessing(event, previous, suggestion):
+    if previous:
+        event.release_schedule(previous)
+    assert guess_schedule_version(event) == suggestion
