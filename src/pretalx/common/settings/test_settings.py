d47cd12f0c8e9b5c7c9b62a95e0fd12874e298c3
Tobias Kunze <r@rixx.de>
2018-11-02 09:01:18 +0100

    Use only a single settings file

--- src/pretalx/common/settings/test_settings.py
+++ src/pretalx/common/settings/test_settings.py
@@ -9,13 +9,22 @@ if os.path.exists('test/sqlite.cfg'):
 
 from pretalx.settings import *  # NOQA
 
+BASE_DIR = tmpdir.name
 DATA_DIR = tmpdir.name
 LOG_DIR = os.path.join(DATA_DIR, 'logs')
 MEDIA_ROOT = os.path.join(DATA_DIR, 'media')
+STATIC_ROOT = os.path.join(DATA_DIR, 'static')
+HTMLEXPORT_ROOT = os.path.join(DATA_DIR, 'htmlexport')
+
+for directory in (BASE_DIR, DATA_DIR, LOG_DIR, MEDIA_ROOT, HTMLEXPORT_ROOT):
+    os.makedirs(directory, exist_ok=True)
+
+INSTALLED_APPS.append('tests.dummy_app.PluginApp')  # noqa
 
 atexit.register(tmpdir.cleanup)
 
 EMAIL_BACKEND = 'django.core.mail.outbox'
+MAIL_FROM = 'orga@orga.org'
 
 COMPRESS_ENABLED = COMPRESS_OFFLINE = False
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
@@ -37,6 +46,10 @@ CACHES = {
         'BACKEND': 'django.core.cache.backends.dummy.DummyCache',
     }
 }
+
+with suppress(ValueError):
+    INSTALLED_APPS.remove('debug_toolbar.apps.DebugToolbarConfig')  # noqa
+    MIDDLEWARE.remove('debug_toolbar.middleware.DebugToolbarMiddleware')  # noqa
 
 # Don't run migrations
 class DisableMigrations(object):
