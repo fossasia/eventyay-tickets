6250750b3543a51e60f3baf997b9abdeb17bb15a
Tobias Kunze <r@rixx.de>
2018-11-05 12:49:12 +0100

    Render more technically correct html

--- src/pretalx/static/agenda/js/offline.js
+++ src/pretalx/static/agenda/js/offline.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) {
   const source = document.querySelector('#offline-vars')
   if (source) {
-    navigator.serviceWorker.register('/sw.js', {scope: source.getAttribute('event') + '/'})
+    navigator.serviceWorker.register('/sw.js', {scope: source.getAttribute('data-event') + '/'})
   }
 }
