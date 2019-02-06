de35c1854c5d9f3d5bfe90d78cd080ea18cb071d
Tobias Kunze <r@rixx.de>
2019-02-06 23:26:41 +0100

    [common] Support Spanish date ranges

--- src/pretalx/common/utils.py
+++ src/pretalx/common/utils.py
@@ -27,6 +27,28 @@ def daterange(date_from, date_to):
             return "{} – {}".format(_date(date_from, "N jS"), _date(date_to, "jS, Y"))
         elif date_from.year == date_to.year:
             return "{} – {}".format(_date(date_from, "N jS"), _date(date_to, "N jS, Y"))
+    elif language.startswith("es"):
+        if (
+            date_from.year == date_to.year
+            and date_from.month == date_to.month
+            and date_from.day == date_to.day
+        ):
+            return "{}".format(_date(date_from, "DATE_FORMAT"))
+        elif date_from.year == date_to.year and date_from.month == date_to.month:
+            return "{} - {} de {} de {}".format(
+                _date(date_from, "j"),
+                _date(date_to, "j"),
+                _date(date_to, "F"),
+                _date(date_to, "Y"),
+            )
+        elif date_from.year == date_to.year:
+            return "{} de {} - {} de {} de {}".format(
+                _date(date_from, "j"),
+                _date(date_from, "F"),
+                _date(date_to, "j"),
+                _date(date_to, "F"),
+                _date(date_to, "Y"),
+            )
 
     return _("{date_from} – {date_to}").format(
         date_from=_date(date_from, "DATE_FORMAT"), date_to=_date(date_to, "DATE_FORMAT")
