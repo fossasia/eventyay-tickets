51a9b0f56cef089334d35c2e8799f4caefac4a99
Peter Körner <pkoerner@seibert-media.net>
2019-02-06 19:29:03 +0100

    [cfp] fix misalignment of orga-edit button

--- src/pretalx/orga/templatetags/orga_edit_link.py
+++ src/pretalx/orga/templatetags/orga_edit_link.py
@@ -9,5 +9,5 @@ register = template.Library()
 def orga_edit_link(url, target=None):
     if target:
         url = f'{url}#{target}'
-    result = f'<a href="{url}" class="btn btn-xs btn-outline-primary orga-edit-link float-right" title="{_("Edit")}"><i class="fa fa-pencil"></i></a>'
+    result = f'<a href="{url}" class="btn btn-xs btn-outline-primary orga-edit-link ml-auto" title="{_("Edit")}"><i class="fa fa-pencil"></i></a>'
     return mark_safe(result)
