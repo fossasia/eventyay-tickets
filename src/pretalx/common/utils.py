d4242071261fcd54c3041198b6690d52f6a4c9fd
Tobias Kunze <r@rixx.de>
2019-02-09 12:24:02 +0100

    [common] Make daterange method more readable

--- src/pretalx/common/utils.py
+++ src/pretalx/common/utils.py
@@ -2,54 +2,67 @@ from django.template.defaultfilters import date as _date
 from django.utils.translation import get_language, ugettext_lazy as _
 
 
+def daterange_de(date_from, date_to):
+    if (
+        date_from.year == date_to.year
+        and date_from.month == date_to.month
+        and date_from.day == date_to.day
+    ):
+        return str(_date(date_from, "j. F Y"))
+    elif date_from.year == date_to.year and date_from.month == date_to.month:
+        return "{}.–{}".format(_date(date_from, "j"), _date(date_to, "j. F Y"))
+    elif date_from.year == date_to.year:
+        return "{} – {}".format(_date(date_from, "j. F"), _date(date_to, "j. F Y"))
+
+
+def daterange_en(date_from, date_to):
+    if (
+        date_from.year == date_to.year
+        and date_from.month == date_to.month
+        and date_from.day == date_to.day
+    ):
+        return str(_date(date_from, "N jS, Y"))
+    elif date_from.year == date_to.year and date_from.month == date_to.month:
+        return "{} – {}".format(_date(date_from, "N jS"), _date(date_to, "jS, Y"))
+    elif date_from.year == date_to.year:
+        return "{} – {}".format(_date(date_from, "N jS"), _date(date_to, "N jS, Y"))
+
+
+def daterange_es(date_from, date_to):
+    if (
+        date_from.year == date_to.year
+        and date_from.month == date_to.month
+        and date_from.day == date_to.day
+    ):
+        return "{}".format(_date(date_from, "DATE_FORMAT"))
+    elif date_from.year == date_to.year and date_from.month == date_to.month:
+        return "{} - {} de {} de {}".format(
+            _date(date_from, "j"),
+            _date(date_to, "j"),
+            _date(date_to, "F"),
+            _date(date_to, "Y"),
+        )
+    elif date_from.year == date_to.year:
+        return "{} de {} - {} de {} de {}".format(
+            _date(date_from, "j"),
+            _date(date_from, "F"),
+            _date(date_to, "j"),
+            _date(date_to, "F"),
+            _date(date_to, "Y"),
+        )
+
+
 def daterange(date_from, date_to):
+    result = None
     language = get_language()
 
     if language.startswith("de"):
-        if (
-            date_from.year == date_to.year
-            and date_from.month == date_to.month
-            and date_from.day == date_to.day
-        ):
-            return str(_date(date_from, "j. F Y"))
-        elif date_from.year == date_to.year and date_from.month == date_to.month:
-            return "{}.–{}".format(_date(date_from, "j"), _date(date_to, "j. F Y"))
-        elif date_from.year == date_to.year:
-            return "{} – {}".format(_date(date_from, "j. F"), _date(date_to, "j. F Y"))
+        result = daterange_de(date_from, date_to)
     elif language.startswith("en"):
-        if (
-            date_from.year == date_to.year
-            and date_from.month == date_to.month
-            and date_from.day == date_to.day
-        ):
-            return str(_date(date_from, "N jS, Y"))
-        elif date_from.year == date_to.year and date_from.month == date_to.month:
-            return "{} – {}".format(_date(date_from, "N jS"), _date(date_to, "jS, Y"))
-        elif date_from.year == date_to.year:
-            return "{} – {}".format(_date(date_from, "N jS"), _date(date_to, "N jS, Y"))
+        result = daterange_en(date_from, date_to)
     elif language.startswith("es"):
-        if (
-            date_from.year == date_to.year
-            and date_from.month == date_to.month
-            and date_from.day == date_to.day
-        ):
-            return "{}".format(_date(date_from, "DATE_FORMAT"))
-        elif date_from.year == date_to.year and date_from.month == date_to.month:
-            return "{} - {} de {} de {}".format(
-                _date(date_from, "j"),
-                _date(date_to, "j"),
-                _date(date_to, "F"),
-                _date(date_to, "Y"),
-            )
-        elif date_from.year == date_to.year:
-            return "{} de {} - {} de {} de {}".format(
-                _date(date_from, "j"),
-                _date(date_from, "F"),
-                _date(date_to, "j"),
-                _date(date_to, "F"),
-                _date(date_to, "Y"),
-            )
+        result = daterange_es(date_from, date_to)
 
-    return _("{date_from} – {date_to}").format(
+    return result or _("{date_from} – {date_to}").format(
         date_from=_date(date_from, "DATE_FORMAT"), date_to=_date(date_to, "DATE_FORMAT")
     )
