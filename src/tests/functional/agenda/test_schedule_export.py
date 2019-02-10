8feef3696fa80f24f6895164e9821df4026fac11
Tobias Kunze <r@rixx.de>
2019-02-10 22:36:33 +0100

    [tests] One more query won't kill us

--- src/tests/functional/agenda/test_schedule_export.py
+++ src/tests/functional/agenda/test_schedule_export.py
@@ -301,7 +301,7 @@ def test_schedule_orga_trigger_export(
 
     mocker.patch('pretalx.agenda.tasks.export_schedule_html.apply_async')
 
-    with django_assert_num_queries(33):
+    with django_assert_num_queries(34):
         response = orga_client.post(
             event.orga_urls.schedule_export_trigger, follow=True
         )
