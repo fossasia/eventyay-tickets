6e6d018f8c474c1082f425680406609c0fc064dd
Tobias Kunze <r@rixx.de>
2019-03-10 23:56:22 +0100

    [cfp] Don't allow speakers to add speakers after cfp end
    Closes #620

--- src/pretalx/cfp/permissions.py
+++ src/pretalx/cfp/permissions.py
@@ -1,4 +1,5 @@
 import rules
+from django.utils.timezone import now
 
 from pretalx.person.permissions import can_change_submissions, is_reviewer
 
@@ -8,4 +9,11 @@ def is_event_visible(user, event):
     return event and event.is_public
 
 
+@rules.predicate
+def submission_deadline_open(user, submission):
+    deadline = submission.submission_type.deadline or submission.event.cfp.deadline
+    return (not deadline) or now() <= deadline
+
+
 rules.add_perm('cfp.view_event', is_event_visible | (can_change_submissions | is_reviewer))
+rules.add_perm('cfp.add_speakers', submission_deadline_open)
