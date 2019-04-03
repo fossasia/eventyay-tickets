764079321a31a465889a1ad44074acaf28304519
Tobias Kunze <r@rixx.de>
2019-04-03 10:02:58 +0200

    [orga] Do not let reviewers without access to speaker names see history
    Closes #649

--- src/tests/orga/views/test_orga_views_dashboard.py
+++ src/tests/orga/views/test_orga_views_dashboard.py
@@ -4,7 +4,7 @@ from django.urls import reverse
 
 @pytest.mark.parametrize('test_user', ('orga', 'speaker', 'superuser', 'None'))
 @pytest.mark.django_db
-def test_dashboard_event_list(orga_user, orga_client, speaker, event, other_event, test_user):
+def test_dashboard_event_list(orga_user, orga_client, speaker, event, other_event, test_user, slot):
     if test_user == 'speaker':
         orga_client.force_login(speaker)
     elif test_user == 'None':
@@ -26,6 +26,46 @@ def test_dashboard_event_list(orga_user, orga_client, speaker, event, other_even
         assert response.status_code == 200
         assert event.slug in response.content.decode(), response.content.decode()
         assert other_event.slug in response.content.decode(), response.content.decode()
+    else:
+        current_url = response.redirect_chain[-1][0]
+        assert 'login' in current_url
+
+
+@pytest.mark.parametrize('test_user', ('orga', 'speaker', 'superuser', 'reviewer', 'None'))
+@pytest.mark.django_db
+def test_event_dashboard(orga_user, orga_client, review_user, speaker, event, test_user, slot):
+    from pretalx.common.models.log import ActivityLog
+    ActivityLog.objects.create(event=event, person=speaker, content_object=slot.submission, action_type='pretalx.submission.create')
+    if test_user == 'speaker':
+        orga_client.force_login(speaker)
+    elif test_user == 'None':
+        orga_client.logout()
+    elif test_user == 'superuser':
+        orga_user.is_administrator = True
+        orga_user.save()
+    elif test_user == 'reviewer':
+        event.active_review_phase.can_see_speaker_names = False
+        event.active_review_phase.save()
+        orga_client.force_login(review_user)
+
+    response = orga_client.get(event.orga_urls.base, follow=True)
+
+    if test_user == 'speaker':
+        assert response.status_code == 404
+        assert event.slug not in response.content.decode()
+    elif test_user == 'orga':
+        assert response.status_code == 200
+        assert event.slug in response.content.decode()
+        assert speaker.name in response.content.decode()
+    elif test_user == 'superuser':
+        assert response.status_code == 200
+        assert event.slug in response.content.decode(), response.content.decode()
+        assert speaker.name in response.content.decode()
+    elif test_user == 'reviewer':
+        assert not review_user.has_perm('orga.view_speakers', event)
+        assert response.status_code == 200
+        assert event.slug in response.content.decode(), response.content.decode()
+        assert speaker.name not in response.content.decode()
     else:
         current_url = response.redirect_chain[-1][0]
         assert 'login' in current_url
