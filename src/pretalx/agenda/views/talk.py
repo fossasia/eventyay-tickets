fossasia/eventyay-tickets

commit c43baac4adadbe93aa966d763625d9a220931c78

🎨 Improve code style

src/pretalx/agenda/views/talk.py
@@ -120,7 +120,7 @@ class TalkView(PermissionRequired, TemplateView):
         return response
 
     def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
+        result = super().get_context_data(**kwargs)
         qs = TalkSlot.objects.none()
         schedule = Schedule.objects.none()
         if self.request.event.current_schedule:
@@ -129,15 +129,15 @@ class TalkView(PermissionRequired, TemplateView):
         elif self.request.is_orga:
             schedule = self.request.event.wip_schedule
             qs = schedule.talks.filter(room__isnull=False).select_related('room')
-        context['talk_slots'] = qs.filter(submission=self.submission).order_by('start').select_related('room')
+        result['talk_slots'] = qs.filter(submission=self.submission).order_by('start').select_related('room')
         result = []
         other_submissions = schedule.slots.exclude(pk=self.submission.pk)
         for speaker in self.submission.speakers.all():
             speaker.talk_profile = speaker.event_profile(event=self.request.event)
             speaker.other_submissions = other_submissions.filter(speakers__in=[speaker]).select_related('event')
             result.append(speaker)
-        context['speakers'] = result
-        return context
+        result['speakers'] = result
+        return result
 
     @context
     @cached_property

src/pretalx/agenda/views/schedule.py
@@ -52,21 +52,21 @@ class ScheduleDataView(EventPermissionRequired, TemplateView):
         return self.get_object()
 
     def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
+        result = super().get_context_data(**kwargs)
         schedule = self.schedule
         event = self.request.event
 
         if not schedule and self.version:
-            context['version'] = self.version
-            context['error'] = f'Schedule "{self.version}" not found.'
-            return context
+            result['version'] = self.version
+            result['error'] = f'Schedule "{self.version}" not found.'
+            return result
         if not schedule:
-            context['error'] = 'Schedule not found.'
-            return context
-        context['schedules'] = event.schedules.filter(
+            result['error'] = 'Schedule not found.'
+            return result
+        result['schedules'] = event.schedules.filter(
             published__isnull=False
         ).values_list('version')
-        return context
+        return result
 
 
 class ExporterView(ScheduleDataView):
@@ -124,21 +124,21 @@ class ScheduleView(ScheduleDataView):
     def get_context_data(self, **kwargs):
         from pretalx.schedule.exporters import ScheduleData
 
-        context = super().get_context_data(**kwargs)
-        context['exporters'] = list(
+        result = super().get_context_data(**kwargs)
+        result['exporters'] = list(
             exporter(self.request.event)
             for _, exporter in register_data_exporters.send(self.request.event)
         )
         timezone = pytz.timezone(self.request.event.timezone)
-        if 'schedule' not in context:
-            return context
+        if 'schedule' not in result:
+            return result
 
-        context['data'] = ScheduleData(
+        result['data'] = ScheduleData(
             event=self.request.event, schedule=self.schedule
         ).data
-        context['search'] = self.request.GET.get('q', '').lower()
+        result['search'] = self.request.GET.get('q', '').lower()
         max_rooms = 0
-        for date in context['data']:
+        for date in result['data']:
             if date.get('first_start') and date.get('last_end'):
                 start = (
                     date.get('first_start')
@@ -162,8 +162,8 @@ class ScheduleView(ScheduleDataView):
                         )
                         talk.height = int(talk.duration * 2)
                         talk.is_active = talk.start <= now() <= talk.real_end
-        context['max_rooms'] = max_rooms
-        return context
+        result['max_rooms'] = max_rooms
+        return result
 
 
 class ChangelogView(EventPermissionRequired, TemplateView):

src/pretalx/api/views/speaker.py
@@ -19,7 +19,7 @@ class SpeakerViewSet(viewsets.ReadOnlyModelViewSet):
     def get_base_queryset(self):
         if self.request.user.has_perm('orga.view_speakers', self.request.event):
             return SpeakerProfile.objects.filter(event=self.request.event)
-        elif (
+        if (
             self.request.event.current_schedule
             and self.request.event.settings.show_schedule
         ):

src/pretalx/api/views/user.py
@@ -3,7 +3,7 @@ from rest_framework.views import APIView
 
 
 class MeView(APIView):
-    def get(self, request, format=None):
+    def get(self, request, **kwargs):
         return Response(
             {
                 'email': getattr(request.user, 'email', None),

src/pretalx/cfp/views/event.py
@@ -69,12 +69,12 @@ class GeneralView(TemplateView):
         ]
 
     def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
+        result = super().get_context_data(**kwargs)
         _now = now().date()
         qs = Event.objects.order_by('-date_to')
-        context['current_events'] = self.filter_events(
+        result['current_events'] = self.filter_events(
             qs.filter(date_from__lte=_now, date_to__gte=_now)
         )
-        context['past_events'] = self.filter_events(qs.filter(date_to__lt=_now))
-        context['future_events'] = self.filter_events(qs.filter(date_from__gt=_now))
-        return context
+        result['past_events'] = self.filter_events(qs.filter(date_to__lt=_now))
+        result['future_events'] = self.filter_events(qs.filter(date_from__gt=_now))
+        return result

src/pretalx/common/forms/validators.py
@@ -10,7 +10,7 @@ class ZXCVBNValidator:
     DEFAULT_USER_ATTRIBUTES = ('first_name', 'last_name', 'email')
 
     def __init__(self, min_score=3, user_attributes=DEFAULT_USER_ATTRIBUTES):
-        if not (0 <= min_score <= 4):
+        if not 0 <= min_score <= 4:
             raise Exception('min_score must be between 0 and 4!')
         self.min_score = min_score
         self.user_attributes = user_attributes

src/pretalx/common/middleware/event.py
@@ -89,8 +89,10 @@ class EventPermissionMiddleware:
         self._set_orga_events(request)
         self._select_locale(request)
         is_exempt = (
-            'agenda' in url.namespaces and url.url_name == 'export'
-        ) or request.path.startswith('/api/')
+            url.url_name == 'export'
+            if 'agenda' in url.namespaces
+            else request.path.startswith('/api/')
+        )
 
         if 'orga' in url.namespaces or (
             'plugins' in url.namespaces and request.path.startswith('/orga')
@@ -170,3 +172,4 @@ class EventPermissionMiddleware:
                 value = get_supported_language_variant(request.user.locale)
                 if value and value in supported:
                     return value
+        return None

src/pretalx/common/mixins/views.py
@@ -76,7 +76,7 @@ class Sortable:
             sort_key = getattr(self, 'default_sort_field', '')
         if sort_key:
             plain_key = sort_key[1:] if sort_key.startswith('-') else sort_key
-            reverse = not (plain_key == sort_key)
+            reverse = not plain_key == sort_key
             if plain_key in self.sortable_fields:
                 is_text = False
                 if '__' not in plain_key:
@@ -163,6 +163,7 @@ class Filterable:
                 if hasattr(field, 'queryset'):
                     field.queryset = field.queryset.filter(event=self.request.event)
             return _form
+        return None
 
 
 class PermissionRequired(PermissionRequiredMixin):
@@ -244,7 +245,6 @@ class SensibleBackWizardMixin():
             if self.steps.current == self.steps.last:
                 # no more steps, render done view
                 return self.render_done(form, **kwargs)
-            else:
-                # proceed to the next step
-                return self.render_next_step(form)
+            # proceed to the next step
+            return self.render_next_step(form)
         return self.render(form)

commit 130cbba7014629fc04937921e09fbc4de3a25d2d

🐛 Fix broken talk detail page
Introduced in refactoring in c43baac4

src/pretalx/agenda/views/talk.py
@@ -115,7 +115,7 @@ class TalkView(PermissionRequired, TemplateView):
         return response
 
     def get_context_data(self, **kwargs):
-        result = super().get_context_data(**kwargs)
+        ctx = super().get_context_data(**kwargs)
         qs = TalkSlot.objects.none()
         schedule = Schedule.objects.none()
         if self.request.event.current_schedule:
@@ -124,15 +124,15 @@ class TalkView(PermissionRequired, TemplateView):
         elif self.request.is_orga:
             schedule = self.request.event.wip_schedule
             qs = schedule.talks.filter(room__isnull=False).select_related('room')
-        result['talk_slots'] = qs.filter(submission=self.submission).order_by('start').select_related('room')
+        ctx['talk_slots'] = qs.filter(submission=self.submission).order_by('start').select_related('room')
         result = []
         other_submissions = schedule.slots.exclude(pk=self.submission.pk)
         for speaker in self.submission.speakers.all():
             speaker.talk_profile = speaker.event_profile(event=self.request.event)
             speaker.other_submissions = other_submissions.filter(speakers__in=[speaker]).select_related('event')
             result.append(speaker)
-        result['speakers'] = result
-        return result
+        ctx['speakers'] = result
+        return ctx
 
     @context
     @cached_property

src/tests/orga/views/test_orga_views_review.py
@@ -234,7 +234,7 @@ def test_orga_can_see_review(orga_client, review):
 @pytest.mark.django_db
 @pytest.mark.parametrize('sort', ('count', '-count', 'score', '-score'))
 def test_reviewer_can_see_dashboard(review_client, submission, review, sort, django_assert_num_queries, other_submission):
-    with django_assert_num_queries(53):
+    with django_assert_num_queries(49):
         response = review_client.get(submission.event.orga_urls.reviews + '?sort=' + sort)
     assert response.status_code == 200
 