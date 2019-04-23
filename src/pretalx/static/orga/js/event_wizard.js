91f800617359fb6e1cbd77a743ff66e1f3b6941d
Tobias Kunze <r@rixx.de>
2019-04-23 21:05:17 +0200

    ✨ Suggest a slug on event creation
    Just takes the first letters of the event name and adds the current
    year. Stops once the user has modified the slug field themself.
    
    Ref #326

---
+++ src/pretalx/static/orga/js/event_wizard.js
@@ -0,0 +1,10 @@
+const number = new Date().getYear() - 100;
+const updateSlug = (ev) => {
+    const value = document.querySelector('#event-name input').value;
+    const matches = value.match(/\b(\w)/g);
+    if (matches) document.querySelector('#id_basics-slug').value = matches.join('') + number;
+}
+document.querySelectorAll('#event-name input').forEach((element) => {element.addEventListener('input', updateSlug)})
+document.querySelector('#id_basics-slug').addEventListener('input', (ev) => {
+    document.querySelectorAll('#event-name input').forEach((element) => {element.removeEventListener('input', updateSlug)})
+})
