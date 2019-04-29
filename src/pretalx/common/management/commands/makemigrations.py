c5041ec4001c5ef66cdb48e718789a086de280a2
Tobias Kunze <r@rixx.de>
2019-04-29 10:32:24 +0200

    🚨 Fix breaking linting by adding #noqa + explanation

--- src/pretalx/common/management/commands/makemigrations.py
+++ src/pretalx/common/management/commands/makemigrations.py
@@ -1,4 +1,4 @@
-from django.core.management.commands.makemigrations import Command
+from django.core.management.commands.makemigrations import Command  # noqa
 from django.db import models
 
 IGNORED_ATTRS = ['verbose_name', 'help_text', 'choices']
@@ -7,9 +7,13 @@ original_deconstruct = models.Field.deconstruct
 
 
 def new_deconstruct(self):
+    """Patches makemigration's logic to ignore fields that are not of relevance to our database.
+
+    This avoids unnecessary, annoying migrations whenever a frontend facing string is changed."""
     name, path, args, kwargs = original_deconstruct(self)
     for attr in IGNORED_ATTRS:
         kwargs.pop(attr, None)
     return name, path, args, kwargs
+
 
 models.Field.deconstruct = new_deconstruct
