c30b6067b025d91077026eba0ae2a8430d87ab83
Tobias Kunze <r@rixx.de>
2019-05-08 23:40:34 +0200

    ✨ Show celery queue length in admin dashboard

--- src/pretalx/orga/views/admin.py
+++ src/pretalx/orga/views/admin.py
@@ -2,13 +2,24 @@ import sys
 
 from django.views.generic import TemplateView
 from django_context_decorator import context
+from djanog.conf import settings
 
+from pretalx.celery_app import app
 from pretalx.common.mixins.views import PermissionRequired
 
 
 class AdminDashboard(PermissionRequired, TemplateView):
     template_name = 'orga/admin.html'
     permission_required = 'person.is_administrator'
+
+    @context
+    def queue_length(self):
+        if settings.HAS_CELERY:
+            try:
+                client = app.broker_connection().channel().client
+                return client.llen('celery')
+            except Exception as e:
+                return str(e)
 
     @context
     def executable(self):
