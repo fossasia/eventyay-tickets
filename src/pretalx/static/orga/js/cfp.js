41590d8ebbfbb30ea95c8c1062734919773baade
Tobias Kunze <r@rixx.de>
2019-05-09 21:34:05 +0200

    🎀 Switch 'request' fields on when 'require' is switched on

---
+++ src/pretalx/static/orga/js/cfp.js
@@ -0,0 +1,7 @@
+document.querySelectorAll('#cfp-option-table .require input').forEach((element) => {
+    element.addEventListener('click', (ev) => {
+        if (ev.target.checked) {
+            ev.target.parentElement.parentElement.parentElement.parentElement.querySelector('.request input').checked = true
+        }
+    })
+})
