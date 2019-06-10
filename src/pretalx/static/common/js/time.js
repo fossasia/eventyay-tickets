b10ce7470488710c7d63191d2aeb582be37e2198
luto <m@luto.at>
2019-06-10 21:39:08 +0200

    [agenda] update "current talk"-highlight in public schedule via javascript

--- src/pretalx/static/common/js/time.js
+++ src/pretalx/static/common/js/time.js
@@ -11,8 +11,27 @@ $(function () {
         });
     }
 
+    function updateCurrentTalk() {
+        var now = moment();
+
+        $('.talk').each(function (index, element) {
+            element = $(element);
+            var start = moment(element.data('start'));
+            var end = moment(element.data('end'));
+
+            if(start < now && end > now) {
+                element.addClass('active');
+            } else {
+                element.removeClass('active');
+            }
+        });
+    }
+
     updateNowlines();
     $('.nowline').css('visibility', 'visible');
 
+    updateCurrentTalk();
+
     setInterval(updateNowlines, 60);
+    setInterval(updateCurrentTalk, 60);
 });
