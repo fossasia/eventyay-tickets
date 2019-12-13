e543da612fc94524b91530856b60771dc4b247b0
Tobias Kunze <r@rixx.de>
2019-12-13 15:36:57 +0100

    ⬆ Upgrade django-bootstrap4

--- src/setup.py
+++ src/setup.py
@@ -97,7 +97,7 @@ setup(
         'cssutils==1.0.*',  # https://pythonhosted.org/cssutils/CHANGELOG.html
         'defusedcsv==1.1.*',  # https://github.com/raphaelm/defusedcsv
         'Django>=2.2.1,<2.3.0',  # https://docs.djangoproject.com/en/2.0/releases/
-        'django-bootstrap4==0.0.8',  # http://django-bootstrap4.readthedocs.io/en/latest/history.html
+        'django-bootstrap4==1.1.1',  # http://django-bootstrap4.readthedocs.io/en/latest/history.html
         'django-compressor==2.3.*',  # https://django-compressor.readthedocs.io/en/latest/changelog/
         'django-context-decorator',
         'django-csp==3.5.*',  # https://github.com/mozilla/django-csp/blob/master/CHANGES
