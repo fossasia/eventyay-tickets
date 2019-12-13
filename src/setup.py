178e84c4188dd0bdeae46353d8ae91df2d72a809
Tobias Kunze <r@rixx.de>
2019-12-13 15:55:58 +0100

    ⬆ Update django-formtools, djangorestframework, whitenoise

--- src/setup.py
+++ src/setup.py
@@ -103,12 +103,12 @@ setup(
         'django-csp==3.5.*',  # https://github.com/mozilla/django-csp/blob/master/CHANGES
         'django-filter==2.2.*',  # https://github.com/carltongibson/django-filter/blob/master/CHANGES.rst
         'django-formset-js-improved==0.5.0.2',  # no changelog, https://github.com/pretix/django-formset-js
-        'django-formtools==2.1.*',  # http://django-formtools.readthedocs.io/en/latest/changelog.html
+        'django-formtools==2.2.*',  # http://django-formtools.readthedocs.io/en/latest/changelog.html
         'django-hierarkey==1.0.*',  # no changelog, https://github.com/raphaelm/django-hierarkey
         'django-i18nfield==1.6.*',  # 2017-11, no changelog, https://github.com/raphaelm/django-i18nfield/
         'django-libsass==0.7',  # inactive, https://github.com/torchbox/django-libsass/blob/master/CHANGELOG.txt
         'django-scopes==1.2.*',  # https://github.com/raphaelm/django-scopes/releases
-        'djangorestframework==3.10.*',  # http://www.django-rest-framework.org/topics/release-notes/
+        'djangorestframework==3.11.*',  # http://www.django-rest-framework.org/community/release-notes/
         'inlinestyler==0.2.*',  # https://github.com/dlanger/inlinestyler/blob/master/CHANGELOG
         'libsass==0.19.*',  # https://sass.github.io/libsass-python/changes.html
         'Markdown==3.1.*',  # https://python-markdown.github.io/change_log/
@@ -120,7 +120,7 @@ setup(
         'rules==2.1.*',  # https://github.com/dfunckt/django-rules/blob/master/CHANGELOG.md
         'urlman==1.3.*',  # https://github.com/andrewgodwin/urlman/blob/master/CHANGELOG
         'vobject==0.9.*',  # 2017-06, http://eventable.github.io/vobject/ look for "release"
-        'whitenoise==4.1.*',  # http://whitenoise.evans.io/en/stable/changelog.html
+        'whitenoise==5.0.*',  # http://whitenoise.evans.io/en/stable/changelog.html
         'zxcvbn==4.4.*',  # Nothing? https://github.com/dwolfhub/zxcvbn-python/issues/38
     ],
     extras_require={
