bd42c563f81e2eb531691ab11c9a4a009d599389
Tobias Kunze <r@rixx.de>
2020-01-29 02:54:50 +0100

    🚨 Fix breaking test

--- src/tests/submission/test_submission_model.py
+++ src/tests/submission/test_submission_model.py
@@ -245,7 +245,7 @@ def test_nonstandard_duration(submission):
 @pytest.mark.django_db
 def test_submission_image_path(submission):
     assert submission_image_path(submission, "foo").startswith(
-        f"{submission.event.slug}/images/{submission.code}"
+        f"{submission.event.slug}/submissions/{submission.code}"
     )
 
 
