2cf645a6c40ed09f9330bb22df3d397573214b47
Raphael Michel <mail@raphaelmichel.de>
2020-04-08 10:30:13 +0200

    Add AllowedHostsOriginValidator

--- video/server/stayseated/routing.py
+++ video/server/stayseated/routing.py
@@ -1,8 +1,13 @@
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
 
 from .live import routing as live
 
 application = ProtocolTypeRouter(
-    {"websocket": AuthMiddlewareStack(URLRouter(live.websocket_urlpatterns)),}
+    {
+        "websocket": AllowedHostsOriginValidator(
+            AuthMiddlewareStack(URLRouter(live.websocket_urlpatterns))
+        ),
+    }
 )
