02f575a89637ba12a8200d0e9bd706c6c75d1acc
Raphael Michel <mail@raphaelmichel.de>
2020-04-14 18:47:12 +0200

    Add docker-compose setup for development

---
+++ video/server/docker-dev-entrypoint.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Apply database migrations
+echo "Apply database migrations"
+python manage.py migrate
+
+# Start server
+echo "Starting server"
+exec python manage.py runserver 0.0.0.0:8000
