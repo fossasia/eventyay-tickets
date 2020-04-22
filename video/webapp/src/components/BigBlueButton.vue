3bae9a66502e9c7dc79200ebc993c663f15bd958
rashfael <rashfael@isobeef.org>
2020-04-22 21:15:55 +0200

    [webapp] integrate bbb

---
+++ video/webapp/src/components/BigBlueButton.vue
@@ -0,0 +1,50 @@
+<template lang="pug">
+.c-bigbluebutton
+	iframe(v-if="url", :src="url", allow="camera; autoplay; microphone; fullscreen; display-capture", allowfullscreen, allowusermedia)
+	error(v-else-if="error") Something went wrong!
+</template>
+<script>
+import api from 'lib/api'
+export default {
+	props: {
+		room: {
+			type: Object,
+			required: true
+		},
+		module: {
+			type: Object,
+			required: true
+		}
+	},
+	data () {
+		return {
+			url: null, // YOUR TEST URL HERE
+			error: null
+		}
+	},
+	computed: {},
+	async created () {
+		try {
+			const {url} = await api.call('bbb.url', {room: this.room.id})
+			this.url = url
+		} catch (error) {
+			// TODO handle bbb.join.missing_profile
+			this.error = error
+			console.log(error)
+		}
+	},
+	mounted () {
+		this.$nextTick(() => {
+		})
+	},
+	methods: {}
+}
+</script>
+<style lang="stylus">
+.c-bigbluebutton
+	flex: auto
+	iframe
+		height: 100%
+		width: 100%
+		border: none
+</style>
