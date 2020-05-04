bbc1bd014e1e9c2a2568b2fcab2d4b29105aee2a
Raphael Michel <mail@raphaelmichel.de>
2020-05-04 15:27:29 +0200

    Do not use mutable default for JSONField

--- video/server/venueless/core/models/world.py
+++ video/server/venueless/core/models/world.py
@@ -2,8 +2,8 @@ from django.contrib.postgres.fields import JSONField
 from django.db import models
 
 
-class World(models.Model):
-    DEFAULT_PERMISSIONS = {
+def default_permissions():
+    return {
         "world.update": ["admin"],
         "world.announce": ["admin"],
         "room.create": ["admin"],
@@ -12,9 +12,12 @@ class World(models.Model):
         "room.delete": ["admin"],
         "chat.moderate": ["admin"],
     }
+
+
+class World(models.Model):
     id = models.CharField(primary_key=True, max_length=50)
     title = models.CharField(max_length=300)
     about = models.TextField(null=True, blank=True)
     config = JSONField(null=True, blank=True)
-    permission_config = JSONField(null=True, blank=True, default=DEFAULT_PERMISSIONS)
+    permission_config = JSONField(null=True, blank=True, default=default_permissions)
     domain = models.CharField(max_length=250, unique=True, null=True, blank=True)
