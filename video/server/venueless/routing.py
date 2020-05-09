d662afefb914351c50c228aa4c7ce7d29f1aad3b
Raphael Michel <mail@raphaelmichel.de>
2020-05-09 15:20:04 +0200

    Remove cookie and session middleware

--- video/server/venueless/routing.py
+++ video/server/venueless/routing.py
@@ -1,15 +1,8 @@
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
-from channels.sessions import CookieMiddleware, SessionMiddleware
 
 from .live import routing as live
 
 application = ProtocolTypeRouter(
-    {
-        "websocket": AllowedHostsOriginValidator(
-            CookieMiddleware(
-                SessionMiddleware((URLRouter(live.websocket_urlpatterns)))
-            ),
-        ),
-    }
+    {"websocket": AllowedHostsOriginValidator(URLRouter(live.websocket_urlpatterns)),}
 )
