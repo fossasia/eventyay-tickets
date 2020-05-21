cc3041e537a650ad70524b2c750112902e9807d9
rashfael <rashfael@isobeef.org>
2020-05-21 15:18:35 +0200

    [webapp] use js files for localization

--- video/webapp/src/i18n.js
+++ video/webapp/src/i18n.js
@@ -4,7 +4,7 @@ import VueI18n from 'vue-i18n'
 Vue.use(VueI18n)
 
 function loadLocaleMessages () {
-	const locales = require.context('./locales', true, /[A-Za-z0-9-_,\s]+\.json$/i)
+	const locales = require.context('./locales', true, /[A-Za-z0-9-_,\s]+\.js$/i)
 	const messages = {}
 	locales.keys().forEach(key => {
 		const matched = key.match(/([A-Za-z0-9-_]+)\./i)
