403558f15a427ef88499146a954c77bd626c221b
rashfael <rashfael@isobeef.org>
2020-05-25 18:32:54 +0200

    [webapp] add browser block preloader

---
+++ video/webapp/src/preloader.js
@@ -0,0 +1,40 @@
+/* eslint no-eval: 0 */
+import Modernizr from 'modernizr'
+import 'styles/preloader.styl'
+
+const showBrowserBlock = function () {
+	document.getElementById('browser-block').style.display = null
+	document.body.removeChild(document.getElementById('v-app'))
+}
+
+// test syntax & API features
+;(function () {
+	try {
+		eval('const f=(a)=>a')
+		eval('function a(b) {for (let b;;);}')
+		eval('class __testfail {}')
+
+		// modernizr haz no object.values flag
+		if (typeof Object.values !== 'function') {
+			throw new Error()
+		}
+		for (const feature in Modernizr) {
+			if (!Modernizr[feature]) {
+				if (feature === 'webanimations') {
+					console.info('loading webanimations polyfill')
+					// eslint-disable-next-line
+					import(/* webpackChunkName: "polyfill-webanimations" */ 'web-animations-js')
+				} else {
+					throw new Error(`Browser feature missing: ${feature}`)
+				}
+			}
+		}
+
+		// load app
+		// eslint-disable-next-line
+		import(/* webpackChunkName: "app" */ './main')
+	} catch (e) {
+		console.error(e)
+		showBrowserBlock()
+	}
+})()
