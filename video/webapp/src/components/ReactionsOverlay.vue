5bddf0afb8884fa10fb0a2bba8f087baa6107480
Raphael Michel <mail@raphaelmichel.de>
2020-05-25 11:37:20 +0200

    Fix reactions overlay having width 0 on some screen widths

--- video/webapp/src/components/ReactionsOverlay.vue
+++ video/webapp/src/components/ReactionsOverlay.vue
@@ -104,4 +104,7 @@ export default {
 		display: inline-block
 		background-image: url("~emoji-datasource-twitter/img/twitter/sheets-256/64.png")
 		background-size: 5700% 5700%
+	+below('s')
+		width: 100vw
+		right: 0
 </style>
