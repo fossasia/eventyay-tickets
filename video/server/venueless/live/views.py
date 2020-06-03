2844ac6316f56be5f1bc01aad6d3a5dc71677c75
Raphael Michel <mail@raphaelmichel.de>
2020-06-03 09:49:03 +0200

    Set <title> tag to world title

--- video/server/venueless/live/views.py
+++ video/server/venueless/live/views.py
@@ -1,5 +1,6 @@
 import json
 import os
+import re
 
 from asgiref.sync import async_to_sync
 from django.http import HttpResponse, JsonResponse
@@ -63,6 +64,7 @@ class AppView(View):
     def get(self, request, *args, **kwargs):
         world = get_object_or_404(World, domain=request.headers["Host"])
         source = sh.source
+        source = re.sub("<title>[^<]*</title>", f"<title>{world.title}</title>", source, re.IGNORECASE | re.MULTILINE)
         source = source.replace(
             "<body>",
             "<script>window.venueless={}</script><body>".format(
