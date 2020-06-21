8bb6d92912d07efbc159aa12d740faa7e9c7037f
Raphael Michel <mail@raphaelmichel.de>
2020-06-21 15:45:19 +0200

    [webapp] add support for iframe-based rooms

---
+++ video/webapp/src/components/IframePage.vue
@@ -0,0 +1,41 @@
+<template lang="pug">
+.c-iframe
+	bunt-progress-circular(size="huge", :page="true")
+	iframe(:src="this.module.config.url", allow="camera; autoplay; microphone; fullscreen; display-capture", allowfullscreen, allowusermedia)
+</template>
+<script>
+import api from 'lib/api'
+export default {
+	props: {
+		room: {
+			type: Object,
+			required: true
+		},
+		module: {
+			type: Object,
+			required: true
+		}
+	},
+	data () {
+		return {
+			url: null,
+		}
+	},
+}
+</script>
+<style lang="stylus">
+.c-iframe
+	flex: auto
+	height: 100%
+	display: flex
+	flex-direction: column
+	position: relative
+	iframe
+		height: 100%
+		width: 100%
+		position: absolute
+		top: 0
+		left: 0
+		border: none
+		flex: auto // because safari
+</style>
--- video/webapp/src/views/rooms/item.vue
+++ video/webapp/src/views/rooms/item.vue
@@ -18,6 +18,7 @@
 				.stage-tool(v-if="$features.enabled('questions-answers')", :class="{active: activeStageTool === 'qa'}", @click="activeStageTool = 'qa'") Ask a question
 				reactions-bar(:expanded="activeStageTool === 'reaction'", @expand="activeStageTool = 'reaction'")
 		markdown-page(v-else-if="modules['page.markdown']", :module="modules['page.markdown']")
+		iframe-page(v-else-if="modules['page.iframe']", :module="modules['page.iframe']")
 		chat(v-if="modules['chat.native']", :room="room", :module="modules['chat.native']", :mode="room.modules.length === 1 ? 'standalone' : 'compact'", :key="room.id")
 </template>
 <script>
@@ -26,6 +27,7 @@ import moment from 'moment'
 import Chat from 'components/Chat'
 import Livestream from 'components/Livestream'
 import MarkdownPage from 'components/MarkdownPage'
+import IframePage from 'components/IframePage'
 import ReactionsBar from 'components/ReactionsBar'
 import ReactionsOverlay from 'components/ReactionsOverlay'
 
@@ -34,7 +36,7 @@ export default {
 	props: {
 		roomId: String
 	},
-	components: { Chat, Livestream, MarkdownPage, ReactionsBar, ReactionsOverlay },
+	components: { Chat, Livestream, MarkdownPage, IframePage, ReactionsBar, ReactionsOverlay },
 	data () {
 		return {
 			activeStageTool: null // reaction, qa
