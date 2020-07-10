bfc746d4b7a4b5e46b813438c181c9aae0f4e89d
Tobias Kunze <r@rixx.de>
2020-07-10 12:12:03 +0200

    🏇 Increase pagination size for small endpoints

--- src/pretalx/api/views/room.py
+++ src/pretalx/api/views/room.py
@@ -1,11 +1,16 @@
-from rest_framework import viewsets
+from rest_framework import pagination, viewsets
 
 from pretalx.api.serializers.room import RoomOrgaSerializer, RoomSerializer
 from pretalx.schedule.models import Room
 
 
+class RoomPagination(pagination.LimitOffsetPagination):
+    page_size = 100
+
+
 class RoomViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Room.objects.none()
+    pagination_class = RoomPagination
 
     def get_queryset(self):
         if self.request.user.has_perm("agenda.view_schedule", self.request.event):
