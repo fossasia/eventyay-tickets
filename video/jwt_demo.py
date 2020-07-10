0146bb1077a61d997d00e3beb4c0f539bf0bc085
rashfael <rashfael@isobeef.org>
2020-07-10 17:08:54 +0200

    use utcnow as uid for token generator script to get new user

--- video/jwt_demo.py
+++ video/jwt_demo.py
@@ -8,7 +8,7 @@ audience = "demo-event"
 
 def create_token(user_id, traits):
     iat = datetime.datetime.utcnow()
-    exp = iat + datetime.timedelta(days=2)
+    exp = iat + datetime.timedelta(days=256)
     payload = {
         "iss": issuer,
         "aud": audience,
@@ -28,7 +28,7 @@ def decode_token(token):
 
 
 print("creating token")
-token = create_token("some-user-id", ["admin"])
+token = create_token(str(datetime.datetime.utcnow().timestamp()), ["admin"])
 print(token)
 print("verifying token")
 print(decode_token(token))
