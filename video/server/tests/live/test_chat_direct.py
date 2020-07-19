ed18e6b1529b57d9d13458cacc083fa1b96ed158
Raphael Michel <mail@raphaelmichel.de>
2020-07-19 15:11:35 +0200

    Adjust tests to new semantics

--- video/server/tests/live/test_chat_direct.py
+++ video/server/tests/live/test_chat_direct.py
@@ -89,6 +89,23 @@ async def test_start_direct_channel(world):
         assert "state" in response[2]
         assert "a" in {a["profile"]["display_name"] for a in response[2]["members"]}
         assert "b" in {a["profile"]["display_name"] for a in response[2]["members"]}
+        await c1.receive_json_from()  # join event
+        await c1.receive_json_from()  # join event
+
+        await c1.send_json_to(
+            [
+                "chat.send",
+                123,
+                {
+                    "event_type": "channel.message",
+                    "content": {"type": "text", "body": "Hello world"},
+                    "channel": response[2]["id"],
+                },
+            ]
+        )
+        response = await c1.receive_json_from()
+        assert "success" == response[0]
+        await c1.receive_json_from()
 
         response = await c2.receive_json_from()  # channel list
         assert response[0] == "chat.channels"
@@ -98,6 +115,7 @@ async def test_start_direct_channel(world):
         assert "b" in {
             a["profile"]["display_name"] for a in response[1]["channels"][0]["members"]
         }
+        await c2.receive_json_from()  # notification pointer
 
 
 @pytest.mark.asyncio
@@ -137,7 +155,6 @@ async def _setup_dms(c1, c2):
 
     await c1.receive_json_from()  # join event 1
     await c1.receive_json_from()  # join event 2
-    await c2.receive_json_from()  # channel list
 
     return channel
 
@@ -202,7 +219,7 @@ async def test_do_not_reuse_direct_channel_with_additional_user(world):
     await database_sync_to_async(world.save)()
     async with world_communicator(client_id="a") as c1, world_communicator(
         client_id="b"
-    ) as c2, world_communicator(client_id="c") as c3:
+    ), world_communicator(client_id="c"):
         await c1.send_json_to(
             [
                 "chat.direct.create",
@@ -234,8 +251,6 @@ async def test_do_not_reuse_direct_channel_with_additional_user(world):
         await c1.receive_json_from()  # join event 1
         await c1.receive_json_from()  # join event 2
         await c1.receive_json_from()  # join event 3
-        await c2.receive_json_from()  # channel list
-        await c3.receive_json_from()  # channel list
 
         await c1.send_json_to(
             [
@@ -471,6 +486,7 @@ async def test_send_if_blocked_by_user(world):
 
         await c1.receive_json_from()  # chat event
         await c1.receive_json_from()  # new notification pointer
+        await c2.receive_json_from()  # channel list
         await c2.receive_json_from()  # new notification pointer
 
         await c2.send_json_to(
@@ -612,7 +628,9 @@ async def test_hide_and_reappear(world):
     await database_sync_to_async(world.save)()
     async with world_communicator(client_id="a") as c1, world_communicator(
         client_id="b"
-    ) as c2:
+    ) as c2, world_communicator(client_id="b") as c2b:
+        await c2.receive_json_from()  # user updated
+
         await c1.send_json_to(
             [
                 "chat.direct.create",
@@ -635,18 +653,37 @@ async def test_hide_and_reappear(world):
         channel = response[2]["id"]
 
         await c1.receive_json_from()  # join event 1
-        await c1.receive_json_from()  # join event 2
-        await c1.receive_json_from()  # channel list
+        await c1.receive_json_from()  # join event ^2
+
+        await c1.send_json_to(
+            [
+                "chat.send",
+                123,
+                {
+                    "event_type": "channel.message",
+                    "content": {"type": "text", "body": "Hello world"},
+                    "channel": channel,
+                },
+            ]
+        )
+        response = await c1.receive_json_from()
+        assert "success" == response[0]
+        await c1.receive_json_from()  # message
+        await c1.receive_json_from()  # notification pointer
 
         cl = await c2.receive_json_from()  # channel list
         assert channel in [c["id"] for c in cl[1]["channels"]]
+        await c2.receive_json_from()  # notification pointer
+
+        await c2b.receive_json_from()  # channel list
+        await c2b.receive_json_from()  # notification pointer
 
         await c2.send_json_to(
             ["chat.leave", 123, {"channel": channel},]
         )
         response = await c2.receive_json_from()
         assert "success" == response[0]
-        cl = await c2.receive_json_from()  # channel list
+        cl = await c2b.receive_json_from()  # channel list
         assert channel not in [c["id"] for c in cl[1]["channels"]]
 
         await c1.send_json_to(
@@ -667,3 +704,5 @@ async def test_hide_and_reappear(world):
         cl = await c2.receive_json_from()  # channel list
         assert channel in [c["id"] for c in cl[1]["channels"]]
         await c2.receive_json_from()  # notification pointer
+        await c2b.receive_json_from()  # channel list
+        await c2b.receive_json_from()  # notification pointer
