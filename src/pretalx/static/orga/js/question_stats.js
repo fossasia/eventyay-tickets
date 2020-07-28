8accde1b9da7a1316d143289c300f06cdd4cf20e
Tobias Kunze <r@rixx.de>
2020-07-28 20:44:51 +0200

    🎀 Pretty question stats

--- src/pretalx/static/orga/js/question_stats.js
+++ src/pretalx/static/orga/js/question_stats.js
@@ -1,23 +1,66 @@
-nv.addGraph(() => {
-  try {
-    const data = JSON.parse(
-      document.getElementById("question-data").dataset.states
-    )
-    let chart = nv.models
-      .pieChart()
-      .x(d => d.answer || d.options__answer)
-      .y(d => d.count)
-      .valueFormat(d3.format("d"))
-      .labelsOutside(true)
-      .legendPosition("right")
-      .showLabels(false)
-    d3.select("#question-answers svg")
-      .datum(data)
-      .call(chart)
-    nv.utils.windowResize(() => {
-      chart.update()
-    }) //Update the chart when window resizes.
-    chart.update()
-    return chart
-  } catch {}
-})
+const serverData = document.getElementById("question-data")
+const canvas = document.getElementById("question-answers")
+const data = JSON.parse(serverData.dataset.states)
+let url = serverData.dataset.url
+const options = {
+  series: data.map(e => e.count),
+  labels: data.map(e => e.answer || e.options__answer),
+  chart: {
+    width: 420,
+    type: 'donut',
+    events: {
+      dataPointSelection: (event, chartContext, config) => {
+        const clickedData = data[config.dataPointIndex]
+        if (clickedData.answer) {
+          url = url + "answer=" + encodeURIComponent(clickedData.answer)
+        } else {
+          url = url + "answer__options=" + encodeURIComponent(clickedData.options)
+        }
+        window.location.href = url
+      },
+      dataPointMouseEnter: () => {
+        canvas.style.cursor = "pointer"
+      },
+      dataPointMouseLeave: () => {
+        canvas.style.cursor = "inherit"
+      },
+    },
+  },
+  dataLabels: {
+    enabled: false
+  },
+  fill: {
+    type: 'gradient',
+  },
+  legend: {
+    formatter: function(val, opts) {
+      return val + " - " + opts.w.globals.series[opts.seriesIndex]
+    }
+  },
+  responsive: [{
+    breakpoint: 480,
+    options: {
+      chart: {
+        width: 200
+      },
+      legend: {
+        position: 'bottom'
+      }
+    }
+  }],
+  plotOptions: {
+    pie: {
+      donut: {
+        labels: {
+          show: true
+        }
+      }
+    }
+  },
+  tooltip: {
+    enabled: false
+  }
+};
+
+let chart = new ApexCharts(document.querySelector("#question-answers"), options);
+chart.render();
