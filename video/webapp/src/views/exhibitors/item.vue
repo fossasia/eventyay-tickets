88f33fbb0e4e5cb1a82dba6ca94093d43ee68927
rashfael <rashfael@isobeef.org>
2020-08-02 21:06:18 +0200

    [webapp] mock exhibitor downloads

--- video/webapp/src/views/exhibitors/item.vue
+++ video/webapp/src/views/exhibitors/item.vue
@@ -3,7 +3,19 @@ scrollbars.c-exhibitor(y)
 	.content-wrapper(v-if="exhibitor")
 		.content
 			//- TODO banner
+			img.banner(src="/sponsors/venueless-stage.png")
 			markdown-content.text(:markdown="exhibitor.text")
+			.downloads
+				h2 Downloads
+				a.download(href="#")
+					.mdi.mdi-file-pdf-outline
+					.filename brochure.pdf
+				a.download(href="#")
+					.mdi.mdi-file-pdf-outline
+					.filename datasheet-product-XLW23030.pdf
+				a.download(href="#")
+					.mdi.mdi-file-powerpoint-outline
+					.filename some-weird-presentation.ppx
 		.sidebar
 			.header
 				img.logo(:src="exhibitor.logo")
@@ -62,6 +74,10 @@ export default {
 		width: 100%
 		max-width: 720px
 		padding: 0 16px 0 0
+		img.banner
+			object-fit: contain
+			width: 100%
+			margin-top: 16px
 	.sidebar
 		flex: none
 		min-height: min-content
@@ -71,6 +87,7 @@ export default {
 		border: border-separator()
 		border-radius: 4px
 		margin-top: 16px
+		align-self: flex-start
 		.header
 			flex: none
 			display: flex
@@ -115,6 +132,8 @@ export default {
 				width: 100%
 			.name
 				white-space: nowrap
+			a:hover
+					text-decoration: underline
 		.contact
 			flex: none
 			padding: 8px
@@ -122,4 +141,25 @@ export default {
 			flex-direction: column
 			.bunt-button
 				themed-button-primary()
+	.downloads
+		border: border-separator()
+		border-radius: 4px
+		display: flex
+		flex-direction: column
+		margin-top: 16px
+		h2
+			margin: 4px 8px
+		.download
+			display: flex
+			align-items: center
+			height: 56px
+			font-weight: 600
+			font-size: 16px
+			border-top: border-separator()
+			&:hover
+				background-color: $clr-grey-100
+				text-decoration: underline
+			.mdi
+				font-size: 36px
+				margin: 0 4px
 </style>
