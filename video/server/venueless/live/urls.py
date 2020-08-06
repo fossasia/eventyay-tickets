503491de7b179d2d5d6778ab336ab1016353bcd7
Raphael Michel <mail@raphaelmichel.de>
2020-08-06 18:14:23 +0200

    Fix URL conflict

--- video/server/venueless/live/urls.py
+++ video/server/venueless/live/urls.py
@@ -3,6 +3,6 @@ from django.urls import re_path
 from . import views
 
 urlpatterns = [
-    re_path("custom.css$", views.CustomCSSView.as_view(), name="css.custom"),
-    re_path("bbb.css$", views.BBBCSSView.as_view(), name="css.bbb"),
+    re_path("_custom.css$", views.CustomCSSView.as_view(), name="css.custom"),
+    re_path("_bbb.css$", views.BBBCSSView.as_view(), name="css.bbb"),
 ]
