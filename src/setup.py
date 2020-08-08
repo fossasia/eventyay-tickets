12e5e5c231265e4adcac472a60ab7a3cfdd176e4
Tobias Kunze <r@rixx.de>
2020-08-08 22:02:52 +0200

    ⬆ Move to more readable dependency format

--- src/setup.py
+++ src/setup.py
@@ -91,40 +91,40 @@ setup(
     keywords="conference cfp event schedule",
     python_requires=">={}.{}".format(*REQUIRED_PYTHON),
     install_requires=[
-        "beautifulsoup4==4.9.*",  # https://bazaar.launchpad.net/~leonardr/beautifulsoup/bs4/view/head:/CHANGELOG
-        "bleach==3.1.*",  # https://bleach.readthedocs.io/en/latest/changes.html
-        "celery==4.4.*",  # search for "what's new" on http://docs.celeryproject.org/en/latest/
-        "csscompressor==0.9.*",  # 2017-11, no changelog, https://github.com/sprymix/csscompressor
-        "cssutils==1.0.*",  # https://pythonhosted.org/cssutils/CHANGELOG.html
-        "defusedcsv==1.1.*",  # https://github.com/raphaelm/defusedcsv
-        "Django==3.0.*",  # https://docs.djangoproject.com/en/2.0/releases/
-        "django-bootstrap4==2.0.*",  # http://django-bootstrap4.readthedocs.io/en/latest/history.html
-        "django-compressor==2.4.*",  # https://django-compressor.readthedocs.io/en/latest/changelog/
+        "beautifulsoup4~=4.9.0",  # https://bazaar.launchpad.net/~leonardr/beautifulsoup/bs4/view/head:/CHANGELOG
+        "bleach~=3.1.0",  # https://bleach.readthedocs.io/en/latest/changes.html
+        "celery~=4.4.0",  # search for "what's new" on http://docs.celeryproject.org/en/latest/
+        "csscompressor~=0.9.0",  # 2017-11, no changelog, https://github.com/sprymix/csscompressor
+        "cssutils~=1.0.0",  # https://pythonhosted.org/cssutils/CHANGELOG.html
+        "defusedcsv~=1.1.0",  # https://github.com/raphaelm/defusedcsv
+        "Django~=3.0.0",  # https://docs.djangoproject.com/en/2.0/releases/
+        "django-bootstrap4~=2.0.0",  # http://django-bootstrap4.readthedocs.io/en/latest/history.html
+        "django-compressor~=2.4.0",  # https://django-compressor.readthedocs.io/en/latest/changelog/
         "django-context-decorator",
-        "django-countries==6.1.*",  # https://github.com/SmileyChris/django-countries/blob/master/CHANGES.rst
-        "django-csp==3.6.*",  # https://github.com/mozilla/django-csp/blob/master/CHANGES
-        "django-filter==2.3.*",  # https://github.com/carltongibson/django-filter/blob/master/CHANGES.rst
+        "django-countries~=6.1.0",  # https://github.com/SmileyChris/django-countries/blob/master/CHANGES.rst
+        "django-csp~=3.6.0",  # https://github.com/mozilla/django-csp/blob/master/CHANGES
+        "django-filter~=2.3.0",  # https://github.com/carltongibson/django-filter/blob/master/CHANGES.rst
         "django-formset-js-improved==0.5.0.2",  # no changelog, https://github.com/pretix/django-formset-js
-        "django-formtools==2.2.*",  # http://django-formtools.readthedocs.io/en/latest/changelog.html
-        "django-hierarkey==1.0.*",  # no changelog, https://github.com/raphaelm/django-hierarkey
-        "django-i18nfield==1.8.*",  # 2020-06, no changelog, https://github.com/raphaelm/django-i18nfield/
-        "django-libsass==0.8",  # inactive, https://github.com/torchbox/django-libsass/blob/master/CHANGELOG.txt
-        "django-scopes==1.2.*",  # https://github.com/raphaelm/django-scopes/releases
-        "djangorestframework==3.11.*",  # http://www.django-rest-framework.org/community/release-notes/
-        "inlinestyler==0.2.*",  # https://github.com/dlanger/inlinestyler/blob/master/CHANGELOG
-        "libsass==0.20.*",  # https://sass.github.io/libsass-python/changes.html
-        "Markdown==3.2.*",  # https://python-markdown.github.io/change_log/
-        "publicsuffixlist==0.7.*",
-        "python-dateutil==2.8.*",  # https://dateutil.readthedocs.io/en/stable/changelog.html
+        "django-formtools~=2.2.0",  # http://django-formtools.readthedocs.io/en/latest/changelog.html
+        "django-hierarkey~=1.0.0",  # no changelog, https://github.com/raphaelm/django-hierarkey
+        "django-i18nfield~=1.8.0",  # 2020-06, no changelog, https://github.com/raphaelm/django-i18nfield/
+        "django-libsass~=0.8",  # inactive, https://github.com/torchbox/django-libsass/blob/master/CHANGELOG.txt
+        "django-scopes~=1.2.0",  # https://github.com/raphaelm/django-scopes/releases
+        "djangorestframework~=3.11.0",  # http://www.django-rest-framework.org/community/release-notes/
+        "inlinestyler~=0.2.0",  # https://github.com/dlanger/inlinestyler/blob/master/CHANGELOG
+        "libsass~=0.20.0",  # https://sass.github.io/libsass-python/changes.html
+        "Markdown~=3.2.0",  # https://python-markdown.github.io/change_log/
+        "publicsuffixlist~=0.7.0",
+        "python-dateutil~=2.8.0",  # https://dateutil.readthedocs.io/en/stable/changelog.html
         "pytz",
-        "qrcode==6.1",  # https://github.com/lincolnloop/python-qrcode/blob/master/CHANGES.rst
-        "reportlab==3.5.*",  # https://www.reportlab.com/documentation/relnotes/
-        "requests==2.24.*",  # https://2.python-requests.org/en/master/community/updates/#release-and-version-history
-        "rules==2.2.*",  # https://github.com/dfunckt/django-rules/blob/master/CHANGELOG.md
-        "urlman==1.3.*",  # https://github.com/andrewgodwin/urlman/blob/master/CHANGELOG
-        "vobject==0.9.*",  # 2017-06, http://eventable.github.io/vobject/ look for "release"
-        "whitenoise==5.1.*",  # http://whitenoise.evans.io/en/stable/changelog.html
-        "zxcvbn==4.4.*",  # Nothing? https://github.com/dwolfhub/zxcvbn-python/issues/38
+        "qrcode~=6.1",  # https://github.com/lincolnloop/python-qrcode/blob/master/CHANGES.rst
+        "reportlab~=3.5.0",  # https://www.reportlab.com/documentation/relnotes/
+        "requests~=2.24.0",  # https://2.python-requests.org/en/master/community/updates/#release-and-version-history
+        "rules~=2.2.0",  # https://github.com/dfunckt/django-rules/blob/master/CHANGELOG.md
+        "urlman~=1.3.0",  # https://github.com/andrewgodwin/urlman/blob/master/CHANGELOG
+        "vobject~=0.9.0",  # 2017-06, http://eventable.github.io/vobject/ look for "release"
+        "whitenoise~=5.2.0",  # http://whitenoise.evans.io/en/stable/changelog.html
+        "zxcvbn~=4.4.0",  # Nothing? https://github.com/dwolfhub/zxcvbn-python/issues/38
     ],
     extras_require={
         "dev": [
@@ -150,7 +150,7 @@ setup(
         ],
         "mysql": ["mysqlclient"],
         "postgres": ["psycopg2-binary"],
-        "redis": ["django_redis==4.12.*", "redis==3.5.*",],
+        "redis": ["django_redis~=4.12.0", "redis~=3.5.0",],
     },
     packages=find_packages(exclude=["tests", "tests.*"]),
     include_package_data=True,
