0d99d7fd1641f6782b4703eec9735046e1862ecd
rashfael <rashfael@isobeef.org>
2020-08-14 23:58:20 +0200

    [server] correctly handle room descriptions

--- video/server/venueless/core/services/world.py
+++ video/server/venueless/core/services/world.py
@@ -61,6 +61,7 @@ def get_room_config(room, permissions):
     room_config = {
         "id": str(room.id),
         "name": room.name,
+        "description": room.description,
         "picture": room.picture.url if room.picture else None,
         "import_id": room.import_id,
         "pretalx_id": room.pretalx_id,
@@ -170,6 +171,7 @@ async def create_room(world, data, creator):
         {
             "world": world,
             "name": data["name"],
+            "description": data["description"],
             "module_config": data.get("modules", []),
         },
         permission_preset=data.get("permission_preset", "public"),
