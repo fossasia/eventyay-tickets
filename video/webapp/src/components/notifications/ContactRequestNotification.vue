df5f5a841866c766a924d55c784312ae140cc9e3
Arne Rick <rick@rami.io>
2020-08-17 12:28:42 +0200

    [webapp] Dismiss contact request & timer indicator

--- video/webapp/src/components/notifications/ContactRequestNotification.vue
+++ video/webapp/src/components/notifications/ContactRequestNotification.vue
@@ -1,11 +1,14 @@
 <template lang="pug">
-.c-contact-request-notification
+.c-contact-request-notification(v-if="showNotification")
+	bunt-icon-button#btn-close(@click="showNotification=false") close
 	.details {{ $t('ContactRequest:notification:text') }} #[br] #[span.exhibitor {{ contactRequest.exhibitor.name }}]
 	.user
 		avatar(:user="contactRequest.user", :size="36")
 		.display-name {{ contactRequest.user ? contactRequest.user.profile.display_name : '' }}
 	.actions
 		bunt-button#btn-accept(@click="$store.dispatch('exhibition/acceptContactRequest', contactRequest)") {{ $t('ContactRequest:accept-button:label') }}
+	.timer
+		.timer-bar
 </template>
 <script>
 import Avatar from 'components/Avatar'
@@ -17,6 +20,7 @@ export default {
 	components: { Avatar },
 	data () {
 		return {
+			showNotification: true,
 		}
 	},
 	computed: {},
@@ -35,6 +39,10 @@ export default {
 	flex-direction: column
 	margin: 4px 0
 	padding: 8px
+	#btn-close
+		icon-button-style(style: clear)
+		position: absolute
+		right: 8px
 	span.exhibitor
 		font-weight: 500
 	.user
@@ -49,4 +57,15 @@ export default {
 		justify-content: flex-end
 	#btn-accept
 		themed-button-secondary()
+	.timer
+		display: block
+		height: 3.5px
+		.timer-bar
+			height: 100%
+			background-color: $clr-primary
+			animation: timerBar 30s linear
+			animation-fill-mode: forwards
+		@keyframes timerBar
+			0% { width: 100% }
+			100% { width: 0 }
 </style>
