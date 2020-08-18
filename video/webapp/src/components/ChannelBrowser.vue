83f26904f6b53b2f3b45b7932401e9804f3d500f
rashfael <rashfael@isobeef.org>
2020-08-18 20:56:57 +0200

    [webapp] add search to channel browser

--- video/webapp/src/components/ChannelBrowser.vue
+++ video/webapp/src/components/ChannelBrowser.vue
@@ -3,8 +3,9 @@ prompt.c-channel-browser(@close="$emit('close')", :scrollable="false")
 	.content
 		h2 Explore rooms
 		p bleep blorp, #[a(href="#", @click="$emit('createChannel')") create a new channel]?
-		.channels(v-scrollbar.y="")
-			router-link.channel(v-for="channel of channels", :to="{name: 'room', params: {roomId: channel.room.id}}", @click.native="$emit('close')")
+		bunt-input(icon="search", name="search", placeholder="Search rooms", v-model="search")
+		scrollbars.channels(y)
+			router-link.channel(v-for="channel of searchedChannels", :to="{name: 'room', params: {roomId: channel.room.id}}", @click.native="$emit('close')")
 				.channel-info
 					.name {{ channel.room.name }}
 					.description {{ channel.room.description }}
@@ -14,15 +15,18 @@ prompt.c-channel-browser(@close="$emit('close')", :scrollable="false")
 					template(v-else)
 						bunt-button#btn-preview preview
 						bunt-button#btn-join(@click="join(channel)") join
+			.no-results(v-if="search && searchedChannels.length === 0") Sorry, could not find any matching channels
 </template>
 <script>
 import { mapState } from 'vuex'
 import Prompt from 'components/Prompt'
+import fuzzysearch from 'lib/fuzzysearch'
 
 export default {
 	components: { Prompt },
 	data () {
 		return {
+			search: ''
 		}
 	},
 	computed: {
@@ -32,20 +36,19 @@ export default {
 			return this.rooms
 				.filter(room => room.modules.length === 1 && room.modules[0].type === 'chat.native')
 				.map(room => ({room, channelJoined: this.joinedChannels.some(channel => channel.id === room.modules[0].channel_id)}))
+		},
+		searchedChannels () {
+			if (!this.search) return this.channels
+			return this.channels.filter(channel => fuzzysearch(this.search.toLowerCase(), channel.room.name.toLowerCase()) || fuzzysearch(this.search.toLowerCase(), channel.room.description?.toLowerCase()))
 		}
-	},
-	created () {},
-	mounted () {
-		this.$nextTick(() => {
-		})
-	},
-	methods: {}
+	}
 }
 </script>
 <style lang="stylus">
 .c-channel-browser
 	.prompt-wrapper
 		width: 580px
+		height: 80vh
 	.content
 		min-height: 0
 		display: flex
@@ -56,14 +59,15 @@ export default {
 		margin: 0 16px 8px 16px
 		a
 			font-weight: 600
+	.bunt-input
+		margin: 0 16px
 	.channels
-		display: flex
-		flex-direction: column
 		.channel
 			padding: 16px
 			display: flex
 			align-items: center
-			border-bottom: border-separator()
+			&:not(:first-child)
+				border-top: border-separator()
 			.channel-info
 				flex: auto
 				color: $clr-primary-text-light
@@ -78,4 +82,7 @@ export default {
 					themed-button-secondary()
 				#btn-join
 					themed-button-primary()
+		.no-results
+			margin: 16px
+			text-align: center
 </style>
