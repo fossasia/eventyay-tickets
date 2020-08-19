c21311047ef9f51e98585749e1f2b2d8ecb40c35
Raphael Michel <mail@raphaelmichel.de>
2020-08-19 09:22:01 +0200

    Improve i18n and German translation

--- video/webapp/src/locales/en.js
+++ video/webapp/src/locales/en.js
@@ -91,5 +91,6 @@ export default {
 	'UserActionPrompt:action.unblock:execute:label': 'unblock',
 	'UserActionPrompt:action.unblock:question': 'Unblock this user?',
 	'UserActionPrompt:action.unblock:confirmation': 'User has been unblocked.',
-	'UserActionPrompt:action.unblock:explanation': 'Unblocking this user will allow them to send direct messages to you again.'
+	'UserActionPrompt:action.unblock:explanation': 'Unblocking this user will allow them to send direct messages to you again.',
+	'Schedule:headline:text': 'Schedule'
 }
