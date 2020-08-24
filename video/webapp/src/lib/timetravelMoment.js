4e32032dfc38810a9606957fa1210f2ecda0b7d3
rashfael <rashfael@isobeef.org>
2020-08-24 09:32:03 +0200

    [schedule] timetravel only on development

--- video/webapp/src/lib/timetravelMoment.js
+++ video/webapp/src/lib/timetravelMoment.js
@@ -1,3 +1,4 @@
+/* global ENV_DEVELOPMENT */
 // moment does not let us clone and only locally override `now`
 // we need to get a clean instance to manipulate
 delete require.cache[require.resolve('moment')]
@@ -5,6 +6,9 @@ var moment = require('moment')
 moment.locale('en-ie') // use ireland for 24h formatting
 delete require.cache[require.resolve('moment')]
 
-moment.now = function () { return 1587206880000 }
-console.log(moment()._d)
+if (ENV_DEVELOPMENT) {
+	moment.now = function () { return 1587206880000 }
+	console.warn('timetraveling to', moment()._d)
+}
+
 export default moment
