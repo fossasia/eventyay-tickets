ae835c70d0f4ff27e9eb8112815bfa3291ac42c1
Raphael Michel <mail@raphaelmichel.de>
2020-08-24 12:49:56 +0200

    Support date locale on server

--- video/webapp/src/components/config/MainConfig.vue
+++ video/webapp/src/components/config/MainConfig.vue
@@ -5,6 +5,7 @@
 	.main-form(v-if="config != null")
 		bunt-input(v-model="config.title", label="Title", name="title", :validation="$v.config.title")
 		bunt-select(v-model="config.locale", label="Language", name="locale", :options="locales")
+		bunt-input(v-model="config.dateLocale", label="Date locale", name="dateLocale")
 		bunt-input(v-model="config.timezone", label="Time zone", name="timezone", :validation="$v.config.timezone")
 		bunt-input(v-model="config.connection_limit", label="Connection limit", name="connection_limit", hint="Set to 0 to allow unlimited connections per user", :validation="$v.config.connection_limit")
 		bunt-input(v-model="config.pretalx.domain", label="pretalx domain", name="pretalx_domain", :validation="$v.config.pretalx.domain")
@@ -70,6 +71,7 @@ export default {
 			await api.call('world.config.patch', {
 				title: this.config.title,
 				locale: this.config.locale,
+				dateLocale: this.config.dateLocale,
 				timezone: this.config.timezone,
 				connection_limit: this.config.connection_limit,
 				bbb_defaults: this.config.bbb_defaults,
