b524f3843f20e5e8022377924df23cc3f160cd83
rashfael <rashfael@isobeef.org>
2020-08-27 10:36:00 +0200

    [webapp] new user select component

---
+++ video/webapp/src/components/CreateDmPrompt.vue
@@ -0,0 +1,73 @@
+<template lang="pug">
+prompt.c-create-dm-prompt(:scrollable="false", @close="$emit('close')")
+	.content
+		h1 {{ $t('CreateDMPrompt:headline:text') }}
+		p {{ $t('CreateDMPrompt:intro:text') }}
+		user-select(:button-label="$t('CreateDMPrompt:create-button:label')", @selected="create")
+</template>
+<script>
+import {mapGetters} from 'vuex'
+import Prompt from 'components/Prompt'
+import UserSelect from 'components/UserSelect'
+
+export default {
+	components: { Prompt, UserSelect },
+	data () {
+		return {
+			selectedUser: null
+		}
+	},
+	computed: {
+		...mapGetters(['hasPermission'])
+	},
+	methods: {
+		async create (users) {
+			// TODO error handling
+			await this.$store.dispatch('chat/openDirectMessage', {users: users})
+		}
+	}
+}
+</script>
+<style lang="stylus">
+.c-create-dm-prompt
+	.prompt-wrapper
+		height: 80vh
+		width: 600px
+	.content
+		display: flex
+		flex-direction: column
+		position: relative
+		box-sizing: border-box
+		min-height: 0
+		#btn-close
+			icon-button-style(style: clear)
+			position: absolute
+			top: 8px
+			right: 8px
+		h1
+			margin: 0
+			text-align: center
+		p
+			max-width: 320px
+		form
+			display: flex
+			flex-direction: column
+			align-self: stretch
+			.bunt-button
+				themed-button-primary()
+				margin-top: 16px
+			.bunt-select
+				select-style(size: compact)
+				ul li
+					display: flex
+					.mdi
+						margin-right: 8px
+			.bunt-input-outline-container
+				textarea
+					background-color: transparent
+					border: none
+					outline: none
+					resize: vertical
+					min-height: 64px
+					padding: 0 8px
+</style>
