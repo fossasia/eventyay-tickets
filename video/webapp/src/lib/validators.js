15a413cae136810eb1b55b1f7b06b2c2f13a44d7
rashfael <rashfael@isobeef.org>
2020-08-29 21:35:58 +0200

    commit missing file

--- video/webapp/src/lib/validators.js
+++ video/webapp/src/lib/validators.js
@@ -6,7 +6,21 @@ const required = message => withParams({message}, req)
 // TODO does not correctly detect localhost urls
 const url = message => withParams({message}, _url)
 
+const isJson = () => withParams((addParams) => {
+	return value => {
+		if (!value || value.length === 0) return true
+		try {
+			JSON.parse(value)
+			return true
+		} catch (exception) {
+			addParams({message: exception.message})
+		}
+		return false
+	}
+})
+
 export {
 	required,
-	url
+	url,
+	isJson
 }
