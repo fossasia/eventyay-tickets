41967aa220c76979ed520b00b27939c471c599dc
rashfael <rashfael@isobeef.org>
2020-08-29 09:52:49 +0200

    [webapp] also apply moment locale to non-timetravel instance

--- video/webapp/src/lib/timetravelMoment.js
+++ video/webapp/src/lib/timetravelMoment.js
@@ -2,9 +2,13 @@
 // we need to get a clean instance to manipulate
 import config from 'config'
 delete require.cache[require.resolve('moment')]
-var moment = require('moment')
+const moment = require('moment')
 moment.locale(config.dateLocale || 'en-ie') // use ireland for 24h clock
 delete require.cache[require.resolve('moment')]
+
+// conf the global moment instance here
+const mainMoment = require('moment')
+mainMoment.locale(config.dateLocale || 'en-ie') // use ireland for 24h clock
 
 if (config.timetravelTo) {
 	const timetravelTimestamp = moment(config.timetravelTo).valueOf()
