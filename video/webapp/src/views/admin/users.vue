b2b4ad07d3aa07b3d50303a9b8df074b52304d3e
rashfael <rashfael@isobeef.org>
2020-09-02 01:11:39 +0200

    [webapp] clean up user badges
    - use .ui- class prefix for common class
    - use absolute sizes
    - fix pug markup
    - replace $clr-primary with themed variable
    - more round edge to make it look less like a keyboard focus outline

--- video/webapp/src/views/admin/users.vue
+++ video/webapp/src/views/admin/users.vue
@@ -16,9 +16,9 @@
 				avatar.avatar(:user="user", :size="32")
 				.id(:title="user.id") {{ user.id }}
 				.tokenid(:title="user.token_id") {{ user.token_id }}
-				.name {{ user.profile.display_name }}
-					|
-					.user-badge(v-for="b in user.badges") {{ b }}
+				.name
+					| {{ user.profile.display_name }}
+					.ui-badge(v-for="badge in sender.badges") {{ badge }}
 				.state {{ user.moderation_state }}
 				.actions(v-if="user.id !== ownUser.id")
 					//- moderation_state
