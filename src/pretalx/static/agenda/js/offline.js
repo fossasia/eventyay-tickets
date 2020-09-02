5beb07a2e9ffb458ade4bc601f335710ee194199
Tobias Kunze <r@rixx.de>
2020-09-02 23:15:35 +0200

    Remove broken serviceWorker

--- src/pretalx/static/agenda/js/offline.js
+++
@@ -1,8 +0,0 @@
-if ("serviceWorker" in navigator) {
-  const source = document.querySelector("#offline-vars")
-  if (source) {
-    navigator.serviceWorker.register("/sw.js", {
-      scope: source.getAttribute("data-event"),
-    })
-  }
-}
