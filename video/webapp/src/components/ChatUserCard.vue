c7497237e749a078d542e0b16e19677c0116d908
Arne Rick <rick@rami.io>
2020-09-03 18:30:44 +0200

    [webapp] Fix ChatUserCard profile field access

--- video/webapp/src/components/ChatUserCard.vue
+++ video/webapp/src/components/ChatUserCard.vue
@@ -58,7 +58,7 @@ export default {
 		},
 		availableFields () {
 			if (!this.sender.profile?.fields) return
-			return this.world?.user_profile?.additional_fields
+			return this.world?.profile_fields
 				.map(field => ({label: field.label, value: this.sender.profile.fields[field.label]}))
 				.filter(field => !!field.value)
 		},
