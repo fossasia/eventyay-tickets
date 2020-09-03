4cd0f81540d152d3e61fcd9c5fdbf506a23acb91
Arne Rick <rick@rami.io>
2020-09-03 15:12:08 +0200

    [webapp] Profile fields: more field types

--- video/webapp/src/components/profile/ChangeAdditionalFields.vue
+++ video/webapp/src/components/profile/ChangeAdditionalFields.vue
@@ -2,6 +2,9 @@
 .c-additional-fields
 	span(v-for="field of fields")
 		bunt-input(v-if="field.type === 'text'", :name="field.label", :label="field.label", v-model="value[field.label]")
+		bunt-input-outline-container(v-if="field.type === 'textarea'", :label="field.label", :name="field.label")
+			textarea(slot-scope="{focus, blur}", @focus="focus", @blur="blur", v-model="value[field.label]")
+		bunt-select(v-if="field.type === 'select'", v-model="value[field.label]", :label="field.label", name="field.label", :options="field.choices")
 </template>
 <script>
 import { mapState } from 'vuex'
@@ -19,4 +22,13 @@ export default {
 }
 </script>
 <style lang="stylus">
+.c-additional-fields
+	.bunt-input-outline-container
+		textarea
+			background-color transparent
+			border none
+			outline none
+			resize vertical
+			min-height 250px
+			padding 0 8px
 </style>
