49f276158f9f7b2264836d3d89f1d9807038b318
Tobias Kunze <r@rixx.de>
2020-09-03 10:14:55 +0200

    Offer to leave chat channels

--- video/webapp/src/store/chat.js
+++ video/webapp/src/store/chat.js
@@ -179,12 +179,12 @@ export default {
 			}
 			return channel
 		},
-		async closeDirectMessage ({state}, {channel}) {
-			await api.call('chat.leave', {channel: channel.id})
-			if (router.currentRoute.name === 'channel' && router.currentRoute.params.channelId === channel.id) {
+		async leaveChannel ({state}, {channelId}) {
+			await api.call('chat.leave', {channel: channelId})
+			if (router.currentRoute.name === 'channel' && router.currentRoute.params.channelId === channelId) {
 				await router.push({name: 'home'})
 			}
-			const index = state.joinedChannels.findIndex(c => c.id === channel.id)
+			const index = state.joinedChannels.findIndex(c => c.id === channelId)
 			if (index > -1) state.joinedChannels.splice(index, 1)
 		},
 		async startCall ({state, dispatch}, {channel}) {
