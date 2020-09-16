e54fad2b5724c40f83182136ec79218cc71b4948
Arne Rick <rick@rami.io>
2020-09-16 12:10:37 +0200

    [webapp] Scrollbars for long user cards

--- video/webapp/src/components/ChatUserCard.vue
+++ video/webapp/src/components/ChatUserCard.vue
@@ -1,7 +1,7 @@
 <template lang="pug">
 .c-chat-user-card
 	.ui-background-blocker(v-if="!userAction", @click="$emit('close')")
-	.user-card(v-if="!userAction", ref="card", @mousedown="showMoreActions=false")
+	.user-card(v-scrollbar.y="", v-if="!userAction", ref="card", @mousedown="showMoreActions=false")
 		avatar(:user="sender", :size="128")
 		.name
 			| {{ sender.profile ? sender.profile.display_name : (sender.id ? sender.id : '(unknown user)') }}
@@ -99,6 +99,8 @@ export default {
 		padding: 8px
 		min-width: 196px
 		max-width: 380px
+		min-height: 0
+		max-height: 100%
 		.name
 			font-size: 24px
 			font-weight: 600
