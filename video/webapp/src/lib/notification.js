631ff39384309fe305767acec60708d0e0378ea7
Arne Rick <rick@rami.io>
2020-09-24 10:37:16 +0200

    [webapp] Display avatar in contact request notifications

--- video/webapp/src/lib/notification.js
+++ video/webapp/src/lib/notification.js
@@ -1,7 +1,7 @@
 export default function notification (title, text, close = null, click = null, img = '') {
 	if (localStorage.desktopNotificationPermission !== 'granted') return
 	const audio = new Audio('/notify.wav')
-	const desktopNotification = new Notification(title, {body: text, image: img})
+	const desktopNotification = new Notification(title, {body: text, icon: img})
 	if (localStorage.playDesktopNotificationSound === 'true') audio.play()
 	if (close) desktopNotification.onclose = close
 	if (click) desktopNotification.onclick = click
