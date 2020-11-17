cd7f5438e297a609c922d1dbb4d1a249165faa67
Tobias Kunze <r@rixx.de>
2020-11-17 10:47:24 +0100

    Update djangorestframework

--- src/setup.py
+++ src/setup.py
@@ -110,7 +110,7 @@ setup(
         "django-i18nfield~=1.8.1",  # 2020-06, no changelog, https://github.com/raphaelm/django-i18nfield/
         "django-libsass~=0.8",  # inactive, https://github.com/torchbox/django-libsass/blob/master/CHANGELOG.txt
         "django-scopes~=1.2.0",  # https://github.com/raphaelm/django-scopes/releases
-        "djangorestframework~=3.11.1",  # http://www.django-rest-framework.org/community/release-notes/
+        "djangorestframework~=3.12.0",  # http://www.django-rest-framework.org/community/release-notes/
         "inlinestyler~=0.2.0",  # https://github.com/dlanger/inlinestyler/blob/master/CHANGELOG
         "libsass~=0.20.0",  # https://sass.github.io/libsass-python/changes.html
         "Markdown~=3.3.0",  # https://python-markdown.github.io/change_log/
