0aea0c8a6c66536f13cfad4bce7c26d4df5deee9
Raphael Michel <mail@raphaelmichel.de>
2020-11-21 12:24:26 +0100

    Re-design permission admin interface

--- video/webapp/src/components/config/PermissionConfig.vue
+++ video/webapp/src/components/config/PermissionConfig.vue
@@ -3,17 +3,41 @@
 	bunt-progress-circular(size="huge", v-if="error == null && config == null")
 	.error(v-if="error") We could not fetch the current configuration.
 	div(v-if="config != null")
+		h3 Roles
 		div.role(v-for="(val, key, index) in config.roles")
-			bunt-input(:value="key", label="role name", @input="set_role_name(key, $event)", name="n", :disabled="index < Object.keys(config.roles).length - 1")
-			.role-config-permissions
+			.role-head
+				bunt-icon-button.chevron(@click.prevent="toggle_role(key)") {{ expandedRoles.includes(key) ? 'chevron-down' : 'chevron-right' }}
+				h4(@click.prevent="toggle_role(key)") {{ key }}
+				bunt-icon-button(@click.prevent="delete_role(key)") delete
+			.role-config-permissions(v-if="expandedRoles.includes(key)")
 				bunt-checkbox(v-for="p in config.available_permissions", :label="p", :value="val.includes(p)", name="p",
 											@input="toggle_permission(key, p, $event)")
-			bunt-checkbox(label="Grant role based on traits", :value="typeof config.trait_grants[key] !== 'undefined'", name="p",
-										@input="toggle_trait_grants(key, $event)")
-			bunt-input(label="Required traits (comma-separated)", @input="set_trait_grants(key, $event)",
-									:disabled="typeof config.trait_grants[key] === 'undefined'", name="g"
-									:value="config.trait_grants[key] ? config.trait_grants[key].map(i => (Array.isArray(i) ? i.join('|') : i)).join(', ') : ''")
-		bunt-button.btn-add-role(@click="add_role") Add new role
+		div.role
+			div.role-head
+				bunt-input(label="role name", v-model="newRoleName", name="newRoleName")
+				bunt-button.btn-add-role(@click="add_role", :disabled="!newRoleName || newRoleName in config.roles") Add new role
+		h3 Roles assigned to traits globally (valid for ALL rooms)
+		table.trait-grants
+			thead
+				tr
+					th Role
+					th Required traits
+					th
+			tbody
+				tr(v-for="(val, key, index) in config.trait_grants")
+					td
+						bunt-input(:value="key", label="Role name", @input="set_trait_grant_role_name(key, $event)", name="n", :disabled="index < Object.keys(config.trait_grants).length - 1")
+					td
+						bunt-input(label="Required traits (comma-separated)", @input="set_trait_grants(key, $event)", name="g"
+											:value="val ? val.map(i => (Array.isArray(i) ? i.join('|') : i)).join(', ') : ''")
+					td.actions
+						bunt-icon-button(@click="remove_trait_grant(key)") delete-outline
+			tfoot
+				tr
+					td
+						bunt-button.btn-add-role(@click="add_trait_grant") Add role
+					td
+					td
 		bunt-button.btn-save(@click="save", :loading="saving") Save
 
 </template>
@@ -27,6 +51,8 @@ export default {
 	data () {
 		return {
 			config: null,
+			expandedRoles: [],
+			newRoleName: '',
 
 			saving: false,
 			error: null
@@ -48,6 +74,24 @@ export default {
 		}
 	},
 	methods: {
+		toggle_role (role) {
+			if (this.expandedRoles.includes(role)) {
+				this.expandedRoles = this.expandedRoles.filter((r) => r !== role)
+			} else {
+				this.expandedRoles.push(role)
+			}
+		},
+		delete_role (role) {
+			this.$delete(this.config.roles, role)
+		},
+		add_role () {
+			if (this.newRoleName) {
+				console.log(this.newRoleName)
+				this.$set(this.config.roles, this.newRoleName, [])
+				this.expandedRoles.push(this.newRoleName)
+				this.newRoleName = ''
+			}
+		},
 		toggle_permission (role, perm, toggle) {
 			if (toggle) {
 				this.config.roles[role].push(perm)
@@ -62,23 +106,15 @@ export default {
 				).filter((i) => i.length > 0))
 			}
 		},
-		add_role () {
-			this.$set(this.config.roles, '', [])
+		remove_trait_grant (role) {
+			this.$delete(this.config.trait_grants, role)
 		},
-		toggle_trait_grants (role, toggle) {
-			if (toggle) {
-				this.$set(this.config.trait_grants, role, [])
-			} else {
-				this.$delete(this.config.trait_grants, role)
-			}
+		add_trait_grant () {
+			this.$set(this.config.trait_grants, '', [])
 		},
-		set_role_name (old, n) {
-			this.$set(this.config.roles, n, this.config.roles[old])
-			this.$delete(this.config.roles, old)
-			if (typeof this.config.trait_grants[old] !== 'undefined') {
-				this.$set(this.config.trait_grants, n, this.config.trait_grants[old])
-				this.$delete(this.config.trait_grants, old)
-			}
+		set_trait_grant_role_name (old, n) {
+			this.$set(this.config.trait_grants, n, this.config.trait_grants[old])
+			this.$delete(this.config.trait_grants, old)
 		},
 		async save () {
 			// TODO validate connection limit is a number
@@ -100,17 +136,27 @@ export default {
 		border-radius: 4px
 		padding: 5px 15px
 		margin-bottom 15px
+	.role-head
+		display: flex
+		flex-direction: row
+		align-items: center
+		.bunt-icon-button.chevron
+			position: relative
+			left: -6px
+		h4, .bunt-input
+			flex: auto 1 1
 	.role-config-permissions
 		display: flex
 		flex-direction: row
 		flex-wrap: wrap
 		.bunt-checkbox
 			width: 25%
+	.trait-grants
+		width: 100%
 	.btn-save
 		margin-top: 16px
 		margin-left: 16px
 		themed-button-primary(size: large)
 	.btn-add-role
-		margin-top: 16px
-		themed-button-secondary(size: large)
+		themed-button-secondary()
 </style>
