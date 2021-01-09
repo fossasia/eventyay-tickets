4c458a5619c2483c451bc1b4c161bb73c810af79
rash <sebastian.gepperth@ax-semantics.com>
2021-01-09 17:42:07 +0100

    [webapp] add presentation mode

---
+++ video/webapp/src/PresentationMode.vue
@@ -0,0 +1,91 @@
+<template lang="pug">
+#presentation-mode(:style="[style, themeVariables]")
+	.fatal-indicator.mdi.mdi-alert-octagon(v-if="fatalError || fatalConnectionError", :title="errorMessage")
+	.content(v-else-if="world")
+		.question(v-if="pinnedQuestion")
+			| {{ pinnedQuestion.content }}
+	bunt-progress-circular(v-else, size="small")
+
+</template>
+<script>
+import { mapState, mapGetters } from 'vuex'
+import { themeVariables } from 'theme'
+import api from 'lib/api'
+
+const SLIDE_WIDTH = 960
+const SLIDE_HEIGHT = 700
+
+export default {
+	components: {},
+	data () {
+		return {
+			themeVariables,
+			scale: 1
+		}
+	},
+	computed: {
+		...mapState(['fatalConnectionError', 'fatalError', 'connected', 'world', 'rooms']),
+		...mapGetters('question', ['pinnedQuestion']),
+		errorMessage () {
+			return this.fatalConnectionError?.code || this.fatalError?.message
+		},
+		room () {
+			return this.rooms?.find(room => room.id === this.$route.params.roomId) || this.rooms?.[0]
+		},
+		style () {
+			return {
+				'--scale': this.scale.toFixed(1)
+			}
+		},
+	},
+	watch: {
+		room () {
+			this.$store.dispatch('changeRoom', this.room)
+			api.call('room.enter', {room: this.room.id})
+		}
+	},
+	mounted () {
+		window.addEventListener('resize', this.computeScale)
+		this.computeScale()
+	},
+	beforeDestroy () {
+		window.removeEventListener('resize', this.computeScale)
+	},
+	methods: {
+		computeScale () {
+			const width = document.body.offsetWidth
+			const height = document.body.offsetHeight
+			this.scale = Math.min(width / SLIDE_WIDTH, height / SLIDE_HEIGHT)
+		},
+	}
+}
+</script>
+<style lang="stylus">
+#presentation-mode
+	height: 100%
+	display: flex
+	flex-direction: column
+	justify-content: center
+	align-items: center
+	> .bunt-progress-circular, > .fatal-indicator
+		position: fixed
+		top: 100%
+		left: 0
+		transform: translate(4px, calc(-100% - 4px))
+	> .fatal-indicator
+		color: $clr-danger
+		font-size: 1vw
+
+	.content
+		display: flex
+		flex-direction: column
+		justify-content: center
+		align-items: center
+		width: 960px
+		height: 700px
+		flex: none
+		transform: scale(var(--scale)) translateZ(0)
+	.question
+		font-size: 36px
+		font-weight: 500
+</style>
