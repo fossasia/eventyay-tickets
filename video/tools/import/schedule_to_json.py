b51c32a859ab7de649ec2b3a38a5a0a580309dd9
Tobias Kunze <r@rixx.de>
2021-01-15 11:49:39 +0100

    [importer] Validate data, better error messages

--- video/tools/import/schedule_to_json.py
+++ video/tools/import/schedule_to_json.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import datetime as dt
 import json
 import math
 import sys
@@ -6,6 +7,7 @@ from collections import defaultdict
 from pathlib import Path
 
 import pandas
+from dateutil.parser import parse
 
 
 def exit(message):
@@ -39,6 +41,8 @@ def load_sheet(fname):
 
 
 def to_iso(value):
+    if not isinstance(value, dt.datetime):
+        raise Exception("Not a valid date and time.")
     return value.isoformat()
 
 
@@ -88,14 +92,43 @@ def transform_data(data, table_name, field_mapping, mandatory_fields, methods=No
             continue  # empty row, probably
         if not all(truthy_fields.values()):
             missing = [key for key, value in truthy_fields.items() if not value]
-            exit(f"Missing key(s) in {table_name}, row {_}: {missing}")
-        result.append(
-            {
-                field: transformers[field](row.get(excel))
-                for field, excel in field_mapping.items()
-            }
-        )
+            exit(f"Missing key(s) in sheet {table_name}, row {_ + 1}: {missing}")
+        row_result = {}
+        for field, excel in field_mapping.items():
+            try:
+                row_result[field] = transformers[field](row.get(excel))
+            except Exception as e:
+                exit(
+                    f"Error in sheet {table_name}, row {_ + 1} when importing column {excel}: {e}"
+                )
+        result.append(row_result)
     return result
+
+
+def validate_data(data):
+    room_ids = [room["id"] for room in data["rooms"]]
+    track_ids = [track["id"] for track in data["tracks"]]
+    speaker_ids = [speaker["code"] for speaker in data["speakers"]]
+    for talk in data["talks"]:
+        if not talk["start"]:
+            exit(f"Talk {talk['code']} ({talk['title']}) has no valid start datetime.")
+        if not talk["end"]:
+            exit(f"Talk {talk['code']} ({talk['title']}) has no valid start datetime.")
+        start = parse(talk["start"])
+        end = parse(talk["end"])
+        if start > end:
+            exit(
+                f"Talk {talk['code']} ({talk['title']}) has a start that is later than its end."
+            )
+        if talk["track"] and not talk["track"] in track_ids:
+            exit(f"Talk {talk['code']} ({talk['title']}) has an invalid track ID.")
+        if not talk["room"] in room_ids:
+            exit(f"Talk {talk['code']} ({talk['title']}) has an invalid room ID.")
+        for speaker in talk.get("speakers", []):
+            if speaker not in speaker_ids:
+                exit(
+                    f"Talk {talk['code']} ({talk['title']}) has an unknown speaker ID: {speaker}."
+                )
 
 
 def main():
@@ -148,6 +181,7 @@ def main():
         mandatory_fields=["Title", "Start", "End", "Room ID"],
         methods={"start": to_iso, "end": to_iso, "speakers": to_list},
     )
+    validate_data(result)
     with open("schedule.json", "w") as fp:
         json.dump(result, fp, indent=4)
 
