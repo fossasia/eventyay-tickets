b1c387e84e9b9ab72024a929cb4c590b442f264b
Raphael Michel <mail@raphaelmichel.de>
2021-01-24 18:57:01 +0100

    Fix #177 -- Re-encode uploaded images to strip metadata

--- video/server/venueless/storage/views.py
+++ video/server/venueless/storage/views.py
@@ -1,6 +1,8 @@
 import logging
+from io import BytesIO
 
-from django.core.exceptions import PermissionDenied
+from django.core.exceptions import PermissionDenied, ValidationError
+from django.core.files import File
 from django.http import JsonResponse
 from django.shortcuts import get_object_or_404
 from django.utils.decorators import method_decorator
@@ -8,6 +10,7 @@ from django.utils.functional import cached_property
 from django.utils.timezone import now
 from django.views import View
 from django.views.decorators.csrf import csrf_exempt
+from PIL import Image
 from rest_framework.authentication import get_authorization_header
 
 from venueless.core.models import World
@@ -32,6 +35,12 @@ class UploadView(View):
         ".gif",
         ".pdf",
         ".svg",
+    )
+    pillow_formats = (
+        ".png",
+        ".jpg",
+        ".jpeg",
+        ".gif",
     )
     max_size = 10 * 1024 * 1024
 
@@ -86,13 +95,56 @@ class UploadView(View):
         ):
             return JsonResponse({"error": "file.type"}, status=400)
 
+        if any(
+            request.FILES["file"].name.lower().endswith(e) for e in self.pillow_formats
+        ):
+            try:
+                content_type, file = self.validate_image(request.FILES["file"])
+            except ValidationError:
+                return JsonResponse({"error": "file.picture.invalid"}, status=400)
+        else:
+            file = request.FILES["file"]
+            content_type = request.FILES["file"].content_type
+
         sf = StoredFile.objects.create(
             world=self.world,
             date=now(),
             filename=request.FILES["file"].name,
-            type=request.FILES["file"].content_type,
-            file=request.FILES["file"],
+            type=content_type,
+            file=file,
             public=True,
             user=self.user,
         )
         return JsonResponse({"url": sf.file.url}, status=201)
+
+    def validate_image(self, data):
+        # partially vendored from django.forms.fields.ImageField
+        # We need to get a file object for Pillow. We might have a path or we might
+        # have to read the data into memory.
+        if hasattr(data, "temporary_file_path"):
+            file = data.temporary_file_path()
+        else:
+            if hasattr(data, "read"):
+                file = BytesIO(data.read())
+            else:
+                file = BytesIO(data["content"])
+
+        try:
+            # load() could spot a truncated JPEG, but it loads the entire
+            # image in memory, which is a DoS vector. See #3848 and #18520.
+            image = Image.open(file)
+            # verify() must be called immediately after the constructor.
+            image.verify()
+        except Exception:
+            # Pillow doesn't recognize it as an image.
+            raise ValidationError("invalid image")
+
+        file.seek(0)
+        o = BytesIO()
+        o.name = data.name
+        image = Image.open(file)
+        image_without_exif = Image.new(image.mode, image.size)
+        image_without_exif.putdata(image.getdata())
+        image_without_exif.save(o)
+        o.seek(0)
+        return Image.MIME.get(image.format), File(o, name=data.name)
