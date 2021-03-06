2b767b343e000c20fb15598772753d516807d223
rash <sebastian.gepperth@ax-semantics.com>
2021-03-06 17:14:17 +0100

    [webapp] fix duplicate file input bug
    fixes: when a user joins an event with a stage as landing page and 
    already has a display name (via token), the avatar upload does not 
    report the file input to the greeting dialog but to the file upload of 
    the chat input

--- video/webapp/src/components/UploadButton.vue
+++ video/webapp/src/components/UploadButton.vue
@@ -1,14 +1,15 @@
 <template lang="pug">
 .c-upload-button
-	label(for="file-chooser", :class="buttonClass", v-tooltip="tooltipOptions || {text: _tooltip, placement: tooltipPlacement, fixed: tooltipFixed}")
+	label(:for="id", :class="buttonClass", v-tooltip="tooltipOptions || {text: _tooltip, placement: tooltipPlacement, fixed: tooltipFixed}")
 		i.bunt-icon.mdi(v-if="iconClass", :class="iconClass")
 		.bunt-button-content
 			.bunt-button-text
 				slot
 		ripple-ink
-	input#file-chooser(type="file", @change="$emit('change', $event)", :accept="accept" :multiple="multiple")
+	input(:id="id", type="file", @change="$emit('change', $event)", :accept="accept" :multiple="multiple")
 </template>
 <script>
+import { v4 as uuid } from 'uuid'
 import RippleInk from 'buntpapier/src/mixins/ripple-ink'
 import iconHelper from 'buntpapier/src/helpers/icon'
 
@@ -33,6 +34,7 @@ export default {
 	},
 	data () {
 		return {
+			id: 'file-input' + uuid() // we need to generate a unique id for label "for" to work with multiple existing upload inputs
 		}
 	},
 	computed: {
@@ -51,7 +53,7 @@ export default {
 </script>
 <style lang="stylus">
 .c-upload-button
-	#file-chooser
+	input
 		display: none
 	.bunt-icon-button
 		pointer-events: auto
