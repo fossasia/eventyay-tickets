2433170c5c8fcd63ea93d22d1f1308c3c369b82d
Raphael Michel <mail@raphaelmichel.de>
2021-03-07 21:24:03 +0100

    [janus] Conference: Don't persist videoRequested to local storage

--- video/webapp/src/components/janus/JanusConference.vue
+++ video/webapp/src/components/janus/JanusConference.vue
@@ -457,7 +457,6 @@ export default {
 		},
 		disableAllVideo () {
 			this.videoRequested = false
-			localStorage.videoRequested = false
 			localStorage.videoOutput = false
 
 			for (const h of this.feeds) {
