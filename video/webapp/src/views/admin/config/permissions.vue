a63e247045e8b4245948d0c32b2ab561b2b7a05d
rash <sebastian.gepperth@ax-semantics.com>
2021-03-28 21:46:51 +0200

    [webapp] improve permission config layout

--- video/webapp/src/views/admin/config/permissions.vue
+++ video/webapp/src/views/admin/config/permissions.vue
@@ -1,72 +1,51 @@
 <template lang="pug">
 .c-permissionconfig
 	.ui-page-header
-		h1 Theme Config
-	scrollbars(y)
-		bunt-progress-circular(size="huge", v-if="!error && !config")
-		.error(v-if="error") We could not fetch the current configuration.
-		.permission-config(v-if="config")
-			h3 Roles
-			div.role(v-for="(val, key, index) of config.roles")
-				.role-head
-					bunt-icon-button.chevron(@click.prevent="toggleRole(key)") {{ expandedRoles.includes(key) ? 'chevron-down' : 'chevron-right' }}
-					h4(@click.prevent="toggleRole(key)") {{ key }}
-					bunt-icon-button(@click.prevent="deleteRole(key)") delete
-				.role-config-permissions(v-if="expandedRoles.includes(key)")
-					bunt-checkbox(v-for="p of config.available_permissions", :label="p", :value="val.includes(p)", name="p",
-												@input="togglePermission(key, p, $event)")
-			div.role
-				div.role-head
-					bunt-input(label="role name", v-model="newRoleName", name="newRoleName")
-					bunt-button.btn-add-role(@click="addRole", :disabled="!newRoleName || newRoleName in config.roles") Add new role
-			h3 Roles assigned to traits globally (valid for ALL rooms)
-			table.trait-grants
-				thead
-					tr
-						th Role
-						th Required traits
-						th
-				tbody
-					tr(v-for="(val, key, index) of config.trait_grants")
-						td: bunt-input(:value="key", label="Role name", @input="setTraitGrantRoleName(config, key, $event)", name="n", :disabled="index < Object.keys(config.trait_grants).length - 1")
-						td: bunt-input(label="Required traits (comma-separated)", @input="setTraitGrants(config, key, $event)", name="g"
-												:value="val ? val.map(i => (Array.isArray(i) ? i.join('|') : i)).join(', ') : ''")
-						td.actions
-							bunt-icon-button(@click="removeTraitGrant(config, key)") delete-outline
-				tfoot
-					tr
-						td: bunt-button.btn-add-role(@click="addTraitGrant(config)") Add role
-						td
-						td
-			h3 Roles assigned to traits PER ROOM
-			.room-traits(v-for="room of rooms")
-				h4 {{ room.name }}
-				table.trait-grants
-					thead
-						tr
-							th Role
-							th Required traits
-							th
-					tbody
-						tr(v-for="(val, key, index) of room.trait_grants")
-							td: bunt-input(:value="key", label="Role name", @input="setTraitGrantRoleName(room, key, $event)", name="n", :disabled="index < Object.keys(room.trait_grants).length - 1")
-							td: bunt-input(label="Required traits (comma-separated)", @input="setTraitGrants(room, key, $event)", name="g"
-													:value="val ? val.map(i => (Array.isArray(i) ? i.join('|') : i)).join(', ') : ''")
-							td.actions: bunt-icon-button(@click="removeTraitGrant(room, key)") delete-outline
-					tfoot
-						tr
-							td: bunt-button.btn-add-role(@click="addTraitGrant(room)") Add role
-							td
-							td
+		h1 Permission Config
+	bunt-progress-circular(size="huge", v-if="!error && !config")
+	.error(v-if="error") We could not fetch the current configuration.
+	bunt-tabs(v-if="config")
+		bunt-tab(header="Global", v-scrollbar.y="")
+			.permission-config
+				.info
+					h3 Roles assigned to traits globally
+					p Users receive the following roles if they have the required traits in their token/ticket. Global roles are valid for #[b all rooms].
+				trait-grants(:trait-grants="config.trait_grants", :config="config")
+		bunt-tab(header="Per Room", v-scrollbar.y="")
+			.permission-config
+				.info
+					h3 Roles assigned to traits #[i per room]
+					p Users receive the following roles if they have the required traits in their token/ticket. #[b Per room roles] only apply for the corresponding room.
+				.room-traits(v-for="room of rooms")
+					h4 {{ room.name }}
+					trait-grants(:trait-grants="room.trait_grants", :config="config", @click.stop="")
+		bunt-tab(header="Roles", v-scrollbar.y="")
+			.permission-config
+				.info-wrapper
+					.info
+						h3 Roles
+						p List of roles which can be assigned to users globally or per room via token/ticket traits. Each role can give a user certain permissions. Total permissions are computed from the sum of permissions from all granted roles. Permissions can #[b NOT] be subtracted by a role if granted by another role.
+					.collapse-actions
+				.role-definition(v-for="(val, key, index) of config.roles", @click="toggleRole(key)")
+					.role-head
+						bunt-icon-button.chevron {{ expandedRoles.includes(key) ? 'chevron-up' : 'chevron-down' }}
+						h4 {{ key }}
+						bunt-icon-button(@click.stop="deleteRole(key)") delete-outline
+					.role-config-permissions(v-if="expandedRoles.includes(key)", @click.stop="")
+						bunt-checkbox(v-for="p of config.available_permissions", :label="p", :value="val.includes(p)", name="p", @input="togglePermission(key, p, $event)")
+				.role
+					.role-head
+						bunt-input(label="role name", v-model="newRoleName", name="newRoleName")
+						bunt-button.btn-add-role(@click="addRole", :disabled="!newRoleName || newRoleName in config.roles") Add new role
 	.ui-form-actions
 		bunt-button.btn-save(@click="save", :loading="saving", :error-message="error") Save
 </template>
 <script>
 import api from 'lib/api'
-import i18n from 'i18n'
-// TODO: validate color / id values
+import TraitGrants from './trait-grants'
 
 export default {
+	components: { TraitGrants },
 	data () {
 		return {
 			config: null,
@@ -75,11 +54,6 @@ export default {
 			rooms: null,
 			saving: false,
 			error: null
-		}
-	},
-	computed: {
-		locales () {
-			return i18n.availableLocales
 		}
 	},
 	async created () {
@@ -118,23 +92,7 @@ export default {
 				this.$set(this.config.roles, role, this.config.roles[role].filter((i) => i !== perm))
 			}
 		},
-		setTraitGrants (target, role, traits) {
-			if (typeof target.trait_grants[role] !== 'undefined') {
-				this.$set(target.trait_grants, role, traits.split(',').map(
-					(i) => i.trim().split('|').filter((j) => j.length > 0)
-				).filter((i) => i.length > 0))
-			}
-		},
-		removeTraitGrant (target, role) {
-			this.$delete(target.trait_grants, role)
-		},
-		addTraitGrant (target) {
-			this.$set(target.trait_grants, '', [])
-		},
-		setTraitGrantRoleName (target, old, n) {
-			this.$set(target.trait_grants, n, target.trait_grants[old])
-			this.$delete(target.trait_grants, old)
-		},
+
 		async save () {
 			// TODO validate connection limit is a number
 			this.saving = true
@@ -160,30 +118,53 @@ export default {
 	flex: auto
 	display: flex
 	flex-direction: column
-	.permission-config
-		padding: 16px
+	.bunt-icon-button
+		icon-button-style(style: clear)
+	.bunt-input
+		input-style(size: compact)
+	.bunt-tabs
+		tabs-style(active-color: var(--clr-primary), indicator-color: var(--clr-primary), background-color: transparent)
+		margin-bottom: 0
+	.bunt-tabs-header
+		border-bottom: border-separator()
+	.bunt-tabs, .bunt-tabs-body, .bunt-tab
+		flex: auto
 		display: flex
 		flex-direction: column
-	.role
-		border: 1px solid #cccccc
+		min-height: 0
+	.info
+		padding: 16px
+		p
+			max-width: 960px
+	.room-traits
+		border: border-separator()
+		margin: 0 8px 16px 8px
 		border-radius: 4px
-		padding: 5px 15px
-		margin-bottom 15px
-	.role-head
-		display: flex
-		flex-direction: row
-		align-items: center
-		.bunt-icon-button.chevron
-			position: relative
-			left: -6px
-		h4, .bunt-input
-			flex: auto 1 1
+		h4
+			margin: 8px 16px 0px 16px
+	.role-definition
+		border: border-separator()
+		border-radius: 4px
+		padding: 4px 16px
+		margin: 0 8px 16px 8px
+		.role-head
+			display: flex
+			flex-direction: row
+			align-items: center
+			cursor: pointer
+			.bunt-icon-button.chevron
+				position: relative
+				left: -6px
+			h4, .bunt-input
+				flex: auto 1 1
 	.role-config-permissions
-		display: flex
-		flex-direction: row
-		flex-wrap: wrap
-		.bunt-checkbox
-			width: 25%
+		margin: 8px 16px
+		display: grid
+		// grid-auto-flow: column
+		grid-template-columns: repeat(4, 1fr)
+		// grid-template-rows: repeat(2, 150px)
+		// grid-auto-rows: 36px
+		gap: 4px
 	.trait-grants
 		width: 100%
 	.btn-add-role
