3f9bcefca816f3ca38e0909cbfb784df048cca12
rash <sebastian.gepperth@ax-semantics.com>
2021-03-28 15:40:27 +0200

    [webapp] add native color picker to theme config

---
+++ video/webapp/src/components/ColorPicker.vue
@@ -0,0 +1,35 @@
+<template lang="pug">
+.c-color-picker
+	.color(:style="{'--color': value}")
+	bunt-input(v-bind="$attrs", :value="value", @input="$emit('input', $event)")
+
+	input.color-picker(type="color", :value="value", @change="$emit('input', $event.target.value)")
+</template>
+<script>
+// TODO
+// - make picker accept 3 digit hex
+export default {
+	props: {
+		value: String
+	}
+}
+</script>
+<style lang="stylus">
+.c-color-picker
+	position: relative
+	.color
+		position: absolute
+		right: 4px
+		top: 20px
+		height: 29px
+		width: 36px
+		background-color: var(--color)
+		border-radius: 3px
+	.color-picker
+		position: absolute
+		right: 0
+		top: 16px
+		opacity: 0
+		height: 36px
+		width: 36px
+</style>
