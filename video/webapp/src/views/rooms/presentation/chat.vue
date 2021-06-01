f9aab95e8c8544e0fbc647cf7e0b6d9c92628db2
rash <sebastian.gepperth@ax-semantics.com>
2021-06-01 20:54:23 +0200

    fix chat presentation css

--- video/webapp/src/views/rooms/presentation/chat.vue
+++ video/webapp/src/views/rooms/presentation/chat.vue
@@ -1,10 +1,10 @@
 <template lang="pug">
 .v-presentation-chat
 	template(v-for="(message, index) of filteredTimeline")
-		chat-message(:message="message", :previousMessage="filteredTimeline[index - 1]", :nextMessage="filteredTimeline[index + 1]", mode="compact", :readonly="true", :key="message.event_id")
+		chat-message(:message="message", :nextMessage="filteredTimeline[index + 1]", mode="compact", :readonly="true", :key="message.event_id")
 </template>
 <script>
-import { mapState, mapGetters } from 'vuex'
+import { mapState } from 'vuex'
 import ChatMessage from 'components/ChatMessage'
 
 export default {
@@ -18,24 +18,26 @@ export default {
 			return this.room.modules.find(module => module.type === 'chat.native')
 		},
 		filteredTimeline () {
-			return this.timeline.filter(message => message.event_type !== 'channel.member' && message.content.type !== 'deleted' && !message.replaces)
+			return this.timeline.filter(message => message.event_type !== 'channel.member' && message.content.type !== 'deleted' && !message.replaces).reverse()
 		},
 	},
 	created () {
 		this.$store.dispatch('chat/subscribe', {channel: this.module.channel_id, config: this.module.config})
-	},
-	mounted () {
-		this.$nextTick(() => {
-		})
-	},
-	methods: {}
+	}
 }
 </script>
 <style lang="stylus">
 .v-presentation-chat
 	display: flex
-	flex-direction: column
-	justify-content: flex-end
+	// reverse direction so we can wrap from top
+	flex-direction: column-reverse
+	justify-content: flex-start
+	flex-wrap: wrap
 	min-height: 0
-	max-width: 100%
+
+	// sane presentation defaults for chat messages
+	.c-chat-message
+		width: 100%
+		.timestamp, .preview-card
+			display: none
 </style>
