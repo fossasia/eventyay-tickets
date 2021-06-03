befdfd6136b01cd15b3d32b45eb93e2744c873e6
rash <sebastian.gepperth@ax-semantics.com>
2021-06-03 17:39:41 +0200

    [a11y] add more icon button tooltips

--- video/webapp/src/components/EmojiPickerButton.vue
+++ video/webapp/src/components/EmojiPickerButton.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
 .c-emoji-picker-button
-	bunt-icon-button.btn-emoji-picker(ref="button", @click="toggle")
+	bunt-icon-button.btn-emoji-picker(ref="button", :tooltip="$t('EmojiPickerButton:tooltip')", @click="toggle")
 		svg(v-if="iconStyle === 'plain'", xmlns="http://www.w3.org/2000/svg", viewBox="0 0 24 24")
 			path(d="M12 0C5.373 0 0 5.373 0 12s5.373 12 12 12 12-5.373 12-12S18.627 0 12 0m0 22C6.486 22 2 17.514 2 12S6.486 2 12 2s10 4.486 10 10-4.486 10-10 10")
 			path(d="M8 7a2 2 0 1 0-.001 3.999A2 2 0 0 0 8 7M16 7a2 2 0 1 0-.001 3.999A2 2 0 0 0 16 7M15.232 15c-.693 1.195-1.87 2-3.349 2-1.477 0-2.655-.805-3.347-2H15m3-2H6a6 6 0 1 0 12 0")
@@ -78,13 +78,6 @@ export default {
 .c-emoji-picker-button
 	.btn-emoji-picker
 		icon-button-style()
-		// height: 36px
-		// width: @height
-		// box-sizing: border-box
-		// padding: 8px
-		// &:hover
-		// 	border-radius: 50%
-		// 	background-color: $clr-grey-100
 		svg
 			path
 				fill: $clr-primary-text-light
