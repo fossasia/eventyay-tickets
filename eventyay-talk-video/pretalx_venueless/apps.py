4ee62271afd86a061cb7dfbe8c3620bd1429ab10
Tobias Kunze <r@rixx.de>
2021-08-15 17:24:59 +0200

    Initial commit

---
+++ eventyay-talk-video/pretalx_venueless/apps.py
@@ -0,0 +1,17 @@
+from django.apps import AppConfig
+from django.utils.translation import gettext_lazy
+
+
+class PluginApp(AppConfig):
+    name = "pretalx_venueless"
+    verbose_name = "Venueless integration"
+
+    class PretalxPluginMeta:
+        name = gettext_lazy("Venueless integration")
+        author = "Tobias Kunze"
+        description = gettext_lazy("Venueless integration in pretalx: Notify venueless about new schedule releases!")
+        visible = True
+        version = "0.0.0"
+
+    def ready(self):
+        from . import signals  # NOQA
---
+++ eventyay-talk-video/setup.py
@@ -0,0 +1,42 @@
+import os
+from distutils.command.build import build
+
+from django.core import management
+from setuptools import find_packages, setup
+
+try:
+    with open(
+        os.path.join(os.path.dirname(__file__), "README.rst"), encoding="utf-8"
+    ) as f:
+        long_description = f.read()
+except FileNotFoundError:
+    long_description = ""
+
+
+class CustomBuild(build):
+    def run(self):
+        management.call_command("compilemessages", verbosity=1)
+        build.run(self)
+
+
+cmdclass = {"build": CustomBuild}
+
+
+setup(
+    name="pretalx-venueless",
+    version="0.0.0",
+    description="Venueless integration in pretalx: Notify venueless about new schedule releases!",
+    long_description=long_description,
+    url="https://github.com/pretalx/pretalx-venueless",
+    author="Tobias Kunze",
+    author_email="r@rixx.de",
+    license="Apache Software License",
+    install_requires=[],
+    packages=find_packages(exclude=["tests", "tests.*"]),
+    include_package_data=True,
+    cmdclass=cmdclass,
+    entry_points="""
+[pretalx.plugin]
+pretalx_venueless=pretalx_venueless:PretalxPluginMeta
+""",
+)
