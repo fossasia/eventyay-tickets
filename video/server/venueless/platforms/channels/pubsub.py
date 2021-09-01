cceeed7d178caf1f07adb7c5d43d9b0bf54c183a
Raphael Michel <mail@raphaelmichel.de>
2021-09-01 21:29:16 +0200

    [server] Fix inconsistent hash function use

--- video/server/venueless/platforms/channels/pubsub.py
+++ video/server/venueless/platforms/channels/pubsub.py
@@ -1,3 +1,5 @@
+import binascii
+
 from channels_redis.pubsub import RedisPubSubChannelLayer
 
 
@@ -11,3 +13,15 @@ class ChannelLayer(RedisPubSubChannelLayer):
         if channel not in group_channels:
             return
         return await super().group_discard(group, channel)
+
+    def _get_shard(self, channel_or_group_name):
+        # There's a bug in channels_redis, which uses hash(), but hash() isn't consistent between machines.
+        # We'll file a bug/PR against channels_redis, but for the time being, this fixes the bug.
+        if len(self._shards) == 1:
+            # Avoid the overhead of hashing and modulo when it is unnecessary.
+            return self._shards[0]
+        if isinstance(channel_or_group_name, str):
+            channel_or_group_name = channel_or_group_name.encode("utf8")
+        bigval = binascii.crc32(channel_or_group_name) & 0xFFF
+        ring_divisor = 4096 / float(len(self._shards))
+        return self._shards[int(bigval / ring_divisor)]
