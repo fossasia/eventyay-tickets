b258c274ef3a98f08f145ffcfdb3563919c6d832
Tobias Kunze <r@rixx.de>
2021-09-23 03:00:22 +0200

    Use non-deprecated setting

--- src/pretalx/settings.py
+++ src/pretalx/settings.py
@@ -553,12 +553,14 @@ BOOTSTRAP4 = {
 }
 COMPRESS_ENABLED = COMPRESS_OFFLINE = not DEBUG
 COMPRESS_PRECOMPILERS = (("text/x-scss", "django_libsass.SassCompiler"),)
-COMPRESS_CSS_FILTERS = (
-    # CssAbsoluteFilter is incredibly slow, especially when dealing with our _flags.scss
-    # However, we don't need it if we consequently use the static() function in Sass
-    # 'compressor.filters.css_default.CssAbsoluteFilter',
-    "compressor.filters.cssmin.CSSCompressorFilter",
-)
+COMPRESS_FILTERS = {
+    "css": (
+        # CssAbsoluteFilter is incredibly slow, especially when dealing with our _flags.scss
+        # However, we don't need it if we consequently use the static() function in Sass
+        # 'compressor.filters.css_default.CssAbsoluteFilter',
+        "compressor.filters.cssmin.CSSCompressorFilter",
+    )
+}
 
 REST_FRAMEWORK = {
     "DEFAULT_RENDERER_CLASSES": (
