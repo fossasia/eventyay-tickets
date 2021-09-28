823012fe161292a1cf8ac56a406439654f29f281
rash <sebastian.gepperth@ax-semantics.com>
2021-09-28 16:02:14 +0200

    forgot to commit a file

---
+++ video/webapp/src/locales/index.js
@@ -0,0 +1,13 @@
+export const locales = [{
+	code: 'en',
+	nativeLabel: 'English',
+	englishLabel: 'English'
+}, {
+	code: 'de',
+	nativeLabel: 'Deutsch',
+	englishLabel: 'German'
+}, {
+	code: 'pt_BR',
+	nativeLabel: 'Português brasileiro',
+	englishLabel: 'Portuguese (Brazil)'
+}]
