d3598b8d73485d51baf664b1e415dd6324616590
Tobias Kunze <r@rixx.de>
2022-01-03 11:48:00 +0100

    [pretalx] Make apiUrl work in dev mode

--- video/webapp/src/views/admin/config/schedule.vue
+++ video/webapp/src/views/admin/config/schedule.vue
@@ -191,7 +191,8 @@ export default {
 				traits: ['schedule-update'],
 				long: true
 			})
-			window.location = `${this.config.pretalx.domain}orga/event/${this.config.pretalx.event}/settings/p/venueless/?url=${window.location.origin}${config.api.base}&token=${token}&returnUrl=${window.location.href}`
+			const apiUrl = config.api.base.startsWith("http") ? config.api.base : (window.location.origin + config.api.base)
+			window.location = `${this.config.pretalx.domain}orga/event/${this.config.pretalx.event}/settings/p/venueless/?url=${apiUrl}&token=${token}&returnUrl=${window.location.href}`
 		},
 		async save () {
 			this.$v.$touch()
