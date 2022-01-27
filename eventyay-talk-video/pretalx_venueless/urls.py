5326ad8353827a35c0fa19ef2a7e8b2b9e97798f
Tobias Kunze <r@rixx.de>
2022-01-27 19:33:09 +0100

    Code style

--- eventyay-talk-video/pretalx_venueless/urls.py
+++ eventyay-talk-video/pretalx_venueless/urls.py
@@ -1,16 +1,15 @@
-from pretalx.event.models.event import SLUG_CHARS
 from django.urls import path
 
 from . import views
 
 urlpatterns = [
     path(
-        f"orga/event/<slug:event>/settings/p/venueless/",
+        "orga/event/<slug:event>/settings/p/venueless/",
         views.Settings.as_view(),
         name="settings",
     ),
     path(
-        f"<slug:event>/p/venueless/check",
+        "<slug:event>/p/venueless/check",
         views.check,
         name="check",
     ),
