cd499c77bc44c6a97a28ee0c34b6af0fb0e993f9
Tobias Kunze <r@rixx.de>
2022-04-07 11:11:16 +0200

    Remove debug logic

--- src/pretalx/common/mixins/forms.py
+++ src/pretalx/common/mixins/forms.py
@@ -392,7 +392,6 @@ class JsonSubfieldMixin:
             elif getattr(self, "obj", None):
                 self.instance = self.obj
         for field, path in self.Meta.json_fields.items():
-            breakpoint()
             data_dict = getattr(self.instance, path) or {}
             if field in data_dict:
                 self.fields[field].initial = data_dict.get(field)
