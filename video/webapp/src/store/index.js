b50c0a31e7f81ab92d284ebcb1ea4403a64ba670
Raphael Michel <michel@rami.io>
2022-05-07 00:01:39 +0200

    Fix broken BBB if token is missing

--- video/webapp/src/store/index.js
+++ video/webapp/src/store/index.js
@@ -46,6 +46,7 @@ export default new Vuex.Store({
 		},
 		autoplay (state) {
 			if (state.autoplayUserSetting) return state.autoplayUserSetting
+			if (!state.token) return true
 			const token = jwtDecode(state.token)
 			return !doesTraitsMatchGrants(token.traits, state.world.onsite_traits)
 		}
