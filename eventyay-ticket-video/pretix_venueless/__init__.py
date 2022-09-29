65c3953ce26470ff14310ce21d5bd7c97c5b749b
Raphael Michel <michel@rami.io>
2022-09-29 13:35:10 +0200

    Bump version

--- eventyay-ticket-video/pretix_venueless/__init__.py
+++ eventyay-ticket-video/pretix_venueless/__init__.py
@@ -5,7 +5,7 @@ try:
 except ImportError:
     raise RuntimeError("Please use pretix 2.7 or above to run this plugin!")
 
-__version__ = '1.1.0'
+__version__ = '1.1.1'
 
 
 class PluginApp(PluginConfig):
