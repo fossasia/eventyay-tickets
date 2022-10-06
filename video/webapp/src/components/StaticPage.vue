ac68d3e000d53bba880878c6f80cb855f59ed3e8
rash <rash@rash.codes>
2022-10-06 22:43:42 +0200

    try unifying quill css

--- video/webapp/src/components/StaticPage.vue
+++ video/webapp/src/components/StaticPage.vue
@@ -19,30 +19,11 @@ export default {
 		width: 100%
 		max-width: 960px
 		margin: 0 auto
-
-	// TODO decopypaste thhis
 	.rich-text-content .ql-editor
 		margin: 16px 0
-		> *, li
-			max-width: 960px
-			margin: 0 auto
-			+below('m')
-				margin: 0 16px
-		ul, ol
-			margin: 0
-			padding: 0
-			max-width: none
-			li
-				list-style-position: inside
-		img
-			display: block
-			margin: 0 auto
-		.ql-full-width-true
-			margin: 0
-			max-width: none
+		// TODO :has isn't supported by FF yet, but it was borked anyways.
+		.ql-full-width-true:not(:has( > img))
 			margin: 0 16px
-			img, .ql-video-wrapper
-				margin: 0 -16px
 #app.override-sidebar-collapse .c-static-page .rich-text-content .ql-editor
 	> *, li
 		margin: 0 16px
