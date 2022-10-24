bc4ee243dc343b6651c1f46d512201b3aac92fa1
Tobias Kunze <r@rixx.de>
2022-10-24 12:22:41 +0200

    Add utility to move proposals to another event

---
+++ src/pretalx/submission/utils.py
@@ -0,0 +1,52 @@
+from django.db import transaction
+from django_scopes import scopes_disabled
+
+from pretalx.event.models import Event
+from pretalx.submission.models import Submission
+
+
+@scopes_disabled()
+@transaction.atomic
+def move_submission(code, new_event):
+    submission = Submission.objects.get(code__iexact=code)
+    old_event = submission.event
+    new_event = Event.objects.get(slug__iexact=new_event)
+
+    speaker_questions = {
+        str(q.question): q for q in new_event.questions.all().filter(target="speaker")
+    }
+    submission_questions = {
+        str(q.question): q
+        for q in new_event.questions.all().filter(target="submission")
+    }
+
+    submission.event = new_event
+    submission.save()
+
+    for answer in (
+        submission.answers.all()
+        .filter(question__event=old_event)
+        .select_related("question")
+    ):
+        other_question = submission_questions.get(str(answer.question.question))
+        if other_question:
+            answer.id = None
+            answer.question = other_question
+            answer.save()
+
+    for speaker in submission.speakers.all():
+        if not speaker.profiles.filter(event=new_event).exists():
+            old_profile = speaker.event_profile(old_event)
+            old_profile.pk = None
+            old_profile.event = new_event
+            old_profile.save()
+        for answer in (
+            speaker.answers.all()
+            .filter(question__event=old_event)
+            .select_related("question")
+        ):
+            other_question = speaker_questions.get(str(answer.question.question))
+            if other_question:
+                answer.id = None
+                answer.question = other_question
+                answer.save()
