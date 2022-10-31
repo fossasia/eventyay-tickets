b302da3828d419a6047d7f4dd08e76764f085e00
Tobias Kunze <r@rixx.de>
2022-11-01 00:14:59 +0100

    Release v1.2.2

--- eventyay-talk-video/pretalx_venueless/apps.py
+++ eventyay-talk-video/pretalx_venueless/apps.py
@@ -13,7 +13,7 @@ class PluginApp(AppConfig):
             "Venueless integration in pretalx: Notify venueless about new schedule releases!"
         )
         visible = True
-        version = "1.2.1"
+        version = "1.2.2"
 
     def ready(self):
         from . import signals  # NOQA
