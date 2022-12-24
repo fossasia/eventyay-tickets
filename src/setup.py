ecc032a08bcfded27fdcdda86aea703ddd453a83
Tobias Kunze <r@rixx.de>
2022-12-25 00:10:12 +0100

    Update beautifulsoup
    closes #1295

--- src/setup.py
+++ src/setup.py
@@ -92,7 +92,7 @@ setup(
     keywords="conference cfp event schedule",
     python_requires=">={}.{}".format(*REQUIRED_PYTHON),
     install_requires=[
-        "beautifulsoup4~=4.10.0",  # https://bazaar.launchpad.net/~leonardr/beautifulsoup/bs4/view/head:/CHANGELOG
+        "beautifulsoup4~=4.11.0",  # https://bazaar.launchpad.net/~leonardr/beautifulsoup/bs4/view/head:/CHANGELOG
         "bleach~=4.1.0",  # https://bleach.readthedocs.io/en/latest/changes.html
         "celery~=5.2.0",  # search for "what's new" on http://docs.celeryproject.org/en/latest/
         "csscompressor~=0.9.0",  # 2017-11, no changelog, https://github.com/sprymix/csscompressor
