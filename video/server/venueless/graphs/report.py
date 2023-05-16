f398118e860610442707569bca4f2bf6fc1829d0
Tobias Kunze <r@rixx.de>
2023-05-16 11:17:14 +0200

    Fix code style

--- video/server/venueless/graphs/report.py
+++ video/server/venueless/graphs/report.py
@@ -248,7 +248,9 @@ class ReportGenerator:
         )
 
         unique_users = rvqs.filter(room=room).values("user")
-        users_with_duration = User.objects.filter(id__in=unique_users,).annotate(
+        users_with_duration = User.objects.filter(
+            id__in=unique_users,
+        ).annotate(
             total_duration=Subquery(
                 rvqs.filter(
                     user=OuterRef("pk"),
