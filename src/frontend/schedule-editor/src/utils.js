3cd043beb880c628707f8060341e33c63f2f7e6b
Tobias Kunze <r@rixx.de>
2023-08-06 16:02:24 +0200

    Schedule editor: localise pretalx strings correctly

--- src/frontend/schedule-editor/src/utils.js
+++ src/frontend/schedule-editor/src/utils.js
@@ -1,11 +1,10 @@
-// import i18n from 'i18n'
+import i18next from 'i18next'
 
 export function getLocalizedString (string) {
 	if (typeof string === 'string') return string
 	try {
-		return Object.values(string)[0]
+		return string[i18next.language] || Object.values(string)[0]
 	} catch (e) {
 		return ""
 	}
-	// return string[i18n.locale] || string[i18n.fallbackLocale] || Object.values(string)[0]
 }
