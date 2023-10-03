44460f9af25ad8db6cee48cd44b71576a2a0637b
Tobias Kunze <r@rixx.de>
2023-10-03 11:23:58 +0200

    Add plugin category

--- eventyay-talk-video/pretalx_venueless/apps.py
+++ eventyay-talk-video/pretalx_venueless/apps.py
@@ -14,6 +14,7 @@ class PluginApp(AppConfig):
         )
         visible = True
         version = "1.3.0"
+        category = "INTEGRATION"
 
     def ready(self):
         from . import signals  # NOQA
