bedad74e1aef05c25c4b81254b2d51eac93381d8
Tobias Kunze <r@rixx.de>
2023-11-02 17:30:05 +0100

    Switch to pyproject.toml

--- eventyay-talk-video/pretalx_venueless/apps.py
+++ eventyay-talk-video/pretalx_venueless/apps.py
@@ -1,6 +1,8 @@
 from django.apps import AppConfig
 from django.utils.translation import gettext_lazy
 
+from . import __version__
+
 
 class PluginApp(AppConfig):
     name = "pretalx_venueless"
@@ -13,7 +15,7 @@ class PluginApp(AppConfig):
             "Venueless integration in pretalx: Notify venueless about new schedule releases!"
         )
         visible = True
-        version = "1.3.0"
+        version = __version__
         category = "INTEGRATION"
 
     def ready(self):
