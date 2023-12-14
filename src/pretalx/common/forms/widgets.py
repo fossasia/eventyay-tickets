a4be51141e9f0718648deb0e7e35e2e6bd376750
Tobias Kunze <r@rixx.de>
2023-12-15 00:36:37 +0100

    Fix zxcvbn.js path

--- src/pretalx/common/forms/widgets.py
+++ src/pretalx/common/forms/widgets.py
@@ -36,7 +36,7 @@ class PasswordStrengthInput(PasswordInput):
         return mark_safe(super().render(name, value, self.attrs) + markup)
 
     class Media:  # Note: we don't use {{ form.media }}, since it doesn't allow us to load media async, and the zxcvbn scripts are horribly slow
-        js = ("common/js/zxcvbn.js", "common/js/password_strength.js")
+        js = ("vendored/zxcvbn.js", "common/js/password_strength.js")
 
 
 class PasswordConfirmationInput(PasswordInput):
