a963ad9ad1351b28fac16cc0f1bc4acbe473316a
Martin Bähr <martin@codingforafrica.at>
2024-04-23 04:55:09 +0300

    restructure apps

--- src/pretix/helpers/__init__.py
+++ src/pretix/helpers/__init__.py
@@ -1,11 +1,1 @@
-from django.apps import AppConfig
-
 from .database import *  # noqa
-
-
-class PretixHelpersConfig(AppConfig):
-    name = 'pretix.helpers'
-    label = 'pretixhelpers'
-
-
-default_app_config = 'pretix.helpers.PretixHelpersConfig'
