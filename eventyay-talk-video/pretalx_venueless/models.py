7a21a09e8adb2ef0bfcdb45208b52d63de0dae19
Tobias Kunze <r@rixx.de>
2024-04-30 13:13:58 +0200

    Update location of pretalx.common.phrases

--- eventyay-talk-video/pretalx_venueless/models.py
+++ eventyay-talk-video/pretalx_venueless/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.utils.timezone import now
 from django.utils.translation import gettext_lazy as _
 from i18nfield.fields import I18nTextField
-from pretalx.common.phrases import phrases
+from pretalx.common.text.phrases import phrases
 
 
 class VenuelessSettings(models.Model):
