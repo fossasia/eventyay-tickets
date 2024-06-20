623cd4a38e2b1a3c8a981e66defbe9d38e7f4b5f
AviGawande <2041024@gcoej.ac.in>
2024-06-20 17:28:25 +0530

    add ukrainian language support

--- src/pretix/settings.py
+++ src/pretix/settings.py
@@ -449,12 +449,13 @@ ALL_LANGUAGES = [
     ('ru', _('Russian')),
     ('es', _('Spanish')),
     ('tr', _('Turkish')),
+    ('ua', _('Ukrainian')),
 ]
 LANGUAGES_OFFICIAL = {
     'en', 'de', 'de-informal'
 }
 LANGUAGES_INCUBATING = {
-    'pl', 'fi', 'pt-br'
+    'pl', 'fi', 'pt-br','ua'
 } - set(config.get('languages', 'allow_incubating', fallback='').split(','))
 LANGUAGES_RTL = {
     'ar', 'hw'
