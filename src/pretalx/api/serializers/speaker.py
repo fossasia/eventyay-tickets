5ec6ec82ebea27ca61889c05637fba70ef4ba4ea
Duong (Danny) Luu <51145179+lcduong@users.noreply.github.com>
2024-07-27 16:10:11 +0700

    Fix speaker view for eventyay-video (#145)
    * send invitation for additional speaker input
    * issue with speaker view
    * remove url import from other branch

--- src/pretalx/api/serializers/speaker.py
+++ src/pretalx/api/serializers/speaker.py
@@ -61,12 +61,12 @@ class SpeakerSerializer(ModelSerializer):
 
     @staticmethod
     def get_avatar_source(obj):
-        if obj.user.has_avatar:
+        if obj.user.has_avatar and obj.user.avatar_source != '':
             return obj.user.avatar_source(event=obj.event)
 
     @staticmethod
     def get_avatar_license(obj):
-        if obj.user.has_avatar:
+        if obj.user.has_avatar and obj.user.avatar_license != '':
             return obj.user.avatar_license(event=obj.event)
 
     @staticmethod
