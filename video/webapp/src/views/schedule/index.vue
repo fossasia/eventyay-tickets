c766dbc13589576e25a326b96deca47b75915ce1
Duong (Danny) Luu <51145179+lcduong@users.noreply.github.com>
2024-08-02 16:10:24 +0700

    Show filter and stars on schedule page (#193)
    * add button filter tracks, favorites for schedule screen
    
    * update filter type
    
    * correct track name value retrieve
    
    ---------
    
    Co-authored-by: odkhang <odkhang@tma.com.vn>

--- video/webapp/src/views/schedule/index.vue
+++ video/webapp/src/views/schedule/index.vue
@@ -4,6 +4,14 @@
 		//- .timezone-control
 		//- 	p timezone:
 		//- 	timezone-changer
+		div.filter-actions
+			bunt-button.bunt-ripple-ink(@click="open=true",
+				icon="filter-outline"
+				:class="{active: filter.type === 'track'}" ) {{ $t('Filter') }}
+			bunt-button.bunt-ripple-ink(v-if="favs",
+				icon="star"
+				@click="toggleFavFilter"
+				:class="{active: filter.type === 'fav'}") {{favs.length}}
 		bunt-tabs.days(v-if="days && days.length > 1", :active-tab="currentDay.toISOString()", ref="tabs", v-scrollbar.x="")
 			bunt-tab(v-for="day in days", :id="day.toISOString()", :header="moment(day).format('dddd DD. MMMM')", @selected="changeDay(day)")
 		.scroll-parent(ref="scrollParent", v-scrollbar.x.y="")
@@ -33,6 +41,12 @@
 		.mdi.mdi-alert-octagon
 		h1 {{ $t('schedule/index:scheduleLoadingError') }}
 	bunt-progress-circular(v-else, size="huge", :page="true")
+	prompt.c-filter-prompt(v-if="open", @close="open=false", name="filter-prompt")
+		.prompt-content
+			h1 {{ $t('Tracks')}}
+			template(v-for="track in schedule.tracks")
+				div.item(v-if="track")
+					bunt-checkbox(v-model="tracksFilter[track.id]",name="track_room_views") {{track.name}}
 </template>
 <script>
 import { mapState, mapGetters } from 'vuex'
@@ -40,20 +54,33 @@ import { LinearSchedule, GridSchedule} from '@pretalx/schedule'
 import moment from 'lib/timetravelMoment'
 import TimezoneChanger from 'components/TimezoneChanger'
 import scheduleProvidesMixin from 'components/mixins/schedule-provides'
+import Prompt from 'components/Prompt'
 
 export default {
-	components: { LinearSchedule, GridSchedule, TimezoneChanger },
+	components: { LinearSchedule, GridSchedule, TimezoneChanger, Prompt },
 	mixins: [scheduleProvidesMixin],
 	data () {
 		return {
+			tracksFilter: {},
+			open: false,
 			moment,
 			currentDay: moment().startOf('day')
 		}
 	},
 	computed: {
 		...mapState(['now']),
-		...mapState('schedule', ['schedule', 'errorLoading']),
+		...mapState('schedule', ['schedule', 'errorLoading', 'filter']),
 		...mapGetters('schedule', ['days', 'rooms', 'sessions', 'favs']),
+	},
+	watch: {
+		tracksFilter: {
+			handler: function (newValue) {
+				if (!this.open) return
+				const arr = Object.keys(newValue).filter(key => newValue[key])
+				this.$store.dispatch('schedule/filter', {type: 'track', tracks: arr})
+			},
+			deep: true
+		}
 	},
 	methods: {
 		changeDay (day) {
@@ -65,6 +92,14 @@ export default {
 			const tabEl = this.$refs.tabs.$refs.tabElements.find(el => el.id === day.toISOString())
 			// TODO smooth scroll, seems to not work with chrome {behavior: 'smooth', block: 'center', inline: 'center'}
 			tabEl?.$el.scrollIntoView()
+		},
+		toggleFavFilter () {
+			this.tracksFilter = {}
+			if (this.filter.type === 'fav') {
+				this.$store.dispatch('schedule/filter', {})
+			} else {
+				this.$store.dispatch('schedule/filter', {type: 'fav'})
+			}
 		}
 	}
 }
@@ -110,4 +145,21 @@ export default {
 	.scroll-parent
 		.bunt-scrollbar-rail-wrapper-x, .bunt-scrollbar-rail-wrapper-y
 			z-index: 30
+	.c-filter-prompt
+		.bunt-scrollbar
+			border-radius: 30px
+		.prompt-content
+			padding: 16px
+			display: flex
+			flex-direction: column
+			.item
+				margin: 4px 0px
+	.bunt-ripple-ink
+		width: fit-content
+		padding: 0px 16px
+		border-radius: 2px
+		height: 32px
+		margin: 16px 8px
+		&.active
+			border: 2px solid #f9a557
 </style>
