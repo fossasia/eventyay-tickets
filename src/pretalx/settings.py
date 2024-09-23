a232ab5bb0ca1f7b25252d1f58b1f7bc3a3c66ca
Tobias Kunze <r@rixx.de>
2024-09-23 20:34:45 +0200

    Update postgres settings

--- src/pretalx/settings.py
+++ src/pretalx/settings.py
@@ -199,7 +199,8 @@ DATABASES = {
         "PASSWORD": config.get("database", "password"),
         "HOST": config.get("database", "host"),
         "PORT": config.get("database", "port"),
-        "CONN_MAX_AGE": 0 if db_backend == "sqlite3" or HAS_CELERY else 120,
+        "CONN_MAX_AGE": 0 if db_backend == "sqlite3" else 120,
+        "CONN_HEALTH_CHECKS": db_backend != "sqlite3",
         "OPTIONS": {"pool": True} if db_backend == "postgresql" else {},
         "TEST": {},
     }
