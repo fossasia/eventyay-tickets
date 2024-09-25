051ffd54ae1f2face301b5e4218467e9ab2956b9
Tobias Kunze <r@rixx.de>
2024-09-25 11:31:18 +0200

    Improve schedule widget data caching

--- src/pretalx/common/views/cache.py
+++ src/pretalx/common/views/cache.py
@@ -1,32 +1,131 @@
+import hashlib
+
+from django.core.cache import caches
+from django.http import HttpResponse, HttpResponseNotModified
+from django.utils.cache import (
+    get_cache_key,
+    get_max_age,
+    has_vary_header,
+    learn_cache_key,
+    patch_response_headers,
+)
 from django.views.decorators.cache import cache_page
 
 
-def conditional_cache_page(
-    timeout, condition, *, cache=None, key_prefix=None, cache_control=None
-):
-    """This decorator is exactly like cache_page, but with the option to skip
-    the caching entirely.
+def get_requested_etag(request):
+    """Return the ETag requested by the client, or None if not found."""
+    if if_none_match := request.headers.get("If-None-Match"):
+        return if_none_match
 
-    The second argument is a callable, ``condition``. It's given the
-    request and all further arguments, and if it evaluates to a true-ish
-    value, the cache is used.
+
+def get_etag(response):
+    content = response.content
+    if isinstance(content, str):
+        content = content.encode()
+    return hashlib.md5(content).hexdigest()
+
+
+def conditional_cache_page(
+    timeout, *, cache=None, key_prefix=None, condition=None, server_timeout=None
+):
+    """This decorator wraps Django's cache_page decorator, and adds some features:
+
+    - The option to skip caching entirely based on a condition.
+      If the condition function returns False, the page will not be cached at all.
+    - Allow the response to be cached for a different amount of time on the server
+      than we tell to the client (useful because we can invalidate the cache).
+    - Calculate an ETag based on the response content, and cache it along with the
+      cached response. If an ETag is requested, but not found in the cache, the
+      response will be recalculated and cached with the ETag.
     """
 
     def decorator(func):
         def wrapper(request, *args, **kwargs):
-            if condition(request, *args, **kwargs):
-                prefix = key_prefix
-                if callable(prefix):
-                    prefix = prefix(request, *args, **kwargs)
-                response = cache_page(timeout=timeout, cache=cache, key_prefix=prefix)(
-                    func
-                )(request, *args, **kwargs)
-                if cache_control and not cache_control(request, *args, **kwargs):
-                    response.headers.pop("Expires")
-                    response.headers.pop("Cache-Control")
+            if request.method != "GET":
+                return func(request, *args, **kwargs)
+            if condition and not condition(request, *args, **kwargs):
+                return func(request, *args, **kwargs)
 
-            return func(request, *args, **kwargs)
+            return etag_cache_page(
+                timeout,
+                cache_alias=cache,
+                key_prefix=key_prefix,
+                server_timeout=server_timeout,
+                request=request,
+                request_args=args,
+                request_kwargs=kwargs,
+                handler=func,
+            )
 
         return wrapper
 
     return decorator
+
+
+def should_cache(request, response):
+    if response.streaming or response.status_code not in (200, 304):
+        return False
+    if not request.COOKIES and response.cookies and has_vary_header(response, "Cookie"):
+        return False
+    # Don't cache a response with 'Cache-Control: private'
+    if "private" in response.get("Cache-Control", ()):
+        return False
+
+    # The timeout from the "max-age" section of the "Cache-Control"
+    # header takes precedence over the default cache timeout.
+    if get_max_age(response) == 0:
+        return False
+    return True
+
+
+def etag_cache_page(
+    timeout,
+    *,
+    cache_alias=None,
+    key_prefix=None,
+    server_timeout=None,
+    request=None,
+    request_args=None,
+    request_kwargs=None,
+    handler=None,
+):
+    server_timeout = server_timeout or timeout
+    requested_etag = get_requested_etag(request)
+    if callable(key_prefix):
+        key_prefix = key_prefix(request, *request_args, **request_kwargs)
+
+    cache = caches[cache_alias or "default"]
+    cache_key = get_cache_key(request, key_prefix, "GET", cache=cache_alias)
+    etag_suffix = "_etag"
+    current_etag = cache.get(f"{cache_key}{etag_suffix}") if cache_key else None
+
+    if current_etag and requested_etag and current_etag != requested_etag:
+        current_etag = None
+    elif requested_etag and current_etag == requested_etag:
+        return HttpResponseNotModified()
+
+    if cache_key and (cached_response := cache.get(cache_key)):
+        response = HttpResponse(cached_response)
+    else:
+        response = cache_page(
+            timeout=server_timeout, cache=cache_alias, key_prefix=key_prefix
+        )(handler)(request, *request_args, **request_kwargs)
+        if not should_cache(request, response):
+            return response
+
+    cache_key = cache_key or learn_cache_key(
+        request, response, timeout, key_prefix, cache=cache_alias
+    )
+
+    if not current_etag:
+        current_etag = get_etag(response)
+        cache.set(f"{cache_key}{etag_suffix}", current_etag, timeout=server_timeout)
+
+    # If an ETag was requested and we forgot about it, but now our response matches the
+    # requested ETag, return 304
+    if requested_etag and current_etag == requested_etag:
+        return HttpResponseNotModified()
+
+    response["ETag"] = current_etag
+    patch_response_headers(response, timeout)
+    return response
