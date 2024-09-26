080d1e73502968796d58b4899dceaa3b0881c866
Tobias Kunze <r@rixx.de>
2024-09-26 12:48:33 +0200

    Reimplement tabs

--- src/pretalx/orga/views/speaker.py
+++ src/pretalx/orga/views/speaker.py
@@ -361,6 +361,14 @@ class SpeakerExport(EventPermissionRequired, FormView):
             if exporter.group == "speaker"
         ]
 
+    @context
+    def tablist(self):
+        return {
+            "custom": _("CSV/JSON exports"),
+            "general": _("More exports"),
+            "api": _("API"),
+        }
+
     def form_valid(self, form):
         result = form.export_data()
         if not result:
