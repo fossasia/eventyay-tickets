767e99d852e090fbc9a79f4d164892406194c11a
lcduong <lcduong@tma.com.vn>
2024-10-04 14:30:54 +0700

    disable scope for api ticket check

--- src/pretix/api/views/event.py
+++ src/pretix/api/views/event.py
@@ -407,7 +407,7 @@ class EventSettingsView(views.APIView):
 
 class CustomerOrderCheckView(views.View):
 
-    @scopes_disabled
+    @scopes_disabled()
     def get(self, request, *args, **kwargs):
         if (not kwargs.get("customer_id")
                 or not kwargs.get("event")
