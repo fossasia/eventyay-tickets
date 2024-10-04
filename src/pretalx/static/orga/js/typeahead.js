727d454580a6740287978f122c2b6c84e2e3f6e5
Tobias Kunze <r@rixx.de>
2024-10-04 14:14:16 +0200

    Fix orga nav

--- src/pretalx/static/orga/js/typeahead.js
+++ src/pretalx/static/orga/js/typeahead.js
@@ -1,212 +1,119 @@
-$(() => {
-    $("#nav-search-wrapper").on("shown.bs.collapse shown.bs.dropdown", () =>
-        $("#nav-search-dropdown input").val("").change().focus(),
-    )
-    $("#nav-search-dropdown input").click(function (e) {
-        e.stopPropagation()
-    })
+const initNavSearch = () => {
+    const wrapper = document.querySelector("#nav-search-wrapper")
+    const summary = wrapper.querySelector("summary")
+    const searchInput = wrapper.querySelector("input")
+    const searchWrapper = wrapper.querySelector("#nav-search-input-wrapper")
+    const apiURL = searchWrapper.getAttribute("data-source")
+    const queryStr = "?" + (typeof searchWrapper.getAttribute("data-organiser") !== "undefined" ? "&organiser=" + searchWrapper.getAttribute("data-organiser") : "") + "&query="
 
-    $("[data-event-typeahead]").each(function () {
-        var $container = $(this)
-        var $query = $(this).find("[data-typeahead-query]").length
-            ? $(this).find("[data-typeahead-query]")
-            : $($(this).attr("data-typeahead-field"))
-        var lastQuery = null
-        var loadIndicatorTimeout = null
+    let loadIndicatorTimeout = null
+    const showLoadIndicator = () => {
+        searchWrapper.querySelector("ul").innerHTML = '<li class="loading"><span class="fa fa-4x fa-cog fa-spin"></span></li>'
+    }
 
-        function showLoadIndicator() {
-            $container.find("li").remove()
-            $container
-                .find("ul")
-                .append(
-                    "<li class='loading'><span class='fa fa-4x fa-cog fa-spin'></span></li>",
-                )
-            $container.toggleClass(
-                "focused",
-                $query.is(":focus") && $container.children().length > 0,
-            )
+    let lastQuery = null
+    const triggerSearch = () => {
+        if (searchInput.value === lastQuery) {
+            return
         }
+        searchInput.classList.remove("no-focus")
 
-        $query.on("change", function () {
-            if ($query.val() === lastQuery) {
+        const thisQuery = searchInput.value
+        lastQuery = thisQuery
+        if (loadIndicatorTimeout) clearTimeout(loadIndicatorTimeout)
+        loadIndicatorTimeout = setTimeout(showLoadIndicator, 80)
+
+        fetch(apiURL + queryStr + encodeURIComponent(searchInput.value)).then((response) => {
+            if (thisQuery !== lastQuery) {
+                // Ignore this response, it's for an old query
                 return
             }
+            if (loadIndicatorTimeout) clearTimeout(loadIndicatorTimeout)
 
-            var thisQuery = $query.val()
-            lastQuery = thisQuery
-            window.clearTimeout(loadIndicatorTimeout)
-            loadIndicatorTimeout = window.setTimeout(showLoadIndicator, 80)
-
-            $.getJSON(
-                $container.attr("data-source") +
-                    "?query=" +
-                    encodeURIComponent($query.val()) +
-                    (typeof $container.attr("data-organiser") !== "undefined"
-                        ? "&organiser=" + $container.attr("data-organiser")
-                        : ""),
-                function (data) {
-                    if (thisQuery !== lastQuery) {
-                        // Ignore this response, it's for an old query
-                        return
+            response.json().then((data) => {
+                searchWrapper.querySelectorAll("li").forEach((el) => el.remove())
+                data.results.forEach((res) => {
+                    let content = ""
+                    if (res.type === "organiser" || res.type === "user") {
+                        const icon = res.type === "organiser" ? "fa-users" : "fa-user"
+                        content = `<span class="search-title"><i class="fa fa-fw ${icon}"></i> ${res.name}</span>`
+                    } else if (res.type === "submission" || res.type === "speaker") {
+                        content = `<span class="search-title"><span>${res.name}</span></span>
+                            <span class="search-detail"><span class="fa fa-calendar fa-fw"></span> ${res.event}</span>`
+                    } else if (res.type === "event") {
+                        content = `<span class="search-title"><span>${res.name}</span></span>
+                            <span class="search-detail"><span class="fa fa-users fa-fw"></span> ${res.organiser}</span>
+                            <span class="search-detail"><span class="fa fa-calendar fa-fw"></span> ${res.date_range}</span>`
                     }
-                    window.clearTimeout(loadIndicatorTimeout)
 
-                    $container.find("li").remove()
-                    $.each(data.results, function (i, res) {
-                        let $content = $("<div>")
-                        if (res.type === "organiser" || res.type === "user") {
-                            const icon =
-                                res.type === "organiser"
-                                    ? "fa-users"
-                                    : "fa-user"
-                            $content.append(
-                                $("<span>")
-                                    .addClass("search-title")
-                                    .append(
-                                        $("<i>").addClass("fa fa-fw " + icon),
-                                    )
-                                    .append(" ")
-                                    .append($("<span>").text(res.name)),
-                            )
-                        } else if (
-                            res.type === "submission" ||
-                            res.type === "speaker"
-                        ) {
-                            $content
-                                .append(
-                                    $("<span>")
-                                        .addClass("search-title")
-                                        .append($("<span>").text(res.name)),
-                                )
-                                .append(
-                                    $("<span>")
-                                        .addClass("search-detail")
-                                        .append(
-                                            $("<span>").addClass(
-                                                "fa fa-calendar fa-fw",
-                                            ),
-                                        )
-                                        .append(" ")
-                                        .append(
-                                            $("<div>").text(res.event).html(),
-                                        ),
-                                )
-                        } else if (res.type === "event") {
-                            $content
-                                .append(
-                                    $("<span>")
-                                        .addClass("search-title")
-                                        .append($("<span>").text(res.name)),
-                                )
-                                .append(
-                                    $("<span>")
-                                        .addClass("search-detail")
-                                        .append(
-                                            $("<span>").addClass(
-                                                "fa fa-users fa-fw",
-                                            ),
-                                        )
-                                        .append(" ")
-                                        .append(
-                                            $("<div>")
-                                                .text(res.organiser)
-                                                .html(),
-                                        ),
-                                )
-                                .append(
-                                    $("<span>")
-                                        .addClass("search-detail")
-                                        .append(
-                                            $("<span>").addClass(
-                                                "fa fa-calendar fa-fw",
-                                            ),
-                                        )
-                                        .append(" ")
-                                        .append(res.date_range),
-                                )
-                        }
+                    const li = document.createElement("li")
+                    li.innerHTML = `<a href="${res.url}">${content}</a>`
+                    searchWrapper.querySelector("ul").append(li)
+                }) /* data.results.forEach */
+            }) /* response.json().then */
+        }) /* fetch.then */
+    }
 
-                        $container.find("ul").append(
-                            $("<li>").append(
-                                $("<a>")
-                                    .attr("href", res.url)
-                                    .append($content)
-                                    .on("mousedown", function (event) {
-                                        if ($(this).length) {
-                                            location.href = $(this).attr("href")
-                                        }
-                                        $(this).parent().addClass("active")
-                                        event.preventDefault()
-                                        event.stopPropagation()
-                                    }),
-                            ),
-                        )
-                    })
-                    $container.toggleClass(
-                        "focused",
-                        $query.is(":focus") && $container.children().length > 0,
-                    )
-                },
-            )
-        })
-        $query.on("keydown", function (event) {
-            var $selected = $container.find(".active")
-            if (event.which === 13) {
-                // enter
-                var $link = $selected.find("a")
-                if ($link.length) {
-                    location.href = $link.attr("href")
-                }
-                event.preventDefault()
-                event.stopPropagation()
+    searchInput.addEventListener("keydown", (ev) => {
+        // Keyboard navigation: enter
+        if (ev.key === "Enter") {
+            const selected = searchWrapper.querySelector("li.active a")
+            if (selected) {
+                location.href = selected.href
+                ev.preventDefault()
+                ev.stopPropagation()
             }
-        })
-        $query.on("blur", function (event) {
-            $container.removeClass("focused")
-        })
-        $query.on("keyup", function (event) {
-            var $first = $container.find("li:not(.query-holder)").first()
-            var $last = $container.find("li:not(.query-holder)").last()
-            var $selected = $container.find(".active")
-
-            if (event.which === 13) {
-                // enter
-                event.preventDefault()
-                event.stopPropagation()
-                return true
-            } else if (event.which === 40) {
-                // down
-                var $next
-                if ($selected.length === 0) {
-                    $next = $first
-                } else {
-                    $next = $selected.next()
-                }
-                if ($next.length === 0) {
-                    $next = $first
-                }
-                $selected.removeClass("active")
-                $next.addClass("active")
-                event.preventDefault()
-                event.stopPropagation()
-                return true
-            } else if (event.which === 38) {
-                // up
-                if ($selected.length === 0) {
-                    $selected = $first
-                }
-                var $prev = $selected.prev()
-                if ($prev.length === 0 || $prev.find("input").length > 0) {
-                    $prev = $last
-                }
-                $selected.removeClass("active")
-                $prev.addClass("active")
-                event.preventDefault()
-                event.stopPropagation()
-                return true
-            } else {
-                $(this).change()
-            }
-        })
+        } else if (ev.key === "ArrowDown" || ev.key === "ArrowUp") {
+            ev.preventDefault()
+            ev.stopPropagation()
+        }
     })
-})
+
+    searchInput.addEventListener("input", () => {triggerSearch()})
+
+    // Focus search input when dropdown is expanded, and trigger empty search
+    wrapper.addEventListener("click", () => {
+        triggerSearch()
+        setTimeout(() => {
+            if (wrapper.getAttribute("open") === "") {
+                searchInput.focus()
+            } else {
+                // Clear search input when dropdown is collapsed
+                searchInput.value = ""
+            }
+        }, 0)
+    })
+
+    searchInput.addEventListener("keyup", (ev) => {
+        const first = searchWrapper.querySelector("li:not(.query-holder)")
+        const last = searchWrapper.querySelector("li:not(.query-holder):last-child")
+        const selected = searchWrapper.querySelector("li.active")
+
+        // Keyboard navigation: down
+        if (ev.key === "ArrowDown") {
+            const next = (selected && selected.nextElementSibling) ? selected.nextElementSibling : first
+            if (!next) return
+            searchInput.classList.add("no-focus")
+            if (selected) { selected.classList.remove("active") }
+            next.classList.add("active")
+            ev.preventDefault()
+            ev.stopPropagation()
+        } else if (ev.key === "ArrowUp") {
+            // Keyboard navigation: up
+            const prev = (selected && selected.previousElementSibling) ? selected.previousElementSibling : last
+            if (!prev || prev.querySelector("input")) return
+            searchInput.classList.add("no-focus")
+            if (selected) { selected.classList.remove("active") }
+            prev.classList.add("active")
+            ev.preventDefault()
+            ev.stopPropagation()
+        } else if (ev.key === "Enter") {
+            // Keyboard navigation: enter
+            ev.preventDefault()
+            ev.stopPropagation()
+            return true
+        }
+    })
+}
+
+onReady(initNavSearch)
