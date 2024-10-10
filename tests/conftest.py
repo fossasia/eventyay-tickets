211514b48cac1825f258a95dafa0af0a2252363b
Srivatsav Auswin <75414859+Sak1012@users.noreply.github.com>
2024-10-10 12:11:19 +0530

    Fixed Workflow and Added Tests (#3)
    * Fix Workflow
    
    * Added tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,1 +1,15 @@
-# put your pytest fixtures here
+import pytest
+from pretix.base.models import Organizer, Event
+from django.utils.timezone import now
+
+@pytest.fixture
+def event(db):
+    organizer = Organizer.objects.create(name="Test Organizer", slug="test-organizer")
+    event = Event.objects.create(
+        organizer=organizer,
+        name="Test Event",
+        slug="test-event",
+        live=True,
+        date_from=now(),
+    )
+    return event
