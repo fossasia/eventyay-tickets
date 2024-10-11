722af4004a38b11194e9be6b93a58e60e0f28465
Tobias Kunze <r@rixx.de>
2024-10-12 00:41:04 +0200

    Fix broken event slug suggestion

--- src/pretalx/static/orga/js/event_wizard.js
+++ src/pretalx/static/orga/js/event_wizard.js
@@ -1,15 +1,18 @@
 const number = new Date().getYear() + 1900
 const updateSlug = (ev) => {
-    const value = document.querySelector("#event-name input").value
+    const value = ev.target.value
     let slug = value.replace(/\W+/g, "-").toLowerCase()
     if (slug && slug.indexOf(number) == -1) slug += "-" + number
     if (slug) document.querySelector("#id_basics-slug").value = slug
 }
-document.querySelectorAll("#event-name input").forEach((element) => {
-    element.addEventListener("input", updateSlug)
-})
-document.querySelector("#id_basics-slug").addEventListener("input", (ev) => {
-    document.querySelectorAll("#event-name input").forEach((element) => {
-        element.removeEventListener("input", updateSlug)
+onReady(() => {
+    const i18nNameInputs = document.getElementById("id_basics-name_0").parentElement.querySelectorAll("input")
+    i18nNameInputs.forEach((element) => {
+        element.addEventListener("input", updateSlug)
+    })
+    document.querySelector("#id_basics-slug").addEventListener("input", (ev) => {
+        i18nNameInputs.forEach((element) => {
+            element.removeEventListener("input", updateSlug)
+        })
     })
 })
