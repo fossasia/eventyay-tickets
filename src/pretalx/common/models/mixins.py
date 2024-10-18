d6b08c9e6c4d512181de583c2e771c0be6dbf526
Tobias Kunze <r@rixx.de>
2024-10-18 13:15:47 +0200

    Clean up unused files

--- src/pretalx/common/models/mixins.py
+++ src/pretalx/common/models/mixins.py
@@ -4,6 +4,7 @@ from contextlib import suppress
 from django.contrib.contenttypes.models import ContentType
 from django.db import models
 from django.utils.crypto import get_random_string
+from django.utils.functional import cached_property
 from django_scopes import ScopedManager, scopes_disabled
 from i18nfield.utils import I18nJSONEncoder
 
@@ -61,13 +62,50 @@ class LogMixin:
 class FileCleanupMixin:
     """Deletes all uploaded files when object is deleted."""
 
-    def _delete_files(self):
-        file_attributes = [
+    @cached_property
+    def _file_fields(self):
+        return [
             field.name
             for field in self._meta.fields
             if isinstance(field, models.FileField)
         ]
-        for field in file_attributes:
+
+    def save(self, *args, **kwargs):
+        update_fields = kwargs.get("update_fields", None)
+        if not self.pk or (
+            update_fields and not set(self._file_fields) & set(update_fields)
+        ):
+            return super().save(*args, **kwargs)
+
+        try:
+            pre_save_instance = self.__class__.objects.get(pk=self.pk)
+        except Exception:
+            return super().save(*args, **kwargs)
+
+        for field in self._file_fields:
+            old_value = getattr(pre_save_instance, field)
+            if old_value:
+                new_value = getattr(self, field)
+                if new_value and old_value.path != new_value.path:
+                    # We don't want to delete the file immediately, as the save action
+                    # that triggered this task might still fail, so we schedule the
+                    # deletion for 10 seconds in the future, and pass the file field
+                    # to check if the file is still in use.
+                    from pretalx.common.tasks import task_cleanup_file
+
+                    task_cleanup_file.apply_async(
+                        kwargs={
+                            "model": str(self._meta.model_name.capitalize()),
+                            "pk": self.pk,
+                            "field": field,
+                            "path": old_value.path,
+                        },
+                        countdown=10,
+                    )
+        return super().save(*args, **kwargs)
+
+    def _delete_files(self):
+        for field in self._file_fields:
             value = getattr(self, field, None)
             if value:
                 with suppress(Exception):
