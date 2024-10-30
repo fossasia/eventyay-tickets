a6dc697a393b04ebeb9033e8d8dbb2eb83cdfafc
Duong (Danny) Luu <51145179+lcduong@users.noreply.github.com>
2024-10-30 11:46:20 +0700

    fix speaker UI broken and remove sso login with customer account (#230)
    Co-authored-by: odkhang <odkhang@tma.com.vn>

--- src/pretalx/common/templatetags/socialapp_extras.py
+++
@@ -1,9 +0,0 @@
-from allauth.socialaccount.models import SocialApp
-from django import template
-
-register = template.Library()
-
-
-@register.simple_tag
-def socialapp_exists(provider):
-    return SocialApp.objects.filter(provider=provider).exists()
