8b5f87e162c505c72a5fc2ced1786329d403576d
odkhang <odkhang@tma.com.vn>
2024-11-01 17:16:18 +0700

    Fix conflict pretix base migration

--- src/pretix/base/migrations/0003_event_is_video_creation_and_more.py
+++ src/pretix/base/migrations/0003_event_is_video_creation_and_more.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("pretixbase", "0002_question_description"),
+        ("pretixbase", "0003_alter_cachedcombinedticket_id_alter_cachedticket_id_and_more"),
     ]
 
     operations = [
