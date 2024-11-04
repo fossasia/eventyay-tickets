c2a1eee02dd6cf21a05c59a1bcb4b555448eb760
odkhang <odkhang@tma.com.vn>
2024-11-04 17:56:00 +0700

    correct next url in login page

--- src/pretix/control/templatetags/append_next_url.py
+++ src/pretix/control/templatetags/append_next_url.py
@@ -8,5 +8,5 @@ register = template.Library()
 @register.simple_tag
 def append_next(next_url=None):
     if next_url and next_url.strip():
-        return f'?next={urlencode({"next": next_url})}'
+        return f'?{urlencode({"next": next_url})}'
     return ''
