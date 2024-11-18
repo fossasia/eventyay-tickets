682db772b94b4592f2ca9cbed3c648e6a17861b8
odkhang <odkhang@tma.com.vn>
2024-11-18 14:08:16 +0700

    Add organizer dropdown menu on top right (#425)
    * Implement dropdown menu for ticket component
    
    * Update code
    
    * Optimize code
    
    * Modify variable name

--- src/pretix/static/utils/js/utils.js
+++ src/pretix/static/utils/js/utils.js
@@ -9,3 +9,13 @@ function togglePopover(element) {
         $(element).popover('show');
     }
 }
+
+function handleProfileMenuClick() {
+    $('.profile-menu').off('click').on('click', function (event) {
+        event.preventDefault();
+        const link = $(this).find('a');
+        if (link.length > 0) {
+            window.location.href = link.attr('href');
+        }
+    });
+}
