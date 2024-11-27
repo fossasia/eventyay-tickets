4156e2568f96040f181a741d8c7723a3ba5dd1ed
odkhang <odkhang@tma.com.vn>
2024-11-27 16:24:32 +0700

    Update code

--- src/pretix/urls.py
+++ src/pretix/urls.py
@@ -11,6 +11,7 @@ from pretix.control.views import pages
 from .base.views import cachedfiles, csp, health, js_catalog, metrics, redirect
 
 base_patterns = [
+
     url(r'^download/(?P<id>[^/]+)/$', cachedfiles.DownloadView.as_view(),
         name='cachedfile.download'),
     url(r'^healthcheck/$', health.healthcheck,
@@ -33,9 +34,9 @@ common_patterns = [
     url(r'^common/', include((pretix.eventyay_common.urls, 'common'))),
 ]
 
-page_patterns = [
-    path('<slug:slug>/', pages.ShowPageView.as_view(), name="page"),
-]
+# page_patterns = [
+#     path('<slug:slug>/', pages.ShowPageView.as_view(), name="page"),
+# ]
 
 debug_patterns = []
 if settings.DEBUG:
@@ -46,4 +47,4 @@ if settings.DEBUG:
     except ImportError:
         pass
 
-common_patterns = base_patterns + control_patterns + debug_patterns + common_patterns + page_patterns
+common_patterns = base_patterns + control_patterns + debug_patterns + common_patterns
