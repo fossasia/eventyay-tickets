c7133ccaa995c41575378b459f66ef40e634cfdb
Tobias Kunze <r@rixx.de>
2024-12-06 14:28:41 +0100

    Possibly fix new spurious test failures

--- src/pretalx/person/services.py
+++ src/pretalx/person/services.py
@@ -3,10 +3,10 @@ import datetime as dt
 from django.utils.crypto import get_random_string
 from django.utils.timezone import now
 
-from pretalx.person.models import SpeakerProfile, User
-
 
 def create_user(email, name=None, pw_reset_days=60, event=None):
+    from pretalx.person.models import SpeakerProfile, User
+
     user = User.objects.create_user(
         password=get_random_string(32),
         email=email.lower().strip(),
