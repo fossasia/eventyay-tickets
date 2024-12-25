c87cf23ffa0a05ea2772bbe36ceac1671a497107
HungNgien <nguyenhung@tma.com.vn>
2024-12-25 18:12:15 +0700

    Add UI page for social login (#483)
    * Add UI for generating sso keys
    
    * Check isort and flake8
    
    * Add sign-in button of MediaWiki, Google, Github
    
    * Add back url
    
    * Refactor with sourcery
    
    * Add name url
    
    * Fix test_urls and minor refactor
    
    * Remove print
    
    * Change message log
    
    * Resolve review conversations
    
    * Refactor code
    
    * Remove blank
    
    * Change quote type
    
    * Remove str() usage
    
    * Add UI for toggle on/off login button with providers, delete backends
    
    * Change to use global signal
    
    * Remove backends
    
    * Add back commas
    
    * Add enum, remove inline css
    
    * Add collapse for login form
    
    * Remove signal, add translate
    
    * Update .po files, resolve conversation

--- src/pretix/plugins/socialauth/views.py
+++ src/pretix/plugins/socialauth/views.py
@@ -1,13 +1,32 @@
 import logging
+from enum import StrEnum
+from urllib.parse import urlencode, urlparse, urlunparse
 
+from allauth.socialaccount.adapter import get_adapter
 from django.conf import settings
 from django.contrib import messages
 from django.shortcuts import redirect
+from django.urls import reverse
+from django.views.generic import TemplateView
 
 from pretix.base.models import User
+from pretix.base.settings import GlobalSettingsObject
+from pretix.control.permissions import AdministratorPermissionRequiredMixin
 from pretix.control.views.auth import process_login_and_set_cookie
+from pretix.helpers.urls import build_absolute_uri
 
 logger = logging.getLogger(__name__)
+adapter = get_adapter()
+
+
+def oauth_login(request, provider):
+    base_url = adapter.get_provider(request, provider).get_login_url(request)
+    query_params = {
+        "next": build_absolute_uri("plugins:socialauth:social.oauth.return")
+    }
+    parsed_url = urlparse(base_url)
+    updated_url = parsed_url._replace(query=urlencode(query_params))
+    return redirect(urlunparse(updated_url))
 
 
 def oauth_return(request):
@@ -28,3 +47,37 @@ def oauth_return(request):
         )
         logger.error('Error while authorizing: user has no email address.')
         return redirect('control:auth.login')
+
+
+class LoginState(StrEnum):
+    ENABLED = "enabled"
+    DISABLED = "disabled"
+
+
+class SocialLoginView(AdministratorPermissionRequiredMixin, TemplateView):
+    template_name = 'socialauth/social_auth_settings.html'
+    LOGIN_PROVIDERS = {'mediawiki': False, 'github': False, 'google': False}
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.gs = GlobalSettingsObject()
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        if self.gs.settings.get('login_providers', as_type=dict) is None:
+            self.gs.settings.set('login_providers', self.LOGIN_PROVIDERS)
+        context['login_providers'] = self.gs.settings.get('login_providers', as_type=dict)
+        return context
+
+    def post(self, request, *args, **kwargs):
+        login_providers = self.gs.settings.get('login_providers', as_type=dict)
+        for provider in self.LOGIN_PROVIDERS.keys():
+            value = request.POST.get(f'{provider}_login', '').lower()
+            if value not in [s.value for s in LoginState]:
+                continue
+            login_providers[provider] = value == LoginState.ENABLED
+        self.gs.settings.set('login_providers', login_providers)
+        return redirect(self.get_success_url())
+
+    def get_success_url(self) -> str:
+        return reverse('plugins:socialauth:admin.global.social.auth.settings')
