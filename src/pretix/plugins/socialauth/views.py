0994fbba29dfc71bd31644eb7f3409d34b409627
Hung Nguyen <nguyenhung@tma.com.vn>
2025-01-20 14:23:29 +0700

    Resolve conversations

--- src/pretix/plugins/socialauth/views.py
+++ src/pretix/plugins/socialauth/views.py
@@ -6,7 +6,7 @@ from allauth.socialaccount.adapter import get_adapter
 from allauth.socialaccount.models import SocialApp
 from django.conf import settings
 from django.contrib import messages
-from django.http import HttpRequest, HttpResponse, QueryDict
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import redirect
 from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
@@ -45,7 +45,8 @@ class OAuthLoginView(View):
         updated_url = parsed_url._replace(query=urlencode(query_params))
         return redirect(urlunparse(updated_url))
 
-    def set_oauth2_params(self, request: HttpRequest) -> None:
+    @staticmethod
+    def set_oauth2_params(request: HttpRequest) -> None:
         """
         Handle Login with SSO button from other components
         This function will set 'oauth2_params' in session for oauth2_callback
@@ -81,14 +82,10 @@ class OAuthReturnView(View):
         try:
             user = self.get_or_create_user(request)
             response = process_login_and_set_cookie(request, user, False)
-            oauth2_params = request.session.get("oauth2_params", {})
+            oauth2_params = request.session.pop("oauth2_params", {})
             if oauth2_params:
                 get_params = self.prepare_oauth2_params(oauth2_params)
                 auth_url = reverse("control:oauth2_provider.authorize")
-
-                # Clean up session after use
-                del request.session["oauth2_params"]
-
                 return redirect(f"{auth_url}?{get_params.urlencode()}")
 
             return response
@@ -99,7 +96,8 @@ class OAuthReturnView(View):
             logger.error("Error while authorizing: %s", e)
             return redirect("control:auth.login")
 
-    def get_or_create_user(self, request: HttpRequest) -> User:
+    @staticmethod
+    def get_or_create_user(request: HttpRequest) -> User:
         """
         Get or create a user from social auth information.
         """
@@ -113,11 +111,12 @@ class OAuthReturnView(View):
             },
         )[0]
 
-    def prepare_oauth2_params(self, oauth2_params: dict) -> QueryDict:
+    @staticmethod
+    def prepare_oauth2_params(oauth2_params: dict) -> dict:
         """
         Prepare OAuth2 parameters to be passed to the OAuth2 authorization view.
         """
-        get_params = QueryDict("", mutable=True)
+        get_params = {}
         base_params = {
             "response_type": oauth2_params.get("response_type", "code"),
             "client_id": oauth2_params.get("client_id"),
