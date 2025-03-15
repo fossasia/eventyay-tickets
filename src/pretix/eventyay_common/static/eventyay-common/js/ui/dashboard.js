c07fc5a645af45eef24d8a045cdce8b913ad439e
suhailnadaf509 <suhailnadaf509@gmail.com>
2025-03-15 15:19:05 +0530

    Fixes issue #516

---
+++ src/pretix/eventyay_common/static/eventyay-common/js/ui/dashboard.js
@@ -0,0 +1,37 @@
+$(document).ready(function () {
+  // Initialize typeahead for event search
+  var eventSearch = new Bloodhound({
+    datumTokenizer: Bloodhound.tokenizers.obj.whitespace("name"),
+    queryTokenizer: Bloodhound.tokenizers.whitespace,
+    remote: {
+      url: "/common/events/search/?query=%QUERY",
+      wildcard: "%QUERY",
+    },
+  });
+
+  $("#dashboard_query").typeahead(
+    {
+      hint: true,
+      highlight: true,
+      minLength: 1,
+    },
+    {
+      name: "events",
+      display: "name",
+      source: eventSearch,
+      templates: {
+        suggestion: function (data) {
+          return (
+            '<div><a href="/common/event/' +
+            data.organizer +
+            "/" +
+            data.slug +
+            '/">' +
+            data.name +
+            "</a></div>"
+          );
+        },
+      },
+    }
+  );
+});
