660fc216ee9c4f964ea925c439b9f988b8cfbe98
Sana Fathima <154485124+Sana0523@users.noreply.github.com>
2025-06-01 06:04:13 +0530

    fixed the signature text  according to the requirement (#711)

--- src/pretix/base/secrets.py
+++ src/pretix/base/secrets.py
@@ -111,7 +111,7 @@ class Sig1TicketSecretGenerator(BaseTicketSecretGenerator):
     The resulting string is REVERSED, to avoid all secrets of same length beginning with the same 10
     characters, which would make it impossible to search for secrets manually.
     """
-    verbose_name = _('signature scheme 1 (for very large events, does not work with pretixSCAN on iOS and '
+    verbose_name = _('signature scheme 1 (for very large events, does not work on all apps in the ecosystem and '
                      'changes semantics of offline scanning – please refer to documentation or support for details)')
     identifier = 'pretix_sig1'
     use_revocation_list = True
