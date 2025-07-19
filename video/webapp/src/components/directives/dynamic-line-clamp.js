116f3c33c884fb1966c39425cbe00d8c16820f53
Saksham Sirohi <sakshamsirohi27@gmail.com>
2025-07-19 22:14:51 +0000

    fixing mixins and directives for vue3

--- video/webapp/src/components/directives/dynamic-line-clamp.js
+++ video/webapp/src/components/directives/dynamic-line-clamp.js
@@ -1,9 +1,10 @@
-import Vue from 'vue'
-
-Vue.directive('dynamic-line-clamp', {
-	inserted: function(el) {
+export default {
+  install(app) {
+    app.directive('dynamic-line-clamp', {
+      mounted(el) {
 		const rect = el.getBoundingClientRect()
 		// set webkit-line-clamp
 		el.style.setProperty('--dynamic-line-clamp', Math.floor(rect.height / (14 * 1.4)))
 	}
 })
+}}
