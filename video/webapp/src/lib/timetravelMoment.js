f90be25e950d2fdeaf4af7e42641550f5087a8f1
Saksham Sirohi <sakshamsirohi27@gmail.com>
2025-08-22 10:47:06 +0000

    emoji js fix

--- video/webapp/src/lib/timetravelMoment.js
+++ video/webapp/src/lib/timetravelMoment.js
@@ -1,9 +1,12 @@
 // Vite/ESM-friendly moment-timezone bootstrap with optional "timetravel" override
-import moment from 'moment-timezone'
+import moment from 'moment'
+import 'moment-timezone'
+import 'moment/dist/locale/en-ie'
 import config from 'config'
 
 // Set locale (use Ireland for 24h clock by default, matching previous behavior)
-moment.locale(config.date_locale || 'en-ie')
+const locale=(config.date_locale || 'en-ie')
+moment.locale(locale)
 
 // Optionally override current time for demos/tests
 if (config.timetravelTo) {
@@ -11,7 +14,7 @@ if (config.timetravelTo) {
 	// Override moment.now to freeze time
 	moment.now = () => timetravelTimestamp
 	// eslint-disable-next-line no-console
-	console.warn('timetraveling to', moment().toDate())
+	console.warn('timetraveling to', moment()._d())
 }
 
 export default moment
