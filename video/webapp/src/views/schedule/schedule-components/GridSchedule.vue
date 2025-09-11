8aec473f311d42575b051a682f43b45ea1af7e45
Saksham Sirohi <sakshamsirohi27@gmail.com>
2025-09-12 00:15:01 +0530

    gridschedule scrollbar fix

--- video/webapp/src/views/schedule/schedule-components/GridSchedule.vue
+++ video/webapp/src/views/schedule/schedule-components/GridSchedule.vue
@@ -331,12 +331,13 @@ export default {
 .c-grid-schedule
 	flex: auto
 	background-color: $clr-grey-50
-	max-height: 88vh
 	.grid
 		display: grid
 		grid-template-columns: 78px repeat(var(--total-rooms), 1fr) auto
 		// grid-gap: 8px
 		position: relative
+		max-height: 88vh
+		overflow: auto
 		min-width: min-content
 		> .room
 			position: sticky
