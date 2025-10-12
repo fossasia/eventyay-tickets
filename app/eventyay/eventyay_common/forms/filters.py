0a30173f071738f8e48612a7230c4e696d0310c1
Gagan-Ram <gaganram2003@outlook.com>
2025-10-12 20:15:36 +0530

    add my sessions to common area

--- app/eventyay/eventyay_common/forms/filters.py
+++ app/eventyay/eventyay_common/forms/filters.py
@@ -21,3 +21,28 @@ class UserOrderFilterForm(forms.Form):
             # Query distinct events based on the user's orders
             events = Event.objects.filter(orders__email__iexact=user.email).distinct()
             self.fields['event'].queryset = events
+
+
+class SessionsFilterForm(forms.Form):
+    event = forms.ModelChoiceField(
+        queryset=None,
+        required=False,
+        label=_('Event'),
+        widget=forms.Select(attrs={'class': 'form-control'}),
+        empty_label=_('Select an Event'),
+    )
+
+    search = forms.CharField(
+        required=False,
+        label=_('Search'),
+        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': _('Search by session name')})
+    )
+
+    def __init__(self, *args, **kwargs):
+        user = kwargs.pop('user', None)  # Get the user from the kwargs
+        super().__init__(*args, **kwargs)
+
+        if user:
+            # Query distinct events based on the user's proposals
+            events = Event.objects.filter(submissions__speakers__email__iexact=user.email).distinct()
+            self.fields['event'].queryset = events
